a6df7d647b845248ae7fbab2ea0aaa64f9ed6279
ERussel <emkil.russel@gmail.com>
2021-10-12 16:28:03 +0300

    fix ethereum address generation

--- fearlessTests/Common/Crypto/EthereumAddressTests.swift
+++ fearlessTests/Common/Crypto/EthereumAddressTests.swift
@@ -5,12 +5,12 @@ class EthereumAddressTests: XCTestCase {
     func testAddressFromPublicKey() {
         do {
             let pubKey = try Data(
-                hexString: "6e145ccef1033dea239875dd00dfb4fee6e3348b84985c92f103444683bae07b83b5c38e5e2b0c8529d7fa3f64d46daa1ece2d9ac14cab9477d042c84c32ccd0"
+                hexString: "0x02d661622161b7244c978795b743f4957e199b2400045330700496ec57885632c5"
             )
 
-            let expectedAddress = "001d3f1ef827552ae1114027bd3ecf1f086ba0f9"
+            let expectedAddress = "0xd7381d7f3920495B2e25E6404fD62b8Ce0632d74".lowercased()
 
-            let actualAddress = try pubKey.ethereumAddressFromPublicKey().toHex()
+            let actualAddress = try pubKey.ethereumAddressFromPublicKey().toHex(includePrefix: true)
 
             XCTAssertEqual(expectedAddress, actualAddress)
         } catch {
