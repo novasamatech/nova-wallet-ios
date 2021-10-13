8d99645ba28bbec14c124b85561065b6f1c30346
ERussel <emkil.russel@gmail.com>
2021-10-13 11:03:50 +0300

    fix insets

--- fearless/Modules/Wallet/AccountList/WalletAccountListConstants.swift
+++ fearless/Modules/Wallet/AccountList/WalletAccountListConstants.swift
@@ -5,7 +5,7 @@ struct WalletAccountListConstants {
     static let totalPriceHeight: CGFloat = 66.0
 
     static let assetCellId: String = "fearlessAssetCellId"
-    static let assetCellHeight: CGFloat = 96.0
+    static let assetCellHeight: CGFloat = 88.0
 
     static let actionsCellId: String = "fearlessActionsCellId"
     static let actionsCellHeight: CGFloat = 90
