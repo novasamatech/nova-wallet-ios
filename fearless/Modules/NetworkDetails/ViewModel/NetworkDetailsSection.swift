9ccade131469c27267e1d2f393dbc2e9709528ca
Aleksandr Malina <ef1rspb@gmail.com>
2021-10-20 18:44:06 +0300

    Delete unused code

--- fearless/Modules/NetworkDetails/ViewModel/NetworkDetailsSection.swift
+++ fearless/Modules/NetworkDetails/ViewModel/NetworkDetailsSection.swift
@@ -11,16 +11,3 @@ struct NetworkDetailsSectionViewModel {
     let highlight: Bool
     let title: String
 }
-
-// extension NetworkDetailsSection {
-//    func title(for locale: Locale) -> String? {
-//        switch self {
-//        case .autoSelectNodes:
-//            return nil
-//        case .defaultNodes:
-//            return R.string.localizable.connectionManagementDefaultTitle(preferredLanguages: locale.rLanguages)
-//        case .customNodes:
-//            return R.string.localizable.connectionManagementCustomTitle(preferredLanguages: locale.rLanguages)
-//        }
-//    }
-// }
