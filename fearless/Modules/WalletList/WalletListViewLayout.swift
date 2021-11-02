cf8d73ab2ff8234efb65fd9fd7ca730d7571232e
ERussel <emkil.russel@gmail.com>
2021-11-02 19:16:26 +0300

    add wallet background

--- fearless/Modules/WalletList/WalletListViewLayout.swift
+++ fearless/Modules/WalletList/WalletListViewLayout.swift
@@ -1,12 +1,27 @@
 import UIKit
 
 final class WalletListViewLayout: UIView {
+    let backgroundView: UIImageView = {
+        let view = UIImageView(image: R.image.backgroundImage())
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        setup()
     }
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func setup() {
+        addSubview(backgroundView)
+        backgroundView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
