2247be1db1089e3a80cf214576354a9ddff8e3fa
Aleksandr Malina <ef1rspb@gmail.com>
2021-11-03 12:16:11 +0300

    Add info view

--- fearless/Modules/Crowdloan/CustomCrowdloan/Acala/ContributionSetup/AcalaContributionSetupViewLayout.swift
+++ fearless/Modules/Crowdloan/CustomCrowdloan/Acala/ContributionSetup/AcalaContributionSetupViewLayout.swift
@@ -6,6 +6,13 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
         AcalaContributionMethod.allCases.map { Button(model: $0) }
     }()
 
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.colorBlue()
+        label.font = .p2Paragraph
+        return label
+    }()
+
     override func setupLayout() {
         super.setupLayout()
 
@@ -21,10 +28,31 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
         )
 
         contentView.stackView.insertArrangedSubview(buttonsStack, at: titleLabelIndex + 1)
+        contentView.stackView.setCustomSpacing(16, after: buttonsStack)
         buttonsStack.snp.makeConstraints { make in
             make.width.equalTo(self).offset(-2.0 * UIConstants.horizontalInset)
             make.height.equalTo(44)
         }
+
+        let infoIcon = UIImageView(image: R.image.iconInfoFilled()!.withRenderingMode(.alwaysTemplate))
+        infoIcon.tintColor = R.color.colorBlue()
+
+        let arrowIcon = UIImageView(image: R.image.iconSmallArrow()!.withRenderingMode(.alwaysTemplate))
+        arrowIcon.tintColor = R.color.colorBlue()
+        arrowIcon.contentMode = .center
+
+        let infoStack = UIView.hStack(
+            alignment: .center,
+            spacing: 8,
+            [infoIcon, infoLabel, arrowIcon, UIView()]
+        )
+
+        contentView.stackView.insertArrangedSubview(infoStack, at: titleLabelIndex + 2)
+        contentView.stackView.setCustomSpacing(24, after: infoStack)
+        infoStack.snp.makeConstraints { make in
+            make.width.equalTo(self).offset(-2.0 * UIConstants.horizontalInset)
+        }
+        arrowIcon.snp.makeConstraints { $0.size.equalTo(12) }
     }
 
     override func applyLocalization() {
@@ -34,6 +62,7 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
             let title = button.model.title(for: locale)
             button.imageWithTitleView?.title = title
         }
+        infoLabel.text = "Learn more about different contributions to Acala"
     }
 
     func bind(selectedMethod: AcalaContributionMethod) {
