98a6d5db94a185793fc1c8a05e290085db60c267
Aleksandr Malina <ef1rspb@gmail.com>
2021-11-04 12:57:13 +0300

    Fix ci keys

--- fearless/Modules/Crowdloan/CustomCrowdloan/Acala/AcalaKeys.swift
+++ fearless/Modules/Crowdloan/CustomCrowdloan/Acala/AcalaKeys.swift
@@ -4,6 +4,9 @@ enum AcalaKeys {
     static var authToken: String =
         AcalaKeys.variable(named: "ACALA_AUTH_TOKEN") ?? AcalaCIKeys.authToken
 
+    static var authTestToken: String =
+        AcalaKeys.variable(named: "ACALA_TEST_AUTH_TOKEN") ?? AcalaCIKeys.authTestToken
+
     static func variable(named name: String) -> String? {
         let processInfo = ProcessInfo.processInfo
         guard let value = processInfo.environment[name] else {
