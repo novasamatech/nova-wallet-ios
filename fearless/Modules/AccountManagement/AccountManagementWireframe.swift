6265741106ffc53ea6ebc539ba17eebdf984cd4f
ERussel <emkil.russel@gmail.com>
2021-11-06 21:16:26 +0300

    base export and fix sorting on account list

--- fearless/Modules/AccountManagement/AccountManagementWireframe.swift
+++ fearless/Modules/AccountManagement/AccountManagementWireframe.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-final class AccountManagementWireframe: AccountManagementWireframeProtocol {
+final class AccountManagementWireframe: AccountManagementWireframeProtocol, AuthorizationPresentable {
     func showCreateAccount(
         from view: ControllerBackedProtocol?,
         wallet: MetaAccountModel,
@@ -41,5 +41,117 @@ final class AccountManagementWireframe: AccountManagementWireframeProtocol {
         if let navigationController = view?.controller.navigationController {
             navigationController.pushViewController(controller, animated: true)
         }
+    }
+
+    func showExportAccount(
+        for wallet: MetaAccountModel,
+        chain: ChainModel,
+        options: [ExportOption],
+        locale: Locale?,
+        from view: AccountManagementViewProtocol?
+    ) {
+        authorize(animated: true, cancellable: true) { [weak self] success in
+            if success {
+                self?.performExportPresentation(
+                    for: wallet,
+                    chain: chain,
+                    options: options,
+                    locale: locale,
+                    from: view
+                )
+            }
+        }
+    }
+
+    // MARK: Private
+
+    private func performExportPresentation(
+        for wallet: MetaAccountModel,
+        chain: ChainModel,
+        options: [ExportOption],
+        locale: Locale?,
+        from view: AccountManagementViewProtocol?
+    ) {
+        let cancelTitle = R.string.localizable
+            .commonCancel(preferredLanguages: locale?.rLanguages)
+
+        let actions: [AlertPresentableAction] = options.map { option in
+            switch option {
+            case .mnemonic:
+                let title = R.string.localizable.importMnemonic(preferredLanguages: locale?.rLanguages)
+                return AlertPresentableAction(title: title) { [weak self] in
+                    self?.showMnemonicExport(for: wallet, chain: chain, from: view)
+                }
+            case .keystore:
+                let title = R.string.localizable.importRecoveryJson(preferredLanguages: locale?.rLanguages)
+                return AlertPresentableAction(title: title) { [weak self] in
+                    self?.showKeystoreExport(for: wallet, chain: chain, from: view)
+                }
+            case .seed:
+                let title = R.string.localizable.importRawSeed(preferredLanguages: locale?.rLanguages)
+                return AlertPresentableAction(title: title) { [weak self] in
+                    self?.showSeedExport(for: wallet, chain: chain, from: view)
+                }
+            }
+        }
+
+        let title = R.string.localizable.importSourcePickerTitle(preferredLanguages: locale?.rLanguages)
+        let alertViewModel = AlertPresentableViewModel(
+            title: title,
+            message: nil,
+            actions: actions,
+            closeAction: cancelTitle
+        )
+
+        present(
+            viewModel: alertViewModel,
+            style: .actionSheet,
+            from: view
+        )
+    }
+
+    private func showMnemonicExport(
+        for _: MetaAccountModel,
+        chain _: ChainModel,
+        from view: AccountManagementViewProtocol?
+    ) {
+        guard let mnemonicView = ExportMnemonicViewFactory.createViewForAddress("") else {
+            return
+        }
+
+        view?.controller.navigationController?.pushViewController(
+            mnemonicView.controller,
+            animated: true
+        )
+    }
+
+    private func showKeystoreExport(
+        for _: MetaAccountModel,
+        chain _: ChainModel,
+        from view: AccountManagementViewProtocol?
+    ) {
+        guard let passwordView = AccountExportPasswordViewFactory.createView(with: "") else {
+            return
+        }
+
+        view?.controller.navigationController?.pushViewController(
+            passwordView.controller,
+            animated: true
+        )
+    }
+
+    private func showSeedExport(
+        for _: MetaAccountModel,
+        chain _: ChainModel,
+        from view: AccountManagementViewProtocol?
+    ) {
+        guard let seedView = ExportSeedViewFactory.createViewForAddress("") else {
+            return
+        }
+
+        view?.controller.navigationController?.pushViewController(
+            seedView.controller,
+            animated: true
+        )
     }
 }
