ac09af2633163b86cdb403467edb3588e90a303c
Aleksandr Malina <ef1rspb@gmail.com>
2021-11-07 16:56:05 +0300

    Define "Learn more about Acala" view

--- fearless/Modules/Crowdloan/CustomCrowdloan/Acala/ContributionSetup/AcalaContributionSetupViewLayout.swift
+++ fearless/Modules/Crowdloan/CustomCrowdloan/Acala/ContributionSetup/AcalaContributionSetupViewLayout.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SoraUI
 
 final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayout {
     typealias Button = RadioButton<AcalaContributionMethod>
@@ -6,12 +7,7 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
         AcalaContributionMethod.allCases.map { Button(model: $0) }
     }()
 
-    let infoLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = R.color.colorNovaBlue()
-        label.font = .p2Paragraph
-        return label
-    }()
+    let acalaLearnMoreView: BackgroundedContentControl = AcalaLearnMoreView()
 
     override func setupLayout() {
         super.setupLayout()
@@ -28,13 +24,15 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
         )
 
         contentView.stackView.insertArrangedSubview(buttonsStack, at: titleLabelIndex + 1)
-        contentView.stackView.setCustomSpacing(16, after: buttonsStack)
         buttonsStack.snp.makeConstraints { make in
             make.width.equalTo(self).offset(-2.0 * UIConstants.horizontalInset)
             make.height.equalTo(44)
         }
 
-        // TODO: Add custom  "Learn more about different contributions to Acala" backgroundControl
+        contentView.stackView.insertArrangedSubview(acalaLearnMoreView, at: titleLabelIndex + 2)
+        acalaLearnMoreView.snp.makeConstraints { make in
+            make.width.equalTo(self)
+        }
     }
 
     override func applyLocalization() {
@@ -44,10 +42,104 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
             let title = button.model.title(for: locale)
             button.imageWithTitleView?.title = title
         }
-        infoLabel.text = "Learn more about different contributions to Acala"
+        if let acalaLearnMoreView = acalaLearnMoreView as? AcalaLearnMoreView {
+            acalaLearnMoreView.titleLabel.text = R.string.localizable
+                .crowdloanAcalaLearnMore(preferredLanguages: locale.rLanguages)
+        }
     }
 
     func bind(selectedMethod: AcalaContributionMethod) {
         buttons.forEach { $0.isSelected = $0.model == selectedMethod }
+    }
+}
+
+private final class AcalaLearnMoreView: BackgroundedContentControl {
+    let iconView: UIImageView = {
+        let view = UIImageView()
+        let image = R.image.iconInfoFilled()!.withRenderingMode(.alwaysTemplate)
+        view.image = image
+        view.tintColor = R.color.colorNovaBlue()!
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .p2Paragraph
+        label.textColor = R.color.colorNovaBlue()
+        return label
+    }()
+
+    let arrowIconView: UIView = {
+        let imageView = UIImageView()
+        let image = R.image.iconSmallArrow()!.withRenderingMode(.alwaysTemplate)
+        imageView.image = image
+        imageView.tintColor = R.color.colorNovaBlue()!
+        // imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        let shapeView = ShapeView()
+        shapeView.isUserInteractionEnabled = false
+        shapeView.fillColor = .clear
+        shapeView.highlightedFillColor = R.color.colorHighlightedAccent()!
+        backgroundView = shapeView
+
+        contentInsets = .init(top: 0, left: 16, bottom: 0, right: 16)
+        setupLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        contentView?.frame = CGRect(
+            x: bounds.minX + contentInsets.left,
+            y: bounds.minY + contentInsets.top,
+            width: max(bounds.width - contentInsets.left - contentInsets.right, 0),
+            height: max(bounds.height - contentInsets.top - contentInsets.bottom, 0)
+        )
+    }
+
+    override var intrinsicContentSize: CGSize {
+        CGSize(
+            width: UIView.noIntrinsicMetric,
+            height: 56
+        )
+    }
+
+    private func setupLayout() {
+        let baseView = UIView()
+        baseView.isUserInteractionEnabled = false
+
+        baseView.addSubview(iconView)
+        iconView.snp.makeConstraints { make in
+            make.leading.centerY.equalToSuperview()
+            make.size.equalTo(16)
+        }
+
+        baseView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(iconView.snp.trailing).offset(8)
+            make.centerY.equalToSuperview()
+        }
+
+        baseView.addSubview(arrowIconView)
+        arrowIconView.snp.makeConstraints { make in
+            make.trailing.centerY.equalToSuperview()
+            make.leading.greaterThanOrEqualTo(titleLabel.snp.trailing).offset(4)
+            make.size.equalTo(24)
+        }
+
+        contentView = baseView
+
+        baseView.autoresizingMask = [.flexibleWidth]
     }
 }
