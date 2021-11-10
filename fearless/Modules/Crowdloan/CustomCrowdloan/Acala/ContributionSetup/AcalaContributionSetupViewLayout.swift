c97a20f0740eda357e5ecaf4778db58aab5d27c1
Aleksandr Malina <ef1rspb@gmail.com>
2021-11-10 17:37:11 +0300

    NOVA-3392 Fix Acala "Learn more" link text doesn't fit into screen

--- fearless/Modules/Crowdloan/CustomCrowdloan/Acala/ContributionSetup/AcalaContributionSetupViewLayout.swift
+++ fearless/Modules/Crowdloan/CustomCrowdloan/Acala/ContributionSetup/AcalaContributionSetupViewLayout.swift
@@ -7,7 +7,15 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
         AcalaContributionMethod.allCases.map { Button(model: $0) }
     }()
 
-    let acalaLearnMoreView: BackgroundedContentControl = AcalaLearnMoreView()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .p2Paragraph
+        label.textColor = R.color.colorNovaBlue()
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private(set) var acalaLearnMoreView: RowView<UIView>!
 
     override func setupLayout() {
         super.setupLayout()
@@ -29,6 +37,28 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
             make.height.equalTo(44)
         }
 
+        let iconView = UIImageView()
+        let iconimage = R.image.iconInfoFilled()!.withRenderingMode(.alwaysTemplate)
+        iconView.image = iconimage
+        iconView.tintColor = R.color.colorNovaBlue()!
+        iconView.contentMode = .scaleAspectFit
+
+        let arrowIconView = UIImageView()
+        let arrowImage = R.image.iconSmallArrow()!.withRenderingMode(.alwaysTemplate)
+        arrowIconView.image = arrowImage
+        arrowIconView.tintColor = R.color.colorNovaBlue()!
+
+        let content = UIView.hStack(alignment: .center, spacing: 4, [iconView, titleLabel, arrowIconView])
+        content.setCustomSpacing(8, after: iconView)
+        iconView.snp.makeConstraints { make in
+            make.size.equalTo(16)
+        }
+        arrowIconView.snp.makeConstraints { make in
+            make.size.equalTo(24)
+        }
+
+        acalaLearnMoreView = RowView(contentView: content)
+        acalaLearnMoreView.contentInsets = .init(top: 16, left: 20, bottom: 24, right: 29)
         contentView.stackView.insertArrangedSubview(acalaLearnMoreView, at: titleLabelIndex + 2)
         acalaLearnMoreView.snp.makeConstraints { make in
             make.width.equalTo(self)
@@ -42,103 +72,10 @@ final class AcalaContributionSetupViewLayout: CrowdloanContributionSetupViewLayo
             let title = button.model.title(for: locale)
             button.imageWithTitleView?.title = title
         }
-        if let acalaLearnMoreView = acalaLearnMoreView as? AcalaLearnMoreView {
-            acalaLearnMoreView.titleLabel.text = R.string.localizable
-                .crowdloanAcalaLearnMore(preferredLanguages: locale.rLanguages)
-        }
+        titleLabel.text = R.string.localizable.crowdloanAcalaLearnMore(preferredLanguages: locale.rLanguages)
     }
 
     func bind(selectedMethod: AcalaContributionMethod) {
         buttons.forEach { $0.isSelected = $0.model == selectedMethod }
-    }
-}
-
-private final class AcalaLearnMoreView: BackgroundedContentControl {
-    let iconView: UIImageView = {
-        let view = UIImageView()
-        let image = R.image.iconInfoFilled()!.withRenderingMode(.alwaysTemplate)
-        view.image = image
-        view.tintColor = R.color.colorNovaBlue()!
-        view.contentMode = .scaleAspectFit
-        return view
-    }()
-
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = .p2Paragraph
-        label.textColor = R.color.colorNovaBlue()
-        return label
-    }()
-
-    let arrowIconView: UIView = {
-        let imageView = UIImageView()
-        let image = R.image.iconSmallArrow()!.withRenderingMode(.alwaysTemplate)
-        imageView.image = image
-        imageView.tintColor = R.color.colorNovaBlue()!
-        return imageView
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        let shapeView = ShapeView()
-        shapeView.isUserInteractionEnabled = false
-        shapeView.fillColor = .clear
-        shapeView.highlightedFillColor = R.color.colorHighlightedAccent()!
-        backgroundView = shapeView
-
-        contentInsets = .init(top: 0, left: 16, bottom: 0, right: 16)
-        setupLayout()
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-        contentView?.frame = CGRect(
-            x: bounds.minX + contentInsets.left,
-            y: bounds.minY + contentInsets.top,
-            width: max(bounds.width - contentInsets.left - contentInsets.right, 0),
-            height: max(bounds.height - contentInsets.top - contentInsets.bottom, 0)
-        )
-    }
-
-    override var intrinsicContentSize: CGSize {
-        CGSize(
-            width: UIView.noIntrinsicMetric,
-            height: 56
-        )
-    }
-
-    private func setupLayout() {
-        let baseView = UIView()
-        baseView.isUserInteractionEnabled = false
-
-        baseView.addSubview(iconView)
-        iconView.snp.makeConstraints { make in
-            make.leading.centerY.equalToSuperview()
-            make.size.equalTo(16)
-        }
-
-        baseView.addSubview(titleLabel)
-        titleLabel.snp.makeConstraints { make in
-            make.leading.equalTo(iconView.snp.trailing).offset(8)
-            make.centerY.equalToSuperview()
-        }
-
-        baseView.addSubview(arrowIconView)
-        arrowIconView.snp.makeConstraints { make in
-            make.trailing.centerY.equalToSuperview()
-            make.leading.greaterThanOrEqualTo(titleLabel.snp.trailing).offset(4)
-            make.size.equalTo(24)
-        }
-
-        contentView = baseView
-
-        baseView.autoresizingMask = [.flexibleWidth]
     }
 }
