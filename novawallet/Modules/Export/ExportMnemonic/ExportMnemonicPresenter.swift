f07ec91b63c8e205ce9a83b27dfd56609bc2b9a7
ERussel <emkil.russel@gmail.com>
2021-11-30 18:23:07 +0500

    export redesign

--- novawallet/Modules/Export/ExportMnemonic/ExportMnemonicPresenter.swift
+++ novawallet/Modules/Export/ExportMnemonic/ExportMnemonicPresenter.swift
@@ -3,48 +3,20 @@ import SoraFoundation
 
 final class ExportMnemonicPresenter {
     weak var view: ExportGenericViewProtocol?
-    var wireframe: ExportMnemonicWireframeProtocol!
-    var interactor: ExportMnemonicInteractorInputProtocol!
-
-    let localizationManager: LocalizationManager
+    let wireframe: ExportMnemonicWireframeProtocol
+    let interactor: ExportMnemonicInteractorInputProtocol
+    let localizationManager: LocalizationManagerProtocol
 
     private(set) var exportData: ExportMnemonicData?
 
-    init(localizationManager: LocalizationManager) {
+    init(
+        interactor: ExportMnemonicInteractorInputProtocol,
+        wireframe: ExportMnemonicWireframeProtocol,
+        localizationManager: LocalizationManagerProtocol
+    ) {
+        self.interactor = interactor
+        self.wireframe = wireframe
         self.localizationManager = localizationManager
-    }
-
-    private func share() {
-        guard let data = exportData else {
-            return
-        }
-
-        let text: String
-
-        let locale = localizationManager.selectedLocale
-
-        if let derivationPath = exportData?.derivationPath {
-            text = R.string.localizable
-                .exportMnemonicWithDpTemplate(
-                    data.chain.name,
-                    data.mnemonic.toString(),
-                    derivationPath,
-                    preferredLanguages: locale.rLanguages
-                )
-        } else {
-            text = R.string.localizable
-                .exportMnemonicWithoutDpTemplate(
-                    data.chain.name,
-                    data.mnemonic.toString(),
-                    preferredLanguages: locale.rLanguages
-                )
-        }
-
-        wireframe.share(source: TextSharingSource(message: text), from: view) { [weak self] completed in
-            if completed {
-                self?.wireframe.close(view: self?.view)
-            }
-        }
     }
 }
 
@@ -54,54 +26,21 @@ extension ExportMnemonicPresenter: ExportGenericPresenterProtocol {
     }
 
     func activateExport() {
-        let locale = localizationManager.selectedLocale
-
-        let title = R.string.localizable.accountExportWarningTitle(preferredLanguages: locale.rLanguages)
-        let message = R.string.localizable.accountExportWarningMessage(preferredLanguages: locale.rLanguages)
-
-        let exportTitle = R.string.localizable.accountExportAction(preferredLanguages: locale.rLanguages)
-        let exportAction = AlertPresentableAction(title: exportTitle) { [weak self] in
-            self?.share()
-        }
-
-        let cancelTitle = R.string.localizable.commonCancel(preferredLanguages: locale.rLanguages)
-        let viewModel = AlertPresentableViewModel(
-            title: title,
-            message: message,
-            actions: [exportAction],
-            closeAction: cancelTitle
-        )
-
-        wireframe.present(viewModel: viewModel, style: .alert, from: view)
-    }
-
-    func activateAccessoryOption() {
         guard let exportData = exportData else {
             return
         }
 
         wireframe.openConfirmationForMnemonic(exportData.mnemonic, from: view)
     }
+
+    func activateAdvancedSettings() {}
 }
 
 extension ExportMnemonicPresenter: ExportMnemonicInteractorOutputProtocol {
     func didReceive(exportData: ExportMnemonicData) {
         self.exportData = exportData
 
-        guard let cryptoType = exportData.metaAccount.fetch(
-            for: exportData.chain.accountRequest()
-        )?.cryptoType else {
-            didReceive(error: ChainAccountFetchingError.accountNotExists)
-            return
-        }
-
-        let viewModel = ExportMnemonicViewModel(
-            option: .mnemonic,
-            chain: exportData.chain,
-            derivationPath: exportData.derivationPath,
-            cryptoType: cryptoType,
-            mnemonic: exportData.mnemonic.allWords()
-        )
+        let viewModel = ExportGenericViewModel(sourceDetails: exportData.mnemonic.toString())
         view?.set(viewModel: viewModel)
     }
 
