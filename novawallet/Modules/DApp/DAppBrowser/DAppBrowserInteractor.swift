f566f90908f98a23b46cabfe4b9bf31cc1ac2a4d
ERussel <emkil.russel@gmail.com>
2021-12-16 11:46:55 +0500

    add model logic

--- novawallet/Modules/DApp/DAppBrowser/DAppBrowserInteractor.swift
+++ novawallet/Modules/DApp/DAppBrowser/DAppBrowserInteractor.swift
@@ -1,7 +1,125 @@
 import UIKit
+import RobinHood
 
-final class DAppBrowserInteractor {
-    weak var presenter: DAppBrowserInteractorOutputProtocol!
+enum DAppBrowserInteractorError: Error {
+    case scriptFileMissing
+    case invalidUrl
+    case unexpectedMessageType
 }
 
-extension DAppBrowserInteractor: DAppBrowserInteractorInputProtocol {}
+final class DAppBrowserInteractor {
+    static let subscriptionName = "_nova_"
+
+    weak var presenter: DAppBrowserInteractorOutputProtocol!
+
+    let userInput: String
+    let wallet: MetaAccountModel
+    let operationQueue: OperationQueue
+    let logger: LoggerProtocol?
+
+    init(
+        userInput: String,
+        wallet: MetaAccountModel,
+        operationQueue: OperationQueue,
+        logger: LoggerProtocol? = nil
+    ) {
+        self.userInput = userInput
+        self.wallet = wallet
+        self.operationQueue = operationQueue
+        self.logger = logger
+    }
+
+    private func createBridgeScriptOperation() -> BaseOperation<DAppBrowserScript> {
+        ClosureOperation<DAppBrowserScript> {
+            guard let jsUrl = Bundle.main.url(forResource: "nova-min", withExtension: "js") else {
+                throw DAppBrowserInteractorError.scriptFileMissing
+            }
+
+            let content = try String(contentsOf: jsUrl)
+
+            return DAppBrowserScript(content: content, insertionPoint: .atDocStart)
+        }
+    }
+
+    private func createSubscriptionScript() -> DAppBrowserScript {
+        let content =
+            """
+            window.addEventListener("message", ({ data, source }) => {
+              // only allow messages from our window, by the loader
+              if (source !== window) {
+                return;
+              }
+
+              if (data.origin === "dapp-request") {
+                window.webkit.messageHandlers.\(Self.subscriptionName).postMessage(data);
+              }
+            });
+            """
+
+        let script = DAppBrowserScript(content: content, insertionPoint: .atDocEnd)
+        return script
+    }
+
+    func provideModel() {
+        let maybeUrl: URL? = {
+            if NSPredicate.urlPredicate.evaluate(with: userInput), let inputUrl = URL(string: userInput) {
+                return inputUrl
+            } else {
+                let querySet = CharacterSet.urlQueryAllowed
+                guard let searchQuery = userInput.addingPercentEncoding(withAllowedCharacters: querySet) else {
+                    return nil
+                }
+
+                return URL(string: "https://www.google.com/search?q=\(searchQuery)")
+            }
+        }()
+
+        guard let url = maybeUrl else {
+            presenter.didReceive(error: DAppBrowserInteractorError.invalidUrl)
+            return
+        }
+
+        let bridgeOperation = createBridgeScriptOperation()
+        let subscriptionScript = createSubscriptionScript()
+
+        bridgeOperation.completionBlock = { [weak self] in
+            DispatchQueue.main.async {
+                do {
+                    let bridgeScript = try bridgeOperation.extractNoCancellableResultData()
+
+                    let model = DAppBrowserModel(
+                        url: url,
+                        subscriptionName: Self.subscriptionName,
+                        scripts: [bridgeScript, subscriptionScript]
+                    )
+
+                    self?.presenter.didReceiveDApp(model: model)
+                } catch {
+                    self?.presenter.didReceive(error: error)
+                }
+            }
+        }
+
+        operationQueue.addOperation(bridgeOperation)
+    }
+}
+
+extension DAppBrowserInteractor: DAppBrowserInteractorInputProtocol {
+    func setup() {
+        provideModel()
+    }
+
+    func process(message: Any) {
+        guard let dict = message as? NSDictionary else {
+            presenter.didReceive(error: DAppBrowserInteractorError.unexpectedMessageType)
+            return
+        }
+
+        do {
+            let parsedMessage = try dict.map(to: PolkadotExtensionMessage.self)
+            logger?.error("Did receive message: \(parsedMessage)")
+        } catch {
+            presenter.didReceive(error: error)
+        }
+    }
+}
--- novawallet/Modules/DApp/DAppBrowser/DAppBrowserViewController.swift
+++ novawallet/Modules/DApp/DAppBrowser/DAppBrowserViewController.swift
@@ -1,9 +1,12 @@
 import UIKit
+import WebKit
 
-final class DAppBrowserViewController: UIViewController {
+final class DAppBrowserViewController: UIViewController, ViewHolder {
     typealias RootViewType = DAppBrowserViewLayout
 
     let presenter: DAppBrowserPresenterProtocol
+
+    private var viewModel: DAppBrowserModel?
 
     init(presenter: DAppBrowserPresenterProtocol) {
         self.presenter = presenter
@@ -26,4 +29,53 @@ final class DAppBrowserViewController: UIViewController {
     }
 }
 
-extension DAppBrowserViewController: DAppBrowserViewProtocol {}
+extension DAppBrowserViewController: WKScriptMessageHandler {
+    func userContentController(
+        _: WKUserContentController,
+        didReceive message: WKScriptMessage
+    ) {
+        guard viewModel?.subscriptionName == message.name else {
+            return
+        }
+
+        presenter.process(message: message.body)
+    }
+}
+
+extension DAppBrowserViewController: DAppBrowserViewProtocol {
+    func didReceive(viewModel: DAppBrowserModel) {
+        let contentController = rootView.webView.configuration.userContentController
+
+        if let oldViewModel = self.viewModel {
+            contentController.removeAllUserScripts()
+            contentController.removeScriptMessageHandler(forName: oldViewModel.subscriptionName)
+        }
+
+        self.viewModel = viewModel
+
+        for script in viewModel.scripts {
+            let wkScript: WKUserScript
+            switch script.insertionPoint {
+            case .atDocStart:
+                wkScript = WKUserScript(
+                    source: script.content,
+                    injectionTime: .atDocumentStart,
+                    forMainFrameOnly: false
+                )
+            case .atDocEnd:
+                wkScript = WKUserScript(
+                    source: script.content,
+                    injectionTime: .atDocumentEnd,
+                    forMainFrameOnly: false
+                )
+            }
+
+            contentController.addUserScript(wkScript)
+        }
+
+        contentController.add(self, name: viewModel.subscriptionName)
+
+        let request = URLRequest(url: viewModel.url)
+        rootView.webView.load(request)
+    }
+}
