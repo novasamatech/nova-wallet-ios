4a927feb8d466ff9c1aaef7f73d92a66d9be5420
ERussel <emkil.russel@gmail.com>
2021-12-16 11:46:55 +0500

    add model logic

--- novawallet/Modules/DApp/DAppBrowser/DAppBrowserViewFactory.swift
+++ novawallet/Modules/DApp/DAppBrowser/DAppBrowserViewFactory.swift
@@ -1,11 +1,26 @@
 import Foundation
+import SoraFoundation
 
 struct DAppBrowserViewFactory {
-    static func createView(for _: String) -> DAppBrowserViewProtocol? {
-        let interactor = DAppBrowserInteractor()
+    static func createView(for userInput: String) -> DAppBrowserViewProtocol? {
+        let localizationManager = LocalizationManager.shared
+        let logger = Logger.shared
+
+        let interactor = DAppBrowserInteractor(
+            userInput: userInput,
+            wallet: SelectedWalletSettings.shared.value,
+            operationQueue: OperationManagerFacade.sharedDefaultQueue,
+            logger: logger
+        )
+
         let wireframe = DAppBrowserWireframe()
 
-        let presenter = DAppBrowserPresenter(interactor: interactor, wireframe: wireframe)
+        let presenter = DAppBrowserPresenter(
+            interactor: interactor,
+            wireframe: wireframe,
+            localizationManager: localizationManager,
+            logger: logger
+        )
 
         let view = DAppBrowserViewController(presenter: presenter)
 
