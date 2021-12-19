059a174ed12b11c3ce7f3e6ee1e130aea055f86a
ERussel <emkil.russel@gmail.com>
2021-12-19 12:45:17 +0500

    make staking state setup async

--- novawallet/Modules/Staking/Model/StakingSharedState.swift
+++ novawallet/Modules/Staking/Model/StakingSharedState.swift
@@ -2,15 +2,15 @@ import Foundation
 
 final class StakingSharedState {
     let settings: StakingAssetSettings
-    private(set) var eraValidatorService: EraValidatorServiceProtocol
-    private(set) var rewardCalculationService: RewardCalculatorServiceProtocol
+    private(set) var eraValidatorService: EraValidatorServiceProtocol?
+    private(set) var rewardCalculationService: RewardCalculatorServiceProtocol?
     let stakingLocalSubscriptionFactory: StakingLocalSubscriptionFactoryProtocol
     let stakingAnalyticsLocalSubscriptionFactory: StakingAnalyticsLocalSubscriptionFactoryProtocol
 
     init(
         settings: StakingAssetSettings,
-        eraValidatorService: EraValidatorServiceProtocol,
-        rewardCalculationService: RewardCalculatorServiceProtocol,
+        eraValidatorService: EraValidatorServiceProtocol?,
+        rewardCalculationService: RewardCalculatorServiceProtocol?,
         stakingLocalSubscriptionFactory: StakingLocalSubscriptionFactoryProtocol,
         stakingAnalyticsLocalSubscriptionFactory: StakingAnalyticsLocalSubscriptionFactoryProtocol
     ) {
