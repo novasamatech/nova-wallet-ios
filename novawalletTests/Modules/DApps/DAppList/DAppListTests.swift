6f581db8462d5fd0af568203a835937f7cbd08a2
ERussel <emkil.russel@gmail.com>
2021-12-28 20:58:02 +0500

    add tests

---
+++ novawalletTests/Modules/DApps/DAppList/DAppListTests.swift
@@ -0,0 +1,96 @@
+import XCTest
+@testable import novawallet
+import SoraKeystore
+import SoraFoundation
+import Cuckoo
+
+class DAppListTests: XCTestCase {
+    func testSuccessSetup() throws {
+        // given
+
+        let keychain = InMemoryKeychain()
+
+        let storageFacade = UserDataStorageTestFacade()
+        let walletSettings = SelectedWalletSettings(
+            storageFacade: storageFacade,
+            operationQueue: OperationQueue()
+        )
+
+        try AccountCreationHelper.createMetaAccountFromMnemonic(
+            cryptoType: .sr25519,
+            keychain: keychain,
+            settings: walletSettings
+        )
+
+        let view = MockDAppListViewProtocol()
+        let wireframe = MockDAppListWireframeProtocol()
+
+        let dAppProvider = SingleValueProviderStub(
+            item: DAppList(categories: [], dApps: [])
+        )
+
+        let interactor = DAppListInteractor(
+            walletSettings: walletSettings,
+            eventCenter: EventCenter.shared,
+            dAppProvider: AnySingleValueProvider(dAppProvider)
+        )
+
+        let presenter = DAppListPresenter(
+            interactor: interactor,
+            wireframe: wireframe,
+            viewModelFactory: DAppListViewModelFactory(),
+            localizationManager: LocalizationManager.shared
+        )
+
+        presenter.view = view
+        interactor.presenter = presenter
+
+        // when
+
+        let iconExpectation = XCTestExpectation()
+
+        let stateExpectation = XCTestExpectation()
+        stateExpectation.expectedFulfillmentCount = 2
+
+        let dAppsListExpectation = XCTestExpectation()
+
+        var actualState: DAppListState? = nil
+
+        stub(view) { stub in
+            stub.didReceive(state: any()).then { state in
+                actualState = state
+
+                stateExpectation.fulfill()
+            }
+
+            stub.didReceiveDApps(viewModels: any()).then { _ in
+                dAppsListExpectation.fulfill()
+            }
+
+            stub.didReceiveAccount(icon: any()).then { _ in
+                iconExpectation.fulfill()
+            }
+        }
+
+        presenter.setup()
+
+        // then
+
+        wait(for: [iconExpectation, stateExpectation], timeout: 10.0)
+
+        // when
+
+        presenter.filterDApps(forCategory: nil)
+
+        // then
+
+        wait(for: [dAppsListExpectation], timeout: 10)
+
+        switch actualState {
+        case .loading, .error, .none:
+            XCTFail("Unexpected final state")
+        case .loaded:
+            break
+        }
+    }
+}
