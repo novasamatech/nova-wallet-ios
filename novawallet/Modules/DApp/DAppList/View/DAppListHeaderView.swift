7abb6e084b55ce0a075982e14c26bc948d5ab95a
ERussel <emkil.russel@gmail.com>
2021-12-29 11:25:01 +0500

    move dapp list to collection view

--- novawallet/Modules/DApp/DAppList/View/DAppListHeaderView.swift
+++ novawallet/Modules/DApp/DAppList/View/DAppListHeaderView.swift
@@ -1,7 +1,7 @@
 import UIKit
 import SoraUI
 
-final class DAppListHeaderView: UIView {
+final class DAppListHeaderView: UICollectionViewCell {
     let titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = R.color.colorWhite()
@@ -42,10 +42,45 @@ final class DAppListHeaderView: UIView {
         return label
     }()
 
+    let searchView: ControlView<TriangularedBlurView, IconDetailsView> = {
+        let backgroundView = TriangularedBlurView()
+        backgroundView.overlayView.highlightedFillColor = R.color.colorAccentSelected()!
+
+        let contentView = IconDetailsView()
+        contentView.imageView.image = R.image.iconSearch()?.withRenderingMode(.alwaysTemplate)
+        contentView.tintColor = R.color.colorWhite48()
+        contentView.detailsLabel.textColor = R.color.colorWhite48()
+        contentView.detailsLabel.font = .p1Paragraph
+        contentView.detailsLabel.numberOfLines = 0
+
+        return ControlView(backgroundView: backgroundView, contentView: contentView, preferredHeight: 52.0)
+    }()
+
+    var selectedLocale = Locale.current {
+        didSet {
+            if selectedLocale != oldValue {
+                setupLocalization()
+            }
+        }
+    }
+
+    override func preferredLayoutAttributesFitting(
+        _ layoutAttributes: UICollectionViewLayoutAttributes
+    ) -> UICollectionViewLayoutAttributes {
+        let targetSize = CGSize(width: layoutAttributes.frame.width, height: 0)
+        layoutAttributes.frame.size = contentView.systemLayoutSizeFitting(
+            targetSize,
+            withHorizontalFittingPriority: .required,
+            verticalFittingPriority: .fittingSizeLevel
+        )
+        return layoutAttributes
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
         setupLayout()
+        setupLocalization()
     }
 
     @available(*, unavailable)
@@ -53,39 +88,64 @@ final class DAppListHeaderView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    private func setupLocalization() {
+        titleLabel.text = R.string.localizable.tabbarDappsTitle(
+            preferredLanguages: selectedLocale.rLanguages
+        )
+
+        decorationTitleLabel.text = R.string.localizable.dappDecorationTitle(
+            preferredLanguages: selectedLocale.rLanguages
+        )
+
+        decorationSubtitleLabel.text = R.string.localizable.dappsDecorationSubtitle(
+            preferredLanguages: selectedLocale.rLanguages
+        )
+
+        searchView.controlContentView.detailsLabel.text = R.string.localizable.dappListSearch(
+            preferredLanguages: selectedLocale.rLanguages
+        )
+    }
+
     private func setupLayout() {
-        addSubview(accountButton)
+        contentView.addSubview(accountButton)
         accountButton.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(10.0)
-            make.trailing.equalToSuperview()
+            make.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
             make.size.equalTo(UIConstants.navigationAccountIconSize)
         }
 
-        addSubview(titleLabel)
+        contentView.addSubview(titleLabel)
         titleLabel.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
+            make.leading.equalToSuperview().inset(UIConstants.horizontalInset)
             make.trailing.equalTo(accountButton.snp.leading).offset(-8.0)
             make.centerY.equalTo(accountButton.snp.centerY)
         }
 
-        addSubview(decorationView)
+        contentView.addSubview(decorationView)
         decorationView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
             make.top.equalTo(accountButton.snp.bottom).offset(16.0)
-            make.height.equalTo(162.0)
-            make.bottom.equalToSuperview()
         }
 
         decorationView.addSubview(decorationTitleLabel)
         decorationTitleLabel.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
-            make.top.equalToSuperview().inset(20.0)
+            make.top.equalToSuperview().inset(24.0)
         }
 
         decorationView.addSubview(decorationSubtitleLabel)
         decorationSubtitleLabel.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
             make.top.equalTo(decorationTitleLabel.snp.bottom).offset(8.0)
+            make.bottom.equalToSuperview().inset(24.0)
+        }
+
+        contentView.addSubview(searchView)
+        searchView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
+            make.top.equalTo(decorationView.snp.bottom).offset(12.0)
+            make.height.equalTo(52.0)
+            make.bottom.equalToSuperview().inset(12.0)
         }
     }
 }
