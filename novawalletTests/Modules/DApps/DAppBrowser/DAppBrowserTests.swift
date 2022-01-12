e11a0119b66883b45c27442ad7c437fee7262251
ERussel <emkil.russel@gmail.com>
2022-01-12 10:14:57 +0500

    add setup tests

--- novawalletTests/Modules/DApps/DAppBrowser/DAppBrowserTests.swift
+++ novawalletTests/Modules/DApps/DAppBrowser/DAppBrowserTests.swift
@@ -1,14 +1,80 @@
 import XCTest
+@testable import novawallet
+import Cuckoo
+import SoraKeystore
+import SoraFoundation
 
 class DAppBrowserTests: XCTestCase {
+    let dAppURL = URL(string: "https://polkadot.js.org/apps")!
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let dAppChain = ChainModelGenerator.generateChain(
+        generatingAssets: 2,
+        addressPrefix: 42,
+        hasCrowdloans: true
+    )
+
+    func testSetupCompletion() throws {
+        // given
+
+        let view = MockDAppBrowserViewProtocol()
+        let wireframe = MockDAppBrowserWireframeProtocol()
+
+        let keychain = InMemoryKeychain()
+
+        let storageFacade = UserDataStorageTestFacade()
+        let walletSettings = SelectedWalletSettings(
+            storageFacade: storageFacade,
+            operationQueue: OperationQueue()
+        )
+
+        try AccountCreationHelper.createMetaAccountFromMnemonic(
+            cryptoType: .sr25519,
+            keychain: keychain,
+            settings: walletSettings
+        )
+
+        let chainRegistry = MockChainRegistryProtocol().applyDefault(for: [dAppChain])
+
+        let interactor = DAppBrowserInteractor(
+            userQuery: .url(dAppURL),
+            wallet: walletSettings.value,
+            chainRegistry: chainRegistry,
+            operationQueue: OperationQueue()
+        )
+
+        let presenter = DAppBrowserPresenter(
+            interactor: interactor,
+            wireframe: wireframe,
+            localizationManager: LocalizationManager.shared
+        )
+
+        presenter.view = view
+        interactor.presenter = presenter
+
+        // when
+
+        var loadedModel: DAppBrowserModel?
+
+        let loadingExpectation = XCTestExpectation()
+
+        stub(view) { stub in
+            when(stub).didReceive(viewModel: any()).then { viewModel in
+                loadedModel = viewModel
+
+                loadingExpectation.fulfill()
+            }
+        }
+
+        presenter.setup()
+
+        // then
+
+        wait(for: [loadingExpectation], timeout: 10)
+
+        XCTAssertEqual(loadedModel?.url, dAppURL)
+
+        if (interactor.state as? DAppBrowserWaitingAuthState) == nil {
+            XCTFail("Waiting auth state expected after setup")
+        }
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {}
 }
