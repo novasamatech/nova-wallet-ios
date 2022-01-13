40f222660567abeadeb5836306b07162e820417c
ERussel <emkil.russel@gmail.com>
2022-01-13 23:03:01 +0500

    base search implementation

--- novawallet/Modules/DApp/DAppSearch/DAppSearchPresenter.swift
+++ novawallet/Modules/DApp/DAppSearch/DAppSearchPresenter.swift
@@ -5,34 +5,70 @@ final class DAppSearchPresenter {
     let wireframe: DAppSearchWireframeProtocol
     let interactor: DAppSearchInteractorInputProtocol
 
-    let initialQuery: String?
+    private var dAppList: DAppList?
+
+    private(set) var query: String?
 
     weak var delegate: DAppSearchDelegate?
+
+    let viewModelFactory: DAppListViewModelFactoryProtocol
 
     init(
         interactor: DAppSearchInteractorInputProtocol,
         wireframe: DAppSearchWireframeProtocol,
+        viewModelFactory: DAppListViewModelFactoryProtocol,
         initialQuery: String?,
         delegate: DAppSearchDelegate
     ) {
         self.interactor = interactor
         self.wireframe = wireframe
-        self.initialQuery = initialQuery
+        self.viewModelFactory = viewModelFactory
+        query = initialQuery
         self.delegate = delegate
+    }
+
+    private func provideViewModel() {
+        if let dAppList = dAppList {
+            let viewModels = viewModelFactory.createDAppsFromQuery(query, dAppList: dAppList)
+            view?.didReceiveDApp(viewModels: viewModels)
+        } else {
+            view?.didReceiveDApp(viewModels: [])
+        }
     }
 }
 
 extension DAppSearchPresenter: DAppSearchPresenterProtocol {
     func setup() {
-        if let initialQuery = initialQuery {
-            view?.didReceive(initialQuery: initialQuery)
+        if let query = query {
+            view?.didReceive(initialQuery: query)
         }
+
+        interactor.setup()
     }
 
-    func activateSearch(for input: String) {
-        delegate?.didCompleteDAppSearchQuery(input)
+    func updateSearch(query: String) {
+        self.query = query
+
+        provideViewModel()
+    }
+
+    func selectDApp(viewModel _: DAppViewModel) {}
+
+    func selectSearchQuery() {
+        delegate?.didCompleteDAppSearchQuery(query ?? "")
         wireframe.close(from: view)
     }
 }
 
-extension DAppSearchPresenter: DAppSearchInteractorOutputProtocol {}
+extension DAppSearchPresenter: DAppSearchInteractorOutputProtocol {
+    func didReceive(dAppsResult: Result<DAppList?, Error>) {
+        switch dAppsResult {
+        case let .success(list):
+            dAppList = list
+
+            provideViewModel()
+        case .failure:
+            break
+        }
+    }
+}
---
+++ novawallet/Modules/DApp/DAppSearch/View/DAppSearchDAppTableViewCell.swift
@@ -0,0 +1,99 @@
+import UIKit
+
+final class DAppSearchDAppTableViewCell: UITableViewCell {
+    private enum Constants {
+        static let iconInsets = UIEdgeInsets(top: 8.0, left: 8.0, bottom: 8.0, right: 8.0)
+        static let iconSize = CGSize(width: 36.0, height: 36.0)
+
+        static var preferredIconViewSize: CGSize {
+            CGSize(
+                width: iconInsets.left + iconSize.width + iconInsets.right,
+                height: iconInsets.top + iconSize.height + iconInsets.bottom
+            )
+        }
+    }
+
+    let iconImageView: DAppIconView = {
+        let view = DAppIconView()
+        view.contentInsets = Constants.iconInsets
+        view.backgroundView.cornerRadius = 12.0
+        view.backgroundView.strokeWidth = 0.5
+        view.backgroundView.strokeColor = R.color.colorWhite16()!
+        view.backgroundView.highlightedStrokeColor = R.color.colorWhite16()!
+        view.backgroundView.fillColor = R.color.colorWhite8()!
+        view.backgroundView.highlightedFillColor = R.color.colorWhite8()!
+        return view
+    }()
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.colorWhite()
+        label.font = .regularSubheadline
+        return label
+    }()
+
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.colorTransparentText()
+        label.font = .caption1
+        return label
+    }()
+
+    let accessoryImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = R.image.iconSmallArrow()?.withRenderingMode(.alwaysTemplate)
+        imageView.tintColor = R.color.colorWhite32()!
+        return imageView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        let selectedView = UIView()
+        selectedView.backgroundColor = R.color.colorAccentSelected()
+        selectedBackgroundView = selectedView
+
+        setupLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func bind(viewModel: DAppViewModel) {
+        iconImageView.bind(viewModel: viewModel.icon, size: Constants.iconSize)
+
+        titleLabel.text = viewModel.name
+        subtitleLabel.text = viewModel.details
+    }
+
+    private func setupLayout() {
+        contentView.addSubview(iconImageView)
+        iconImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.centerY.equalToSuperview()
+            make.size.equalTo(Constants.preferredIconViewSize)
+        }
+
+        contentView.addSubview(accessoryImageView)
+        accessoryImageView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(16.0)
+            make.centerY.equalToSuperview()
+        }
+
+        contentView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(iconImageView.snp.top).offset(4.0)
+            make.leading.equalTo(iconImageView.snp.trailing).offset(12.0)
+            make.trailing.lessThanOrEqualTo(accessoryImageView.snp.leading).offset(-4.0)
+        }
+
+        contentView.addSubview(subtitleLabel)
+        subtitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4.0)
+            make.leading.equalTo(iconImageView.snp.trailing).offset(12.0)
+            make.trailing.lessThanOrEqualTo(accessoryImageView.snp.leading).offset(-4.0)
+        }
+    }
+}
