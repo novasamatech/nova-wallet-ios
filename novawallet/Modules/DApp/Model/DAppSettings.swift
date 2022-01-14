74368ff121bb4968da1adfe8f9e132ddc9029320
ERussel <emkil.russel@gmail.com>
2022-01-14 23:03:27 +0500

    persist dapp auth decision

--- novawallet/Modules/DApp/Model/DAppSettings.swift
+++ novawallet/Modules/DApp/Model/DAppSettings.swift
@@ -2,6 +2,7 @@ import Foundation
 import RobinHood
 
 struct DAppSettings: Identifiable {
+    // normaly it is dapp url's host
     let identifier: String
     let allowed: Bool
     let favorite: Bool
