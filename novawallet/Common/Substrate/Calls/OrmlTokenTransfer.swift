2ca872479d266517efb3e4bcda9544407752c655
ERussel <emkil.russel@gmail.com>
2022-01-27 10:22:40 +0300

    fix orml transfers

--- novawallet/Common/Substrate/Calls/OrmlTokenTransfer.swift
+++ novawallet/Common/Substrate/Calls/OrmlTokenTransfer.swift
@@ -3,6 +3,12 @@ import SubstrateSdk
 import BigInt
 
 struct OrmlTokenTransfer: Codable {
+    enum CodingKeys: String, CodingKey {
+        case dest
+        case currencyId = "currency_id"
+        case amount
+    }
+
     let dest: MultiAddress
     let currencyId: JSON
     @StringCodable var amount: BigUInt
