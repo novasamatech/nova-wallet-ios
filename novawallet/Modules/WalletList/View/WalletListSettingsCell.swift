744e369b93e106ac1d48ae82b5a2773b4bf5b3da
ERussel <emkil.russel@gmail.com>
2022-02-01 12:15:47 +0300

    add grouping base implementation

---
+++ novawallet/Modules/WalletList/View/WalletListSettingsCell.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+final class WalletListSettingsCell: UICollectionViewCell {
+    let titleLabel: UILabel = {
+        let view = UILabel()
+        view.font = .semiBoldTitle3
+        view.textColor = R.color.colorWhite()
+        return view
+    }()
+
+    let actionButton: TriangularedBlurButton = {
+        let button = TriangularedBlurButton()
+        button.imageWithTitleView?.iconImage = R.image.iconAssetsSettings()?
+            .withRenderingMode(.alwaysTemplate)
+            .tinted(with: R.color.colorWhite80()!)
+        button.contentInsets = UIEdgeInsets(top: 6, left: 12, bottom: 6, right: 12)
+        button.changesContentOpacityWhenHighlighted = true
+        button.triangularedBlurView?.overlayView.highlightedFillColor =
+            R.color.colorAccentSelected()!
+        return button
+    }()
+
+    var locale = Locale.current {
+        didSet {
+            if oldValue != locale {
+                setupLocalization()
+            }
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupLayout()
+        setupLocalization()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLocalization() {
+        titleLabel.text = R.string.localizable.tabbarAssetsTitle(
+            preferredLanguages: locale.rLanguages
+        )
+    }
+
+    private func setupLayout() {
+        addSubview(actionButton)
+
+        actionButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
+            make.centerY.equalToSuperview()
+        }
+
+        addSubview(titleLabel)
+
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(UIConstants.horizontalInset)
+            make.trailing.lessThanOrEqualTo(actionButton.snp.leading).offset(-8.0)
+            make.centerY.equalToSuperview()
+        }
+    }
+}
--- novawallet/Modules/WalletList/View/WalletListTokenCell.swift
+++ novawallet/Modules/WalletList/View/WalletListTokenCell.swift
@@ -1,58 +1,36 @@
 import UIKit
 
-final class WalletListAssetCell: UITableViewCell {
-    let backgroundBlurView: TriangularedBlurView = {
-        let view = TriangularedBlurView()
-        view.blurStyle = .dark
-        view.overlayView.fillColor = .clear
-        view.overlayView.highlightedFillColor = .clear
-        view.cornerCut = .allCorners
-        return view
-    }()
-
+final class WalletListTokenCell: UICollectionViewCell {
     let iconView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .center
         return view
     }()
 
-    let separatorView: UIView = {
-        let view = UIView()
-        view.backgroundColor = R.color.colorBlurSeparator()
-        return view
-    }()
-
-    let networkLabel: UILabel = {
-        let label = UILabel()
-        label.font = .capsTitle
-        label.textColor = R.color.colorTransparentText()
-        return label
-    }()
-
     let assetLabel: UILabel = {
         let label = UILabel()
-        label.font = .h3Title
+        label.font = .semiBoldBody
         label.textColor = R.color.colorWhite()
         return label
     }()
 
     let priceLabel: UILabel = {
         let label = UILabel()
-        label.font = .p1Paragraph
+        label.font = .regularFootnote
         label.textColor = R.color.colorTransparentText()
         return label
     }()
 
     let priceChangeLabel: UILabel = {
         let label = UILabel()
-        label.font = .p1Paragraph
+        label.font = .regularFootnote
         label.textColor = R.color.colorTransparentText()
         return label
     }()
 
     let balanceLabel: UILabel = {
         let label = UILabel()
-        label.font = .h3Title
+        label.font = .semiBoldBody
         label.textColor = R.color.colorWhite()
         label.textAlignment = .right
         return label
@@ -60,7 +38,7 @@ final class WalletListAssetCell: UITableViewCell {
 
     let balanceValueLabel: UILabel = {
         let label = UILabel()
-        label.font = .p1Paragraph
+        label.font = .regularFootnote
         label.textColor = R.color.colorTransparentText()
         label.textAlignment = .right
         return label
@@ -68,19 +46,10 @@ final class WalletListAssetCell: UITableViewCell {
 
     private var viewModel: WalletListViewModel?
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-        backgroundColor = .clear
-        selectedBackgroundView = UIView()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
 
         setupLayout()
-    }
-
-    override func prepareForReuse() {
-        super.prepareForReuse()
-
-        viewModel?.icon?.cancel(on: iconView)
     }
 
     @available(*, unavailable)
@@ -94,9 +63,8 @@ final class WalletListAssetCell: UITableViewCell {
         self.viewModel = viewModel
 
         iconView.image = nil
-        viewModel.icon?.loadImage(on: iconView, targetSize: CGSize(width: 48.0, height: 48.0), animated: true)
+        viewModel.icon?.loadImage(on: iconView, targetSize: CGSize(width: 28.0, height: 28.0), animated: true)
 
-        networkLabel.text = viewModel.networkName
         assetLabel.text = viewModel.tokenName
 
         applyPrice(viewModel.price)
@@ -152,62 +120,43 @@ final class WalletListAssetCell: UITableViewCell {
     }
 
     private func setupLayout() {
-        contentView.addSubview(backgroundBlurView)
-        backgroundBlurView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
-            make.top.equalToSuperview()
-            make.bottom.equalToSuperview().inset(8.0)
-        }
-
         contentView.addSubview(iconView)
         iconView.snp.makeConstraints { make in
-            make.leading.top.bottom.equalTo(backgroundBlurView)
-            make.width.equalTo(63.0)
-        }
-
-        contentView.addSubview(separatorView)
-        separatorView.snp.makeConstraints { make in
-            make.top.bottom.equalTo(backgroundBlurView).inset(8.0)
-            make.leading.equalTo(iconView.snp.trailing)
-            make.width.equalTo(1 / UIScreen.main.scale)
-        }
-
-        contentView.addSubview(networkLabel)
-        networkLabel.snp.makeConstraints { make in
-            make.leading.equalTo(separatorView).offset(8.0)
-            make.top.equalTo(backgroundBlurView).offset(8.0)
+            make.leading.equalToSuperview().inset(28.0)
+            make.top.bottom.equalToSuperview().inset(8.0)
+            make.size.equalTo(40.0)
         }
 
         contentView.addSubview(assetLabel)
         assetLabel.snp.makeConstraints { make in
-            make.leading.equalTo(separatorView).offset(8.0)
-            make.top.equalTo(backgroundBlurView).offset(26.0)
+            make.leading.equalTo(iconView.snp.trailing).offset(12.0)
+            make.top.equalToSuperview().inset(8.0)
         }
 
         contentView.addSubview(priceLabel)
         priceLabel.snp.makeConstraints { make in
-            make.leading.equalTo(separatorView).offset(8.0)
-            make.bottom.equalTo(backgroundBlurView).offset(-10.0)
+            make.leading.equalTo(iconView.snp.trailing).offset(12.0)
+            make.bottom.equalToSuperview().inset(8.0)
         }
 
         contentView.addSubview(priceChangeLabel)
         priceChangeLabel.snp.makeConstraints { make in
             make.leading.equalTo(priceLabel.snp.trailing).offset(4.0)
-            make.bottom.equalTo(backgroundBlurView).offset(-10.0)
+            make.bottom.equalToSuperview().inset(8.0)
         }
 
         contentView.addSubview(balanceLabel)
         balanceLabel.snp.makeConstraints { make in
-            make.trailing.equalTo(backgroundBlurView).offset(-12.0)
+            make.trailing.equalToSuperview().inset(32.0)
             make.leading.greaterThanOrEqualTo(assetLabel.snp.trailing).offset(4.0)
-            make.top.equalTo(backgroundBlurView).offset(26.0)
+            make.top.equalToSuperview().inset(8.0)
         }
 
         contentView.addSubview(balanceValueLabel)
         balanceValueLabel.snp.makeConstraints { make in
-            make.trailing.equalTo(backgroundBlurView).offset(-12.0)
+            make.trailing.equalToSuperview().inset(32.0)
             make.leading.greaterThanOrEqualTo(priceChangeLabel.snp.trailing).offset(4.0)
-            make.bottom.equalTo(backgroundBlurView).offset(-10.0)
+            make.bottom.equalToSuperview().inset(8.0)
         }
     }
 }
