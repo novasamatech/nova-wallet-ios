8c4630754dda7c16940028d4622fb779ee98a542
ERussel <emkil.russel@gmail.com>
2022-02-03 12:51:56 +0300

    add tests

--- novawalletTests/Modules/AssetsManage/AssetsManageTests.swift
+++ novawalletTests/Modules/AssetsManage/AssetsManageTests.swift
@@ -1,16 +1,78 @@
 import XCTest
+@testable import novawallet
+import SoraKeystore
+import Cuckoo
 
 class AssetsManageTests: XCTestCase {
+    func testSetupAndSave() {
+        // given
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+        let view = MockAssetsManageViewProtocol()
+        let wireframe = MockAssetsManageWireframeProtocol()
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        let settingsManager = InMemorySettingsManager()
+        settingsManager.hidesZeroBalances = true
 
-    func testExample() {
-        XCTFail("Did you forget to add tests?")   
+        let eventCenter = MockEventCenterProtocol()
+
+        let interactor = AssetsManageInteractor(settingsManager: settingsManager, eventCenter: eventCenter)
+        let presenter = AssetsManagePresenter(interactor: interactor, wireframe: wireframe)
+
+        presenter.view = view
+        interactor.presenter = presenter
+
+        // when
+
+        var receivedViewModel: AssetsManageViewModel?
+        let expectedViewModel = AssetsManageViewModel(hideZeroBalances: true, canApply: false)
+
+        let setupCompletion = XCTestExpectation()
+
+        stub(view) { stub in
+            stub.didReceive(viewModel: any()).then { viewModel in
+                receivedViewModel = viewModel
+
+                setupCompletion.fulfill()
+            }
+        }
+
+        presenter.setup()
+
+        // then
+
+        wait(for: [setupCompletion], timeout: 1.0)
+
+        XCTAssertEqual(receivedViewModel, expectedViewModel)
+
+        // when
+
+        let closeExpectation = XCTestExpectation()
+
+        stub(view) { stub in
+            stub.didReceive(viewModel: any()).thenDoNothing()
+        }
+
+        stub(wireframe) { stub in
+            stub.close(view: any()).then { _ in
+                closeExpectation.fulfill()
+            }
+        }
+
+        let notificationExpectation = XCTestExpectation()
+
+        stub(eventCenter) { stub in
+            stub.notify(with: any()).then { event in
+                notificationExpectation.fulfill()
+            }
+        }
+
+        presenter.changeHideZeroBalances(value: false)
+        presenter.apply()
+
+        // then
+
+        wait(for: [closeExpectation, notificationExpectation], timeout: 1.0)
+
+        XCTAssertEqual(settingsManager.hidesZeroBalances, false)
     }
 }
