2b87b91ef6e70557332e8b601373b5f6cc11d7d7
ERussel <emkil.russel@gmail.com>
2022-02-11 11:29:05 +0300

    remove dapp catalog

--- novawallet/Modules/DApp/DAppSearch/DAppSearchPresenter.swift
+++ novawallet/Modules/DApp/DAppSearch/DAppSearchPresenter.swift
@@ -3,41 +3,23 @@ import Foundation
 final class DAppSearchPresenter {
     weak var view: DAppSearchViewProtocol?
     let wireframe: DAppSearchWireframeProtocol
-    let interactor: DAppSearchInteractorInputProtocol
-
-    private var dAppList: DAppList?
 
     private(set) var query: String?
 
     weak var delegate: DAppSearchDelegate?
 
-    let viewModelFactory: DAppListViewModelFactoryProtocol
-
     let logger: LoggerProtocol?
 
     init(
-        interactor: DAppSearchInteractorInputProtocol,
         wireframe: DAppSearchWireframeProtocol,
-        viewModelFactory: DAppListViewModelFactoryProtocol,
         initialQuery: String?,
         delegate: DAppSearchDelegate,
         logger: LoggerProtocol? = nil
     ) {
-        self.interactor = interactor
         self.wireframe = wireframe
-        self.viewModelFactory = viewModelFactory
         query = initialQuery
         self.delegate = delegate
         self.logger = logger
-    }
-
-    private func provideViewModel() {
-        if let dAppList = dAppList {
-            let viewModels = viewModelFactory.createDAppsFromQuery(query, dAppList: dAppList)
-            view?.didReceiveDApp(viewModels: viewModels)
-        } else {
-            view?.didReceiveDApp(viewModels: [])
-        }
     }
 }
 
@@ -46,24 +28,10 @@ extension DAppSearchPresenter: DAppSearchPresenterProtocol {
         if let query = query {
             view?.didReceive(initialQuery: query)
         }
-
-        interactor.setup()
     }
 
     func updateSearch(query: String) {
         self.query = query
-
-        provideViewModel()
-    }
-
-    func selectDApp(viewModel: DAppViewModel) {
-        guard let dAppList = dAppList else {
-            return
-        }
-
-        let dApp = dAppList.dApps[viewModel.index]
-        delegate?.didCompleteDAppSearchResult(.dApp(model: dApp))
-        wireframe.close(from: view)
     }
 
     func selectSearchQuery() {
@@ -73,18 +41,5 @@ extension DAppSearchPresenter: DAppSearchPresenterProtocol {
 
     func cancel() {
         wireframe.close(from: view)
-    }
-}
-
-extension DAppSearchPresenter: DAppSearchInteractorOutputProtocol {
-    func didReceive(dAppsResult: Result<DAppList?, Error>) {
-        switch dAppsResult {
-        case let .success(list):
-            dAppList = list
-
-            provideViewModel()
-        case let .failure(error):
-            logger?.error("Fatal error: \(error)")
-        }
     }
 }
