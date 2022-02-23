9b8cb66a64190c2141676b940a89e22cdd118cce
ERussel <emkil.russel@gmail.com>
2022-02-24 01:13:50 +0300

    fix url detection

--- novawallet/Common/Extension/Foundation/NSPredicate+Validation.swift
+++ novawallet/Common/Extension/Foundation/NSPredicate+Validation.swift
@@ -95,7 +95,7 @@ extension NSPredicate {
 
     static var urlPredicate: NSPredicate {
         let urlRegEx = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?" +
-            "[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}" +
+            "[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,16}" +
             "(/[-\\w@\\+\\.~#\\?&/=%]*)?$"
         return NSPredicate(format: "SELF MATCHES %@", urlRegEx)
     }
