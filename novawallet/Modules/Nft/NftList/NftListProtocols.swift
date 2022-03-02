e4a075044cd5f85baa5d3b75a4750e1ad2dc9b00
ERussel <emkil.russel@gmail.com>
2022-03-02 11:05:24 +0300

    add refresh option

--- novawallet/Modules/Nft/NftList/NftListProtocols.swift
+++ novawallet/Modules/Nft/NftList/NftListProtocols.swift
@@ -2,10 +2,12 @@ import RobinHood
 
 protocol NftListViewProtocol: ControllerBackedProtocol {
     func didReceive(changes: [ListDifference<NftListViewModel>])
+    func didCompleteRefreshing()
 }
 
 protocol NftListPresenterProtocol: AnyObject {
     func setup()
+    func refresh()
 
     func numberOfItems() -> Int
     func nft(at index: Int) -> NftListViewModel
