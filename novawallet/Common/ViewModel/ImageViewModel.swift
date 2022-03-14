7e86522d8d652ff49e71275606c47208d736ca71
ERussel <emkil.russel@gmail.com>
2022-03-14 08:48:42 +0500

    add option to tint image view model

--- novawallet/Common/ViewModel/ImageViewModel.swift
+++ novawallet/Common/ViewModel/ImageViewModel.swift
@@ -1,12 +1,39 @@
 import UIKit
 
+struct ImageViewModelSettings {
+    let targetSize: CGSize
+    let cornerRadius: CGFloat?
+    let tintColor: UIColor?
+}
+
 protocol ImageViewModelProtocol {
-    func loadImage(on imageView: UIImageView, targetSize: CGSize, cornerRadius: CGFloat?, animated: Bool)
+    func loadImage(on imageView: UIImageView, settings: ImageViewModelSettings, animated: Bool)
     func cancel(on imageView: UIImageView)
 }
 
 extension ImageViewModelProtocol {
     func loadImage(on imageView: UIImageView, targetSize: CGSize, animated: Bool) {
-        loadImage(on: imageView, targetSize: targetSize, cornerRadius: nil, animated: animated)
+        let settings = ImageViewModelSettings(
+            targetSize: targetSize,
+            cornerRadius: nil,
+            tintColor: nil
+        )
+
+        loadImage(on: imageView, settings: settings, animated: animated)
+    }
+
+    func loadImage(
+        on imageView: UIImageView,
+        targetSize: CGSize,
+        cornerRadius: CGFloat,
+        animated: Bool
+    ) {
+        let settings = ImageViewModelSettings(
+            targetSize: targetSize,
+            cornerRadius: cornerRadius,
+            tintColor: nil
+        )
+
+        loadImage(on: imageView, settings: settings, animated: animated)
     }
 }
