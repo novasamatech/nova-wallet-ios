8b3b001648bad3941444d20744693a0edcdcb1d3
ERussel <emkil.russel@gmail.com>
2022-03-29 22:20:51 +0500

    revert history background

--- novawallet/Modules/Wallet/History/View/WalletHistoryBackgroundView.swift
+++ novawallet/Modules/Wallet/History/View/WalletHistoryBackgroundView.swift
@@ -3,8 +3,6 @@ import CommonWallet
 
 final class WalletHistoryBackgroundView: TriangularedBlurView {
     let minimizedSideLength: CGFloat = 12.0
-    let minBackgroundAlpha: CGFloat = 0.0
-    let maxBackgroundAlpha: CGFloat = 0.8
 }
 
 extension WalletHistoryBackgroundView: HistoryBackgroundViewProtocol {
@@ -12,9 +10,5 @@ extension WalletHistoryBackgroundView: HistoryBackgroundViewProtocol {
 
     func applyFullscreen(progress: CGFloat) {
         sideLength = minimizedSideLength * progress
-
-        let alpha = minBackgroundAlpha + (maxBackgroundAlpha - minBackgroundAlpha) * (1.0 - progress)
-        let color = R.color.colorBlack()!.withAlphaComponent(alpha)
-        overlayView.fillColor = color
     }
 }
