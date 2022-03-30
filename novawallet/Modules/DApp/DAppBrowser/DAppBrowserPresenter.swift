9ba508f17f8d44d4e2a9a60941151befb538b059
ERussel <emkil.russel@gmail.com>
2022-03-30 15:16:03 +0500

    add detection on url change

--- novawallet/Modules/DApp/DAppBrowser/DAppBrowserPresenter.swift
+++ novawallet/Modules/DApp/DAppBrowser/DAppBrowserPresenter.swift
@@ -8,6 +8,8 @@ final class DAppBrowserPresenter {
     let logger: LoggerProtocol?
     let localizationManager: LocalizationManager
 
+    private var currentHost: String?
+
     init(
         interactor: DAppBrowserInteractorInputProtocol,
         wireframe: DAppBrowserWireframeProtocol,
@@ -46,6 +48,16 @@ final class DAppBrowserPresenter {
 extension DAppBrowserPresenter: DAppBrowserPresenterProtocol {
     func setup() {
         interactor.setup()
+    }
+
+    func processNew(url: URL) {
+        guard let newHost = url.host, newHost != currentHost else {
+            return
+        }
+
+        currentHost = newHost
+
+        interactor.process(host: newHost)
     }
 
     func process(message: Any, host: String, transport name: String) {
@@ -107,6 +119,10 @@ extension DAppBrowserPresenter: DAppBrowserInteractorOutputProtocol {
 
     func didReceiveAuth(request: DAppAuthRequest) {
         wireframe.presentAuth(from: view, request: request, delegate: self)
+    }
+
+    func didDetectPhishing(host: String) {
+        logger?.info("Did detect phishing host: \(host)")
     }
 }
 
