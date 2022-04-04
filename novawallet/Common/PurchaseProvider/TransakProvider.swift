4377af9e4098509a5ee54d7763a940864e717aad
ERussel <emkil.russel@gmail.com>
2022-04-04 10:59:27 +0500

    add transak provider

---
+++ novawallet/Common/PurchaseProvider/TransakProvider.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+final class TransakProvider: PurchaseProviderProtocol {
+    #if F_RELEASE
+        static let pubToken = ""
+        static let baseUrlString = "https://global.transak.com"
+    #else
+        static let pubToken = ""
+        static let baseUrlString = "https://staging-global.transak.com"
+    #endif
+
+    private var callbackUrl: URL?
+
+    func with(callbackUrl: URL) -> Self {
+        self.callbackUrl = callbackUrl
+        return self
+    }
+
+    func buildPurchaseActions(for chainAsset: ChainAsset, accountId: AccountId) -> [PurchaseAction] {
+        guard let address = try? accountId.toAddress(using: chainAsset.chain.chainFormat) else {
+            return []
+        }
+
+        let token = chainAsset.asset.symbol
+        let network = chainAsset.chain.name.lowercased()
+
+        guard let url = buildURLForToken(token, network: network, address: address) else {
+            return []
+        }
+
+        let action = PurchaseAction(title: "Transak", url: url, icon: R.image.iconRamp()!)
+
+        return [action]
+    }
+
+    private func buildURLForToken(_ token: String, network: String, address: String) -> URL? {
+        var components = URLComponents(string: Self.baseUrlString)
+
+        var queryItems = [
+            URLQueryItem(name: "api", value: Self.pubToken),
+            URLQueryItem(name: "network", value: network),
+            URLQueryItem(name: "cryptoCurrencyCode", value: token),
+            URLQueryItem(name: "walletAddress", value: address),
+            URLQueryItem(name: "disableWalletAddressForm", value: "true")
+        ]
+
+        if let callbackUrl = callbackUrl?.absoluteString {
+            queryItems.append(URLQueryItem(name: "redirectURL", value: callbackUrl))
+        }
+
+        components?.queryItems = queryItems
+
+        return components?.url
+    }
+}
