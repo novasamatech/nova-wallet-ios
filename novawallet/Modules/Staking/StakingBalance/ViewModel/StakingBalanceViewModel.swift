7df684567c72a6e8dd5203b679daae00953fbb66
ERussel <emkil.russel@gmail.com>
2022-04-07 16:17:25 +0500

    remove not needed files

--- novawallet/Modules/Staking/StakingBalance/ViewModel/StakingBalanceViewModel.swift
+++
@@ -1,7 +0,0 @@
-typealias UnbondingItemViewModel = StakingRewardHistoryCellViewModel
-
-struct StakingBalanceViewModel {
-    let widgetViewModel: StakingBalanceWidgetViewModel
-    let actionsViewModel: StakingBalanceActionsWidgetViewModel
-    let unbondingViewModel: StakingBalanceUnbondingWidgetViewModel
-}
