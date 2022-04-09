1fa400ca3d1fc4af42e43ab98cbfcd6630a95830
ERussel <emkil.russel@gmail.com>
2022-04-09 16:46:41 +0500

    remove unused files

--- novawallet/Common/Extension/Model/CryptoType+ViewModel.swift
+++ novawallet/Common/Extension/Model/CryptoType+ViewModel.swift
@@ -1,32 +1,6 @@
 import Foundation
 
-// TODO: remove after refactoring
-extension CryptoType {
-    func titleForLocale(_ locale: Locale) -> String {
-        switch self {
-        case .sr25519:
-            return R.string.localizable.sr25519SelectionTitle(preferredLanguages: locale.rLanguages)
-        case .ed25519:
-            return R.string.localizable.ed25519SelectionTitle(preferredLanguages: locale.rLanguages)
-        case .ecdsa:
-            return R.string.localizable.ecdsaSelectionTitle(preferredLanguages: locale.rLanguages)
-        }
-    }
-
-    func subtitleForLocale(_ locale: Locale) -> String {
-        switch self {
-        case .sr25519:
-            return R.string.localizable.sr25519SelectionSubtitle(preferredLanguages: locale.rLanguages)
-        case .ed25519:
-            return R.string.localizable.ed25519SelectionSubtitle(preferredLanguages: locale.rLanguages)
-        case .ecdsa:
-            return R.string.localizable.ecdsaSelectionSubtitle(preferredLanguages: locale.rLanguages)
-        }
-    }
-}
-
 extension MultiassetCryptoType {
-    // TODO: Check with UI
     func titleForLocale(_ locale: Locale) -> String {
         switch utilsType {
         case .sr25519:
@@ -38,7 +12,6 @@ extension MultiassetCryptoType {
         }
     }
 
-    // TODO: Check with UI
     func subtitleForLocale(_ locale: Locale) -> String {
         switch utilsType {
         case .sr25519:
