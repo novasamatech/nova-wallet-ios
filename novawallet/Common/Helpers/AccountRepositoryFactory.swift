bd1902d01be140fc4da79498ad44f7fad422876e
ERussel <emkil.russel@gmail.com>
2022-04-09 16:07:09 +0500

    fix account repository

--- novawallet/Common/Helpers/AccountRepositoryFactory.swift
+++ novawallet/Common/Helpers/AccountRepositoryFactory.swift
@@ -3,14 +3,6 @@ import IrohaCrypto
 import RobinHood
 
 protocol AccountRepositoryFactoryProtocol {
-    // TODO: remove
-    @available(*, deprecated, message: "Use createMetaAccountRepository(for filter:, sortDescriptors:) instead")
-    func createManagedRepository() -> AnyDataProviderRepository<ManagedAccountItem>
-    func createRepository() -> AnyDataProviderRepository<AccountItem>
-
-    // TODO: remove
-    func createAccountRepository(for networkType: SNAddressType) -> AnyDataProviderRepository<AccountItem>
-
     func createMetaAccountRepository(
         for filter: NSPredicate?,
         sortDescriptors: [NSSortDescriptor]
@@ -40,21 +32,6 @@ final class AccountRepositoryFactory: AccountRepositoryFactoryProtocol {
         self.storageFacade = storageFacade
     }
 
-    func createManagedRepository() -> AnyDataProviderRepository<ManagedAccountItem> {
-        Self.createManagedRepository(for: storageFacade)
-    }
-
-    func createRepository() -> AnyDataProviderRepository<AccountItem> {
-        Self.createRepository(for: storageFacade)
-    }
-
-    // TODO: remove
-    func createAccountRepository(
-        for _: SNAddressType
-    ) -> AnyDataProviderRepository<AccountItem> {
-        Self.createRepository(for: storageFacade)
-    }
-
     func createMetaAccountRepository(
         for filter: NSPredicate?,
         sortDescriptors: [NSSortDescriptor]
@@ -81,26 +58,6 @@ final class AccountRepositoryFactory: AccountRepositoryFactoryProtocol {
             sortDescriptors: sortDescriptors,
             mapper: AnyCoreDataMapper(mapper)
         )
-
-        return AnyDataProviderRepository(repository)
-    }
-}
-
-extension AccountRepositoryFactory {
-    static func createManagedRepository(
-        for storageFacade: StorageFacadeProtocol = UserDataStorageFacade.shared
-    ) -> AnyDataProviderRepository<ManagedAccountItem> {
-        let mapper = ManagedAccountItemMapper()
-        let repository = storageFacade.createRepository(mapper: AnyCoreDataMapper(mapper))
-
-        return AnyDataProviderRepository(repository)
-    }
-
-    static func createRepository(
-        for storageFacade: StorageFacadeProtocol = UserDataStorageFacade.shared
-    ) -> AnyDataProviderRepository<AccountItem> {
-        let mapper = CodableCoreDataMapper<AccountItem, CDMetaAccount>()
-        let repository = storageFacade.createRepository(mapper: AnyCoreDataMapper(mapper))
 
         return AnyDataProviderRepository(repository)
     }
