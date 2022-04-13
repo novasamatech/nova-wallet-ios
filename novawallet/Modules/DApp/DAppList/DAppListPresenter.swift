77d3f5e6ce8b544e5004079ab281fef48672d7b4
ERussel <emkil.russel@gmail.com>
2022-04-13 08:48:41 +0500

    add favorite animation

--- novawallet/Modules/DApp/DAppList/DAppListPresenter.swift
+++ novawallet/Modules/DApp/DAppList/DAppListPresenter.swift
@@ -53,6 +53,7 @@ final class DAppListPresenter {
     private var favorites: [String: DAppFavorite] = [:]
     private var hasFavorites: Bool { !favorites.isEmpty }
     private var selectedCategory: CategoryIndex = .all
+    private var pendingFavoriteIdentifier: String?
 
     private lazy var iconGenerator = NovaIconGenerator()
 
@@ -169,6 +170,86 @@ final class DAppListPresenter {
             self?.interactor.removeFromFavorites(dAppIdentifier: identifier)
         }
     }
+
+    private func indexOfViewModelIdentifier(for pendingIdentifier: String) -> Int? {
+        selectedDApps.firstIndex { viewModel in
+            switch viewModel.identifier {
+            case let .index(value):
+                guard let dAppList = try? dAppsResult?.get() else {
+                    return false
+                }
+
+                return pendingIdentifier == dAppList.dApps[value].identifier
+            case let .key(value):
+                return pendingIdentifier == value
+            }
+        }
+    }
+
+    private func updateFavoriteState(
+        for pendingIdentifier: String,
+        change: DataProviderChange<DAppFavorite>
+    ) {
+        let maybeInitIndex = indexOfViewModelIdentifier(for: pendingIdentifier)
+
+        applyFavorite(changes: [change])
+        updateCategories()
+
+        let maybeFinalIndex = indexOfViewModelIdentifier(for: pendingIdentifier)
+
+        if let initIndex = maybeInitIndex, let finalIndex = maybeFinalIndex {
+            view?.didMoveDApp(from: initIndex, to: finalIndex)
+        } else if let initIndex = maybeInitIndex, hasFavorites {
+            view?.didRemoveDApp(at: initIndex)
+        } else {
+            updateState()
+        }
+    }
+
+    private func updatePendingFavorite(for changes: [DataProviderChange<DAppFavorite>]) -> Bool {
+        guard let pendingFavoriteIdentifier = pendingFavoriteIdentifier else {
+            return false
+        }
+
+        guard changes.count == 1, let change = changes.first else {
+            self.pendingFavoriteIdentifier = nil
+            return false
+        }
+
+        switch change {
+        case let .insert(newItem), let .update(newItem):
+            if newItem.identifier == pendingFavoriteIdentifier {
+                updateFavoriteState(for: pendingFavoriteIdentifier, change: change)
+
+                self.pendingFavoriteIdentifier = nil
+                return true
+            } else {
+                self.pendingFavoriteIdentifier = nil
+                return false
+            }
+        case let .delete(deletedIdentifier):
+            if deletedIdentifier == pendingFavoriteIdentifier {
+                updateFavoriteState(for: pendingFavoriteIdentifier, change: change)
+
+                self.pendingFavoriteIdentifier = nil
+                return true
+            } else {
+                self.pendingFavoriteIdentifier = nil
+                return false
+            }
+        }
+    }
+
+    private func applyFavorite(changes: [DataProviderChange<DAppFavorite>]) {
+        for change in changes {
+            switch change {
+            case let .insert(newItem), let .update(newItem):
+                favorites[newItem.identifier] = newItem
+            case let .delete(deletedIdentifier):
+                favorites[deletedIdentifier] = nil
+            }
+        }
+    }
 }
 
 extension DAppListPresenter: DAppListPresenterProtocol {
@@ -283,7 +364,9 @@ extension DAppListPresenter: DAppListPresenterProtocol {
         switch dAppViewModel.identifier {
         case let .index(value):
             let dApp = dAppList.dApps[value]
-            let identifier = dApp.url.absoluteString
+            let identifier = dApp.identifier
+
+            pendingFavoriteIdentifier = identifier
 
             if favorites[identifier] != nil {
                 askDAppRemoval(for: identifier, name: dAppViewModel.name)
@@ -293,6 +376,8 @@ extension DAppListPresenter: DAppListPresenterProtocol {
 
         case let .key(value):
             if let dapp = favorites[value] {
+                pendingFavoriteIdentifier = dapp.identifier
+
                 let name = viewModelFactory.createFavoriteDAppName(from: dapp)
                 askDAppRemoval(for: dapp.identifier, name: name)
             }
@@ -333,17 +418,11 @@ extension DAppListPresenter: DAppListInteractorOutputProtocol {
     }
 
     func didReceiveFavoriteDapp(changes: [DataProviderChange<DAppFavorite>]) {
-        for change in changes {
-            switch change {
-            case let .insert(newItem), let .update(newItem):
-                favorites[newItem.identifier] = newItem
-            case let .delete(deletedIdentifier):
-                favorites[deletedIdentifier] = nil
-            }
+        if !updatePendingFavorite(for: changes) {
+            applyFavorite(changes: changes)
+            updateCategories()
+            updateState()
         }
-
-        updateCategories()
-        updateState()
     }
 }
 
