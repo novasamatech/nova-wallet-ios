3ef6b9265ee43e18879096decf718719737b6883
ERussel <emkil.russel@gmail.com>
2022-04-14 23:14:31 +0500

    add base layout

--- novawallet/Modules/DApp/DAppAddFavorite/DAppAddFavoriteViewFactory.swift
+++ novawallet/Modules/DApp/DAppAddFavorite/DAppAddFavoriteViewFactory.swift
@@ -1,13 +1,38 @@
 import Foundation
+import SoraFoundation
 
 struct DAppAddFavoriteViewFactory {
-    static func createView() -> DAppAddFavoriteViewProtocol? {
-        let interactor = DAppAddFavoriteInteractor()
+    static func createView(for browserPage: DAppBrowserPage) -> DAppAddFavoriteViewProtocol? {
+        let repository = AccountRepositoryFactory(
+            storageFacade: UserDataStorageFacade.shared
+        ).createFavoriteDAppsRepository()
+
+        let dAppsUrl = ApplicationConfig.shared.dAppsListURL
+        let dAppProvider: AnySingleValueProvider<DAppList> = JsonDataProviderFactory.shared.getJson(
+            for: dAppsUrl
+        )
+
+        let interactor = DAppAddFavoriteInteractor(
+            browserPage: browserPage,
+            dAppProvider: dAppProvider,
+            dAppsFavoriteRepository: repository,
+            operationQueue: OperationManagerFacade.sharedDefaultQueue
+        )
+
         let wireframe = DAppAddFavoriteWireframe()
 
-        let presenter = DAppAddFavoritePresenter(interactor: interactor, wireframe: wireframe)
+        let localizationManager = LocalizationManager.shared
 
-        let view = DAppAddFavoriteViewController(presenter: presenter)
+        let presenter = DAppAddFavoritePresenter(
+            interactor: interactor,
+            wireframe: wireframe,
+            localizationManager: localizationManager
+        )
+
+        let view = DAppAddFavoriteViewController(
+            presenter: presenter,
+            localizationManager: localizationManager
+        )
 
         presenter.view = view
         interactor.presenter = presenter
