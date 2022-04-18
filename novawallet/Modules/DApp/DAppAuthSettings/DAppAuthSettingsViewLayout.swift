265b237319e16d2e6749f768e782a685e56c075a
ERussel <emkil.russel@gmail.com>
2022-04-18 06:25:07 +0300

    add authorized ui

--- novawallet/Modules/DApp/DAppAuthSettings/DAppAuthSettingsViewLayout.swift
+++ novawallet/Modules/DApp/DAppAuthSettings/DAppAuthSettingsViewLayout.swift
@@ -1,12 +1,32 @@
 import UIKit
 
 final class DAppAuthSettingsViewLayout: UIView {
+    let tableView: UITableView = {
+        let view = UITableView()
+        view.backgroundColor = .clear
+        view.allowsSelection = false
+        return view
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        backgroundColor = R.color.colorBlack()
+
+        setupLayout()
     }
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
     }
 }
