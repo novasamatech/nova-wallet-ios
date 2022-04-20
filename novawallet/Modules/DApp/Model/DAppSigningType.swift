7c1fb8220d26f1b4aada5652d6424c3dbac501ad
ERussel <emkil.russel@gmail.com>
2022-04-20 16:28:31 +0300

    add personal sign implementation

--- novawallet/Modules/DApp/Model/DAppSigningType.swift
+++ novawallet/Modules/DApp/Model/DAppSigningType.swift
@@ -4,6 +4,7 @@ enum DAppSigningType {
     case extrinsic(chain: ChainModel)
     case bytes(chain: ChainModel)
     case ethereumTransaction(chain: MetamaskChain)
+    case ethereumPersonalSign(chain: MetamaskChain, accountId: AccountId)
 
     var msgType: PolkadotExtensionMessage.MessageType? {
         switch self {
@@ -11,7 +12,7 @@ enum DAppSigningType {
             return .signExtrinsic
         case .bytes:
             return .signBytes
-        case .ethereumTransaction:
+        case .ethereumTransaction, .ethereumPersonalSign:
             return nil
         }
     }
