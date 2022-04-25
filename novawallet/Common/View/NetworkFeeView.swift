ebd897191c05ea2bc38dd5de3e30268cb436193a
ERussel <emkil.russel@gmail.com>
2022-04-25 21:55:32 +0300

    unstake setup screen

--- novawallet/Common/View/NetworkFeeView.swift
+++ novawallet/Common/View/NetworkFeeView.swift
@@ -1,38 +1,7 @@
 import UIKit
 import SoraUI
 
-class NetworkFeeView: UIView {
-    struct ViewStyle {
-        let titleColor: UIColor
-        let titleFont: UIFont
-        let tokenColor: UIColor
-        let tokenFont: UIFont
-        let fiatColor: UIColor
-        let fiatFont: UIFont
-    }
-
-    let titleLabel = UILabel()
-
-    let tokenLabel = UILabel()
-
-    let borderView: BorderedContainerView = {
-        let view = BorderedContainerView()
-        view.backgroundColor = .clear
-        view.borderType = .bottom
-        view.strokeWidth = 1.0
-        view.strokeColor = R.color.colorDarkGray()!
-        return view
-    }()
-
-    let activityIndicator: UIActivityIndicatorView = {
-        let view = UIActivityIndicatorView()
-        view.hidesWhenStopped = true
-        view.style = .white
-        return view
-    }()
-
-    private(set) var fiatLabel: UILabel?
-
+class NetworkFeeView: TitleAmountView {
     var locale = Locale.current {
         didSet {
             if locale != oldValue {
@@ -44,9 +13,7 @@ class NetworkFeeView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        setupLayout()
         applyLocalization()
-        applyStyle()
     }
 
     @available(*, unavailable)
@@ -54,120 +21,7 @@ class NetworkFeeView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    var style = ViewStyle(
-        titleColor: R.color.colorLightGray()!,
-        titleFont: .p1Paragraph,
-        tokenColor: R.color.colorWhite()!,
-        tokenFont: .p1Paragraph,
-        fiatColor: R.color.colorGray()!,
-        fiatFont: .p2Paragraph
-    ) {
-        didSet {
-            applyStyle()
-        }
-    }
-
-    func requiresFlexibleHeight() {
-        titleLabel.snp.remakeConstraints { make in
-            make.leading.equalToSuperview()
-            make.centerY.equalToSuperview()
-        }
-    }
-
-    private func applyStyle() {
-        titleLabel.textColor = style.titleColor
-        titleLabel.font = style.titleFont
-
-        tokenLabel.textColor = style.tokenColor
-        tokenLabel.font = style.tokenFont
-
-        fiatLabel?.textColor = style.fiatColor
-        fiatLabel?.font = style.fiatFont
-    }
-
     private func applyLocalization() {
         titleLabel.text = R.string.localizable.commonNetworkFee(preferredLanguages: locale.rLanguages)
-    }
-
-    private func setupLayout() {
-        addSubview(borderView)
-        borderView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-
-        addSubview(titleLabel)
-        titleLabel.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.top.bottom.equalToSuperview().inset(UIConstants.horizontalInset)
-        }
-
-        addSubview(tokenLabel)
-        tokenLabel.snp.makeConstraints { make in
-            make.trailing.centerY.equalToSuperview()
-            make.leading.greaterThanOrEqualTo(titleLabel.snp.trailing).offset(8.0)
-        }
-
-        addSubview(activityIndicator)
-        activityIndicator.snp.makeConstraints { make in
-            make.trailing.centerY.equalToSuperview()
-        }
-    }
-
-    private func addFiatLabelIfNeeded() {
-        guard fiatLabel == nil else {
-            return
-        }
-
-        let fiatLabel = UILabel()
-        fiatLabel.textColor = style.fiatColor
-        fiatLabel.font = style.fiatFont
-
-        addSubview(fiatLabel)
-        fiatLabel.snp.makeConstraints { make in
-            make.trailing.equalToSuperview()
-            make.top.equalTo(self.snp.centerY)
-            make.leading.greaterThanOrEqualTo(titleLabel.snp.trailing).offset(8.0)
-        }
-
-        tokenLabel.snp.remakeConstraints { make in
-            make.trailing.equalToSuperview()
-            make.bottom.equalTo(self.snp.centerY)
-            make.leading.greaterThanOrEqualTo(titleLabel.snp.trailing).offset(8.0)
-        }
-
-        self.fiatLabel = fiatLabel
-    }
-
-    private func removeFiatLabelIfNeeded() {
-        guard fiatLabel != nil else {
-            return
-        }
-
-        fiatLabel?.removeFromSuperview()
-        fiatLabel = nil
-
-        tokenLabel.snp.remakeConstraints { make in
-            make.trailing.centerY.equalToSuperview()
-            make.leading.greaterThanOrEqualTo(titleLabel.snp.trailing).offset(8.0)
-        }
-    }
-
-    func bind(viewModel: BalanceViewModelProtocol?) {
-        if viewModel != nil {
-            activityIndicator.stopAnimating()
-        } else {
-            activityIndicator.startAnimating()
-        }
-
-        tokenLabel.text = viewModel?.amount
-
-        if let fiatAmount = viewModel?.price {
-            addFiatLabelIfNeeded()
-            fiatLabel?.text = fiatAmount
-        } else {
-            removeFiatLabelIfNeeded()
-        }
-
-        setNeedsLayout()
     }
 }
--- novawallet/Modules/Staking/StakingUnbondSetup/View/StakingUnbondSetupLayout.swift
+++ novawallet/Modules/Staking/StakingUnbondSetup/View/StakingUnbondSetupLayout.swift
@@ -2,28 +2,31 @@ import UIKit
 import SnapKit
 
 final class StakingUnbondSetupLayout: UIView {
-    let contentView: ScrollableContainerView = {
+    let containerView: ScrollableContainerView = {
         let view = ScrollableContainerView()
         view.stackView.isLayoutMarginsRelativeArrangement = true
-        view.stackView.layoutMargins = UIEdgeInsets(top: 16.0, left: 0.0, bottom: 0.0, right: 0.0)
+        view.stackView.layoutMargins = UIEdgeInsets(top: 0.0, left: 16.0, bottom: 0.0, right: 16.0)
+        view.stackView.alignment = .fill
         return view
     }()
 
-    let amountInputView: AmountInputView = UIFactory.default.createAmountInputView(filled: false)
+    let amountView = TitleHorizontalMultiValueView()
 
-    let networkFeeView = UIFactory.default.createNetworkFeeView()
+    let amountInputView = NewAmountInputView()
 
-    let durationView = UIFactory.default.createTitleValueView()
+    let transferrableView = TitleAmountView.dark()
 
-    let actionButton: TriangularedButton = UIFactory.default.createMainActionButton()
+    let networkFeeView: NetworkFeeView = {
+        let view = UIFactory.default.createNetwork26FeeView()
+        view.verticalOffset = 13.0
+        return view
+    }()
 
-    var locale = Locale.current {
-        didSet {
-            if locale != oldValue {
-                applyLocalization()
-            }
-        }
-    }
+    let actionButton: TriangularedButton = {
+        let button = TriangularedButton()
+        button.applyDefaultStyle()
+        return button
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,7 +34,6 @@ final class StakingUnbondSetupLayout: UIView {
         backgroundColor = R.color.colorBlack()!
 
         setupLayout()
-        applyLocalization()
     }
 
     @available(*, unavailable)
@@ -39,50 +41,36 @@ final class StakingUnbondSetupLayout: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func applyLocalization() {
-        networkFeeView.locale = locale
-
-        durationView.titleLabel.text = R.string.localizable
-            .stakingUnbondingPeriod_v190(preferredLanguages: locale.rLanguages)
-
-        amountInputView.title = R.string.localizable
-            .walletSendAmountTitle(preferredLanguages: locale.rLanguages)
-
-        actionButton.imageWithTitleView?.title = R.string.localizable
-            .commonContinue(preferredLanguages: locale.rLanguages)
-    }
-
     private func setupLayout() {
-        addSubview(contentView)
-        contentView.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide)
-            make.bottom.leading.trailing.equalToSuperview()
-        }
-
-        contentView.stackView.addArrangedSubview(amountInputView)
-        amountInputView.snp.makeConstraints { make in
-            make.width.equalTo(self).offset(-2.0 * UIConstants.horizontalInset)
-            make.height.equalTo(72.0)
-        }
-
-        contentView.stackView.setCustomSpacing(16.0, after: amountInputView)
-
-        contentView.stackView.addArrangedSubview(networkFeeView)
-        networkFeeView.snp.makeConstraints { make in
-            make.width.equalTo(self).offset(-2.0 * UIConstants.horizontalInset)
-        }
-
-        contentView.stackView.addArrangedSubview(durationView)
-        durationView.snp.makeConstraints { make in
-            make.width.equalTo(self).offset(-2.0 * UIConstants.horizontalInset)
-            make.height.equalTo(48.0)
-        }
-
         addSubview(actionButton)
         actionButton.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
             make.bottom.equalTo(safeAreaLayoutGuide).inset(UIConstants.actionBottomInset)
             make.height.equalTo(UIConstants.actionHeight)
         }
+
+        addSubview(containerView)
+        containerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(actionButton.snp.top).offset(-8.0)
+        }
+
+        containerView.stackView.addArrangedSubview(amountView)
+        amountView.snp.makeConstraints { make in
+            make.height.equalTo(34.0)
+        }
+
+        containerView.stackView.addArrangedSubview(amountInputView)
+        amountInputView.snp.makeConstraints { make in
+            make.height.equalTo(64)
+        }
+
+        containerView.stackView.setCustomSpacing(16.0, after: amountInputView)
+
+        containerView.stackView.addArrangedSubview(transferrableView)
+
+        containerView.stackView.addArrangedSubview(networkFeeView)
+
+        containerView.stackView.setCustomSpacing(24.0, after: networkFeeView)
     }
 }
