a1386bc702baa433fd81fc304a62ec8af0b8ade1
ERussel <emkil.russel@gmail.com>
2022-04-28 00:27:59 +0300

    redesign your validators

--- novawallet/Modules/Staking/SelectValidatorsFlow/YourValidatorList/View/YourValidatorListWarningSectionView.swift
+++ novawallet/Modules/Staking/SelectValidatorsFlow/YourValidatorList/View/YourValidatorListWarningSectionView.swift
@@ -3,29 +3,17 @@ import SoraUI
 import SoraFoundation
 
 class YourValidatorListWarningSectionView: YourValidatorListStatusSectionView {
-    let hintBorderView = UIFactory.default.createBorderedContainerView()
-
-    let hintView: HintView = {
-        let view = UIFactory.default.createHintView()
-        view.iconView.image = R.image.iconWarning()
-        return view
-    }()
+    let hintView: InlineAlertView = .warning()
 
     override func setupLayout() {
         super.setupLayout()
 
-        mainStackView.insertArranged(view: hintBorderView, before: statusView)
+        mainStackView.insertArranged(view: hintView, before: statusView)
 
-        hintBorderView.addSubview(hintView)
-        hintView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview()
-            make.top.bottom.equalToSuperview().inset(16.0)
-        }
-
-        mainStackView.setCustomSpacing(16.0, after: hintBorderView)
+        mainStackView.setCustomSpacing(20.0, after: hintView)
     }
 
     func bind(warningText: String) {
-        hintView.titleLabel.text = warningText
+        hintView.contentView.detailsLabel.text = warningText
     }
 }
