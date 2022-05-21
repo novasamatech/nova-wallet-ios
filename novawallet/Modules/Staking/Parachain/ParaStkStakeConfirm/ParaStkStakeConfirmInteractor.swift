a565ddcd93df10a8a8b65cd36b2af23cbb7942d4
ERussel <emkil.russel@gmail.com>
2022-05-22 00:00:32 +0300

    add stake confirm view

---
+++ novawallet/Modules/Staking/Parachain/ParaStkStakeConfirm/ParaStkStakeConfirmInteractor.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+final class ParaStkStakeConfirmInteractor {
+    weak var presenter: ParaStkStakeConfirmInteractorOutputProtocol!
+}
+
+extension ParaStkStakeConfirmInteractor: ParaStkStakeConfirmInteractorInputProtocol {}
