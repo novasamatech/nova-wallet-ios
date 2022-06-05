4f7152d68438471c13f729511afcfbb2893685e1
ERussel <emkil.russel@gmail.com>
2022-06-06 01:27:03 +0300

    add unstake confirm

---
+++ novawallet/Modules/Staking/Parachain/ParaStkUnstakeConfirm/ParaStkUnstakeConfirmProtocols.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+protocol ParaStkUnstakeConfirmViewProtocol: ControllerBackedProtocol, LoadableViewProtocol {
+    func didReceiveAmount(viewModel: BalanceViewModelProtocol)
+    func didReceiveWallet(viewModel: DisplayWalletViewModel)
+    func didReceiveAccount(viewModel: DisplayAddressViewModel)
+    func didReceiveFee(viewModel: BalanceViewModelProtocol?)
+    func didReceiveCollator(viewModel: DisplayAddressViewModel)
+    func didReceiveHints(viewModel: [String])
+}
+
+protocol ParaStkUnstakeConfirmPresenterProtocol: AnyObject {
+    func setup()
+    func selectAccount()
+    func selectCollator()
+    func confirm()
+}
+
+protocol ParaStkUnstakeConfirmInteractorInputProtocol: ParaStkBaseUnstakeInteractorInputProtocol {
+    func confirm(for callWrapper: UnstakeCallWrapper)
+}
+
+protocol ParaStkUnstakeConfirmInteractorOutputProtocol: ParaStkBaseUnstakeInteractorOutputProtocol {
+    func didCompleteExtrinsicSubmission(for result: Result<String, Error>)
+}
+
+protocol ParaStkUnstakeConfirmWireframeProtocol: AlertPresentable, ErrorPresentable,
+    ParachainStakingErrorPresentable,
+    AddressOptionsPresentable,
+    FeeRetryable {
+    func complete(on view: ParaStkUnstakeConfirmViewProtocol?, locale: Locale)
+}
