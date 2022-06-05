43807baaad52d4d7ebf8095a2797d5eb6b6651d2
ERussel <emkil.russel@gmail.com>
2022-06-05 15:55:13 +0300

    add unstaking alerts

--- novawallet/Modules/Staking/Parachain/ParaStkUnstake/ParaStkUnstakeWireframe.swift
+++ novawallet/Modules/Staking/Parachain/ParaStkUnstake/ParaStkUnstakeWireframe.swift
@@ -6,4 +6,24 @@ final class ParaStkUnstakeWireframe: ParaStkUnstakeWireframeProtocol {
     init(state: ParachainStakingSharedState) {
         self.state = state
     }
+
+    func showUnstakingCollatorSelection(
+        from view: ParaStkUnstakeViewProtocol?,
+        viewModels: [AccountDetailsPickerViewModel],
+        selectedIndex: Int,
+        delegate: ModalPickerViewControllerDelegate,
+        context: AnyObject?
+    ) {
+        guard let infoVew = ModalPickerFactory.createCollatorsPickingList(
+            viewModels,
+            actionViewModel: nil,
+            selectedIndex: selectedIndex,
+            delegate: delegate,
+            context: context
+        ) else {
+            return
+        }
+
+        view?.controller.present(infoVew, animated: true, completion: nil)
+    }
 }
