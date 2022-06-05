4ae4c1a622be41aa9af0e4853919d61719d5353a
ERussel <emkil.russel@gmail.com>
2022-06-05 23:30:07 +0300

    refactoring unstake

---
+++ novawallet/Modules/Staking/Validation/ParachainStakingValidatorFactoryProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  ParachainStakingValidatorFactoryProtocol.swift
+//  novawallet
+//
+//  Created by Ruslan Rezin on 05.06.2022.
+//  Copyright © 2022 Nova Foundation. All rights reserved.
+//
+
+import Foundation
