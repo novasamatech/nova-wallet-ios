c9bbf6b54710a4a8c2ec3cb1c30723aa77d1687a
ERussel <emkil.russel@gmail.com>
2022-06-07 12:58:57 +0300

    add active collator validation for start staking

--- novawallet/Modules/Staking/Validation/ParachainStakingErrorPresentable.swift
+++ novawallet/Modules/Staking/Validation/ParachainStakingErrorPresentable.swift
@@ -36,6 +36,8 @@ protocol ParachainStakingErrorPresentable: BaseErrorPresentable {
     func presentCantRebond(_ view: ControllerBackedProtocol, locale: Locale?)
 
     func presentCantStakeMoreWhileRevoking(_ view: ControllerBackedProtocol, locale: Locale?)
+
+    func presentCantStakeInactiveCollator(_ view: ControllerBackedProtocol, locale: Locale?)
 }
 
 extension ParachainStakingErrorPresentable where Self: AlertPresentable & ErrorPresentable {
@@ -212,6 +214,18 @@ extension ParachainStakingErrorPresentable where Self: AlertPresentable & ErrorP
         let title = R.string.localizable.parastkCantBondMoreTitle(preferredLanguages: languages)
 
         let message = R.string.localizable.parastkPendingRevokeMessage(preferredLanguages: languages)
+
+        let close = R.string.localizable.commonClose(preferredLanguages: languages)
+
+        present(message: message, title: title, closeAction: close, from: view)
+    }
+
+    func presentCantStakeInactiveCollator(_ view: ControllerBackedProtocol, locale: Locale?) {
+        let languages = locale?.rLanguages
+
+        let title = R.string.localizable.parastkNotActiveCollatorTitle(preferredLanguages: languages)
+
+        let message = R.string.localizable.parastkNotActiveCollatorMessage(preferredLanguages: languages)
 
         let close = R.string.localizable.commonClose(preferredLanguages: languages)
 
