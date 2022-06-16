9b382e587ec0556b99fd5be9a43f4f7862e73efd
ERussel <emkil.russel@gmail.com>
2022-06-16 17:12:37 +0300

    add remote parsing integration test

--- novawallet/Common/Model/Xcm/XcmTransfers.swift
+++ novawallet/Common/Model/Xcm/XcmTransfers.swift
@@ -1,7 +1,16 @@
 import Foundation
+import SubstrateSdk
 
 struct XcmTransfers: Decodable {
-    let assetsLocation: XcmAssetsLocation
-    let instructions: XcmInstructions
+    let assetsLocation: [String: JSON]
+    let instructions: [String: [String]]
     let chains: [XcmChain]
+
+    func assetLocation(for key: String) -> JSON? {
+        assetsLocation[key]
+    }
+
+    func instructions(for key: String) -> [String]? {
+        instructions[key]
+    }
 }
