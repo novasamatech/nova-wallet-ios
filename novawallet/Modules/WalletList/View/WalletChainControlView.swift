d83b9f15bf62ab4c4444c1eac0105c333c604698
ERussel <emkil.russel@gmail.com>
2022-06-27 01:11:15 +0300

    fix layout

--- novawallet/Modules/WalletList/View/WalletChainControlView.swift
+++ novawallet/Modules/WalletList/View/WalletChainControlView.swift
@@ -31,6 +31,11 @@ final class WalletChainControlView: UIView {
 
     let iconSize = CGSize(width: 24.0, height: 24.0)
 
+    convenience init() {
+        let defaultFrame = CGRect(origin: .zero, size: CGSize(width: 48.0, height: 24.0))
+        self.init(frame: defaultFrame)
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
