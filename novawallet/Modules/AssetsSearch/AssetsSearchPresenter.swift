08af221e4844df13cc3911b9a487ca272afea638
ERussel <emkil.russel@gmail.com>
2022-07-15 16:13:33 +0300

    make base preseter for assets list

--- novawallet/Modules/AssetsSearch/AssetsSearchPresenter.swift
+++ novawallet/Modules/AssetsSearch/AssetsSearchPresenter.swift
@@ -7,6 +7,10 @@ final class AssetsSearchPresenter {
     let wireframe: AssetsSearchWireframeProtocol
     let interactor: AssetsSearchInteractorInputProtocol
 
+    private(set) var priceResult: Result<[ChainAssetId: PriceData], Error>?
+    private(set) var balanceResults: [ChainAssetId: Result<BigUInt, Error>] = [:]
+    private(set) var allChains: [ChainModel.Id: ChainModel] = [:]
+
     init(
         interactor: AssetsSearchInteractorInputProtocol,
         wireframe: AssetsSearchWireframeProtocol
@@ -21,9 +25,33 @@ extension AssetsSearchPresenter: AssetsSearchPresenterProtocol {
 }
 
 extension AssetsSearchPresenter: AssetsSearchInteractorOutputProtocol {
-    func didReceiveChainModelChanges(_: [DataProviderChange<ChainModel>]) {}
+    func didReceiveChainModelChanges(_ changes: [DataProviderChange<ChainModel>]) {
+        allChains = changes.reduce(into: allChains) { result, change in
+            switch change {
+            case let .insert(newItem):
+                result[newItem.chainId] = newItem
+            case let .update(newItem):
+                result[newItem.chainId] = newItem
+            case let .delete(deletedIdentifier):
+                result[deletedIdentifier] = nil
+            }
+        }
+    }
 
-    func didReceiveBalance(results _: [ChainAssetId: Result<BigUInt?, Error>]) {}
+    func didReceiveBalance(results: [ChainAssetId: Result<BigUInt?, Error>]) {
+        for (chainAssetId, result) in results {
+            switch result {
+            case let .success(maybeAmount):
+                if let amount = maybeAmount {
+                    balanceResults[chainAssetId] = .success(amount)
+                } else if balanceResults[chainAssetId] == nil {
+                    balanceResults[chainAssetId] = .success(0)
+                }
+            case let .failure(error):
+                balanceResults[chainAssetId] = .failure(error)
+            }
+        }
+    }
 
     func didReceivePrices(result _: Result<[ChainAssetId: PriceData], Error>?) {}
 }
