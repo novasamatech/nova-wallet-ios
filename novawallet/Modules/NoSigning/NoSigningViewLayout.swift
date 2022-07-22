76d1774bcf6df18d7707b0a249fbc95382b352b1
ERussel <emkil.russel@gmail.com>
2022-07-22 09:34:36 +0300

    no signing layout

---
+++ novawallet/Modules/NoSigning/NoSigningViewLayout.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+final class NoSigningViewLayout: UIView {
+    let iconView = UIImageView(image: R.image.imageNoKeys())
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.colorWhite()
+        label.font = .semiBoldTitle3
+        label.textAlignment = .center
+        return label
+    }()
+
+    let detailsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = R.color.colorTransparentText()
+        label.font = .regularFootnote
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+
+    let actionButton: TriangularedButton = {
+        let button = TriangularedButton()
+        button.applyDefaultStyle()
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        backgroundColor = .clear
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        addSubview(iconView)
+        iconView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(16.0)
+        }
+
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.top.equalTo(iconView.snp.bottom).offset(24.0)
+        }
+
+        addSubview(detailsLabel)
+        detailsLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.top.equalTo(titleLabel.snp.bottom).offset(8.0)
+        }
+
+        addSubview(actionButton)
+        actionButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(16.0)
+            make.height.equalTo(UIConstants.actionHeight)
+        }
+    }
+}
