fd93631f4e57451ff5997626cd8525c4bb051838
ERussel <emkil.russel@gmail.com>
2022-08-04 09:10:56 +0300

    refactor username setup

---
+++ novawallet/Modules/UsernameSetup/Base/BaseUsernameSetupPresenter.swift
@@ -0,0 +1,18 @@
+import Foundation
+import SoraFoundation
+
+class BaseUsernameSetupPresenter {
+    weak var view: UsernameSetupViewProtocol?
+
+    let viewModel: InputViewModelProtocol = {
+        let inputHandling = InputHandler(
+            predicate: NSPredicate.notEmpty,
+            processor: ByteLengthProcessor.username
+        )
+        return InputViewModel(inputHandler: inputHandling)
+    }()
+
+    func setup() {
+        view?.setInput(viewModel: viewModel)
+    }
+}
