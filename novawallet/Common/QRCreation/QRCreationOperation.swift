09d8ee4f37913c6ee0fc611a9165021904538e65
ERussel <emkil.russel@gmail.com>
2022-08-08 15:33:24 +0300

    fix message operation factory

--- novawallet/Common/QRCreation/QRCreationOperation.swift
+++ novawallet/Common/QRCreation/QRCreationOperation.swift
@@ -9,62 +9,73 @@ enum QRCreationOperationError: Error {
 }
 
 final class QRCreationOperation: BaseOperation<UIImage> {
-    let payload: Data
+    let payloadClosure: () throws -> Data
     let qrSize: CGSize
 
     init(payload: Data, qrSize: CGSize) {
-        self.payload = payload
+        self.payloadClosure = { payload }
         self.qrSize = qrSize
 
         super.init()
     }
 
+    init(qrSize: CGSize, payloadClosure: @escaping () throws -> Data) {
+        self.qrSize = qrSize
+        self.payloadClosure = payloadClosure
+    }
+
     override public func main() {
         super.main()
 
-        guard let filter = CIFilter(name: "CIQRCodeGenerator") else {
-            if !isCancelled {
-                result = .failure(QRCreationOperationError.generatorUnavailable)
+        do {
+            guard let filter = CIFilter(name: "CIQRCodeGenerator") else {
+                if !isCancelled {
+                    result = .failure(QRCreationOperationError.generatorUnavailable)
+                }
+
+                return
             }
 
-            return
-        }
+            let payload = try payloadClosure()
 
-        filter.setValue(payload, forKey: "inputMessage")
-        filter.setValue("M", forKey: "inputCorrectionLevel")
+            filter.setValue(payload, forKey: "inputMessage")
+            filter.setValue("M", forKey: "inputCorrectionLevel")
 
-        guard let qrImage = filter.outputImage else {
-            if !isCancelled {
-                result = .failure(QRCreationOperationError.generatedImageInvalid)
+            guard let qrImage = filter.outputImage else {
+                if !isCancelled {
+                    result = .failure(QRCreationOperationError.generatedImageInvalid)
+                }
+
+                return
             }
 
-            return
-        }
+            let transformedImage: CIImage
 
-        let transformedImage: CIImage
-
-        if qrImage.extent.size.width * qrImage.extent.height > 0.0 {
-            let transform = CGAffineTransform(
-                scaleX: qrSize.width / qrImage.extent.width,
-                y: qrSize.height / qrImage.extent.height
-            )
-            transformedImage = qrImage.transformed(by: transform)
-        } else {
-            transformedImage = qrImage
-        }
-
-        let context = CIContext()
-
-        guard let cgImage = context.createCGImage(transformedImage, from: transformedImage.extent) else {
-            if !isCancelled {
-                result = .failure(QRCreationOperationError.bitmapImageCreationFailed)
+            if qrImage.extent.size.width * qrImage.extent.height > 0.0 {
+                let transform = CGAffineTransform(
+                    scaleX: qrSize.width / qrImage.extent.width,
+                    y: qrSize.height / qrImage.extent.height
+                )
+                transformedImage = qrImage.transformed(by: transform)
+            } else {
+                transformedImage = qrImage
             }
 
-            return
-        }
+            let context = CIContext()
 
-        if !isCancelled {
-            result = .success(UIImage(cgImage: cgImage))
+            guard let cgImage = context.createCGImage(transformedImage, from: transformedImage.extent) else {
+                if !isCancelled {
+                    result = .failure(QRCreationOperationError.bitmapImageCreationFailed)
+                }
+
+                return
+            }
+
+            if !isCancelled {
+                result = .success(UIImage(cgImage: cgImage))
+            }
+        } catch {
+            result = .failure(error)
         }
     }
 }
