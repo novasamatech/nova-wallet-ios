00afb1e119c8b6c30f4b5f38c117de0db15af84b
Holyberry <666lynx666@mail.ru>
2022-08-11 12:53:20 +0400

    move usd extension to separate file

--- novawallet/Common/Currency/SelectedCurrencyDepending.swift
+++ novawallet/Common/Currency/SelectedCurrencyDepending.swift
@@ -51,15 +51,3 @@ extension SelectedCurrencyDepending {
         return selectedCurrency
     }
 }
-
-extension Currency {
-    static let usd = Currency(
-        id: 0,
-        code: "USD",
-        name: "United States Dollar",
-        symbol: "$",
-        category: .fiat,
-        isPopular: true,
-        coingeckoId: "usd"
-    )
-}
