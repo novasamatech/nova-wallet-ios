73f0e5c4b8f73ef7c32354eecf8eef3b0c0c485c
Gulnaz <666lynx666@mail.ru>
2022-08-15 17:55:30 +0400

    Selected currency feature tests (#356)
    * Added "Currency" screen (#343)
    
    * added currency module logic
    
    * fix currency parsing
    
    * added feature save selected currency
    
    * buildfix
    
    * added atomic value, fixed selected currency bug
    
    * cleanup
    
    * buildfix
    
    * added atomic wrapper
    
    * removed empty test
    
    * PR fixes
    
    * fixed selected currency
    
    * rename render method to bind
    
    * fix naming
    
    * PR fixes: wireframe, selected locale
    
    * CurrencyManager (#345)
    
    * added manager
    
    * renamed state observable
    
    * cleanup
    
    * fix naming
    
    * cleanup
    
    * rename error
    
    * rename key for kvo
    
    * PR fixes
    
    * Added "Currency" menu item to Settings (#346)
    
    * added currency row to settings
    
    * added currency changes logic
    
    * bugfix layout, rename currencyCode
    
    * simplify localization for settings cell title
    
    * fixed coingeckoId for aed sample
    
    * Integrate selected currency to modules with prices (#351)
    
    * init
    
    * fix currency data decoding for coingecko
    
    * missing files
    
    * removed force unwrapping
    
    * cleanup
    
    * fixed tests, fixed settings interactor
    
    * move usd extension to separate file
    
    * added coingeckoId to dataProvider's cache key
    
    * removed unused method
    
    * Support for the selected currency in UI (#353)
    
    * added price asset factory to view factories
    
    * removed priceAssetInfoFactory from presenters
    
    * replace BalanceViewModelFactory with AssetBalanceFormatterFactory for base validators
    
    * added priceAssetInfoFactory to NetworkInfoViewModelFactory
    
    * fix priceId in BalanceContext
    
    * fix asset formatter on details screen
    
    * fix tests
    
    * fix asset details cache
    
    * apply currency to interactor only if presenter is set
    
    * fixed currency for asset list
    
    * fix currency formatting
    
    * added currencies, added new way from selecting currency module
    
    * buildfix after merge
    
    * fiat display on asset cell
    
    * fix languages
    
    * fix zero price formatting
    
    Co-authored-by: ERussel <emkil.russel@gmail.com>
    
    * added tests for currency
    
    Co-authored-by: ERussel <emkil.russel@gmail.com>

---
+++ novawalletTests/Common/Currency/CurrencyRepositoryTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+import Cuckoo
+import RobinHood
+import SoraKeystore
+@testable import novawallet
+
+final class CurrencyRepositoryTests: XCTestCase {
+  
+    func testFetchAvailableCurrencies() throws {
+        let currencyRepository = CurrencyRepository()
+        let queue = OperationQueue()
+        let availableCurrenciesWrapper = currencyRepository.fetchAvailableCurrenciesWrapper()
+        queue.addOperations(availableCurrenciesWrapper.allOperations, waitUntilFinished: true)
+        let currencies = try availableCurrenciesWrapper.targetOperation.extractResultData()
+        XCTAssertNotNil(currencies)
+        XCTAssertFalse(currencies!.isEmpty)
+    }
+    
+    func testParsingCurrencyJson() throws {
+        let currencyRepository = CurrencyRepository()
+        let queue = OperationQueue()
+        let operation: BaseOperation<[Currency]> = currencyRepository.fetchOperation(by: json("currencies"), defaultValue: [])
+        queue.addOperations([operation], waitUntilFinished: true)
+        let currencies = try operation.extractResultData() ?? []
+        XCTAssertFalse(currencies.isEmpty)
+       
+        let currency = currencies[0]
+        XCTAssertEqual(currency.id, 0)
+        XCTAssertEqual(currency.code, "USD")
+        XCTAssertEqual(currency.name, "United States Dollar")
+        XCTAssertEqual(currency.symbol, "$")
+        XCTAssertEqual(currency.category, .fiat)
+        XCTAssertEqual(currency.isPopular, true)
+        XCTAssertEqual(currency.coingeckoId, "usd")
+    }
+    
+    private func json(_ name: String) -> URL? {
+        guard let path = Bundle(for: CurrencyRepositoryTests.self).path(forResource: name, ofType: "json") else {
+            return nil
+        }
+        return URL(fileURLWithPath: path)
+    }
+}
