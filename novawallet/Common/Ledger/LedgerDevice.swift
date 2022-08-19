84f28bc391a3f4bf05cf2f6b7ab41ef15a321ae3
Ruslan <emkil.russel@gmail.com>
2022-08-19 11:38:18 +0300

    Add base Ledger communication logic (#359)
    * add base bluetooth logic
    
    * add ledger application, manager and base transport
    
    * refactor
    
    * refactoring
    
    * add apdu transport logic
    
    * add ledger transport tests
    
    * add signing request
    
    * refactoring
    
    * improve chunking performance
    
    * fix init for bigEndian deserialization
    
    * refactor serialization

---
+++ novawallet/Common/Ledger/LedgerDevice.swift
@@ -0,0 +1,51 @@
+import Foundation
+import CoreBluetooth
+
+protocol LedgerDeviceProtocol {
+    var identifier: UUID { get }
+    var name: String { get }
+}
+
+final class BluetoothLedgerDevice: LedgerDeviceProtocol {
+    typealias WriteCommand = () -> Void
+    typealias ResponseCompletion = (Result<Data, Error>) -> Void
+
+    let peripheral: CBPeripheral
+
+    var name: String {
+        peripheral.name ?? "Unknown device"
+    }
+
+    var identifier: UUID {
+        peripheral.identifier
+    }
+
+    var readCharacteristic: CBCharacteristic?
+    var writeCharacteristic: CBCharacteristic?
+    var notifyCharacteristic: CBCharacteristic?
+
+    var writeCommand: WriteCommand?
+    var responseCompletion: ResponseCompletion?
+
+    let transport = LedgerTransport()
+
+    init(peripheral: CBPeripheral) {
+        self.peripheral = peripheral
+    }
+}
+
+struct SupportedBluetoothDevice {
+    let uuid: CBUUID
+    let notifyUuid: CBUUID
+    let writeUuid: CBUUID
+}
+
+extension SupportedBluetoothDevice {
+    static var ledgerNanoX: SupportedBluetoothDevice {
+        SupportedBluetoothDevice(
+            uuid: CBUUID(string: "13D63400-2C97-0004-0000-4C6564676572"),
+            notifyUuid: CBUUID(string: "13D63400-2C97-0004-0001-4C6564676572"),
+            writeUuid: CBUUID(string: "13D63400-2C97-0004-0002-4C6564676572")
+        )
+    }
+}
---
+++ novawallet/Common/Ledger/LedgerResponse.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+enum LedgerResponse: UInt16 {
+    case unknown = 1
+    case badRequest = 2
+    case unsupported = 3
+    case ineligibleDevice = 4
+    case timeoutU2f = 5
+    case timeout = 14
+    case noError = 0x9000
+    case deviceBusy = 0x9001
+    case derivingKeyError = 0x6802
+    case executionError = 0x6400
+    case wrongLength = 0x6700
+    case emptyBuffer = 0x6982
+    case outputBufferTooSmall = 0x6983
+    case invalidData = 0x6984
+    case conditionsNotSatisfied = 0x6985
+    case transactionRejected = 0x6986
+    case badKey = 0x6A80
+    case invalidP1P2 = 0x6B00
+    case instructionNotSupported = 0x6D00
+    case appNotOpen = 0x6E00
+    case unknownError = 0x6F00
+    case signVerifyError = 0x6F01
+
+    init(responseCode: UInt16) {
+        self = .init(rawValue: responseCode) ?? .unknown
+    }
+}
