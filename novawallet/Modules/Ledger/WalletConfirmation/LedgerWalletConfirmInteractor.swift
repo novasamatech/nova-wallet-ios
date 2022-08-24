061385e02f1f9f4da17f535b4d171c105e581d4b
ERussel <emkil.russel@gmail.com>
2022-08-24 18:05:49 +0300

    add ledger wallet

--- novawallet/Modules/Ledger/WalletConfirmation/LedgerWalletConfirmInteractor.swift
+++ novawallet/Modules/Ledger/WalletConfirmation/LedgerWalletConfirmInteractor.swift
@@ -1,4 +1,5 @@
 import UIKit
+import RobinHood
 
 final class LedgerWalletConfirmInteractor {
     weak var presenter: LedgerWalletConfirmInteractorOutputProtocol?
@@ -22,5 +23,54 @@ final class LedgerWalletConfirmInteractor {
 }
 
 extension LedgerWalletConfirmInteractor: LedgerWalletConfirmInteractorInputProtocol {
-    func save(with _: String) {}
+    func save(with walletName: String) {
+        let chainAccounts: [ChainAccountModel] = accountsStore.state.compactMap { ledgerAccount in
+            guard let info = ledgerAccount.info else {
+                return nil
+            }
+
+            return ChainAccountModel(
+                chainId: ledgerAccount.chain.chainId,
+                accountId: info.accountId,
+                publicKey: info.publicKey,
+                cryptoType: info.cryptoType.rawValue
+            )
+        }
+
+        guard !chainAccounts.isEmpty else {
+            presenter?.didReceive(error: CommonError.dataCorruption)
+            return
+        }
+
+        let wallet = MetaAccountModel(
+            metaId: UUID().uuidString,
+            name: walletName,
+            substrateAccountId: nil,
+            substrateCryptoType: nil,
+            substratePublicKey: nil,
+            ethereumAddress: nil,
+            ethereumPublicKey: nil,
+            chainAccounts: Set(chainAccounts),
+            type: .ledger
+        )
+
+        let saveOperation = ClosureOperation { [weak self] in
+            self?.settings.save(value: wallet)
+        }
+
+        saveOperation.completionBlock = { [weak self] in
+            DispatchQueue.main.async {
+                do {
+                    _ = try saveOperation.extractNoCancellableResultData()
+                    self?.settings.setup()
+                    self?.eventCenter.notify(with: SelectedAccountChanged())
+                    self?.presenter?.didCreateWallet()
+                } catch {
+                    self?.presenter?.didReceive(error: error)
+                }
+            }
+        }
+
+        operationQueue.addOperation(saveOperation)
+    }
 }
