657af4502f10389806295d6ebcde17d7702edd27
ERussel <emkil.russel@gmail.com>
2022-08-24 18:22:44 +0300

    fix add ledger section to manage wallet

--- novawallet/Modules/Ledger/WalletConfirmation/LedgerWalletConfirmWireframe.swift
+++ novawallet/Modules/Ledger/WalletConfirmation/LedgerWalletConfirmWireframe.swift
@@ -1,5 +1,14 @@
 import Foundation
 
 final class LedgerWalletConfirmWireframe: LedgerWalletConfirmWireframeProtocol {
-    func complete(on _: ControllerBackedProtocol?) {}
+    func complete(on view: ControllerBackedProtocol?) {
+        guard let navigationController = view?.controller.navigationController else {
+            return
+        }
+
+        MainTransitionHelper.transitToMainTabBarController(
+            closing: navigationController,
+            animated: true
+        )
+    }
 }
