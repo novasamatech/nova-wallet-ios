9b17f4e6d57775f7da6a382d070310ba39f142de
ERussel <emkil.russel@gmail.com>
2022-08-25 23:55:19 +0300

    add instructions screen

--- novawallet/Modules/Ledger/Instructions/LedgerInstructionsWireframe.swift
+++ novawallet/Modules/Ledger/Instructions/LedgerInstructionsWireframe.swift
@@ -1,3 +1,14 @@
 import Foundation
 
-final class LedgerInstructionsWireframe: LedgerInstructionsWireframeProtocol {}
+final class LedgerInstructionsWireframe: LedgerInstructionsWireframeProtocol {
+    func showNetworkSelection(from view: LedgerInstructionsViewProtocol?) {
+        guard let networkSelectionView = LedgerNetworkSelectionViewFactory.createView() else {
+            return
+        }
+
+        view?.controller.navigationController?.pushViewController(
+            networkSelectionView.controller,
+            animated: true
+        )
+    }
+}
