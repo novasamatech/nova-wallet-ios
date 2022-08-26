c188c1126d625c63793328b9b59dff6fae650ac1
ERussel <emkil.russel@gmail.com>
2022-08-26 17:00:06 +0300

    add signing bottom sheet

--- novawallet/Modules/Ledger/TxConfirmation/LedgerTxConfirmPresenter.swift
+++ novawallet/Modules/Ledger/TxConfirmation/LedgerTxConfirmPresenter.swift
@@ -5,8 +5,17 @@ import SoraFoundation
 final class LedgerTxConfirmPresenter: LedgerPerformOperationPresenter {
     let completion: TransactionSigningClosure
 
+    private var expirationTimeInterval: TimeInterval?
+
     var wireframe: LedgerTxConfirmWireframeProtocol? {
         baseWireframe as? LedgerTxConfirmWireframeProtocol
+    }
+
+    private var timer = CountdownTimerMediator()
+
+    var isExpired: Bool {
+        expirationTimeInterval != nil &&
+            timer.remainedInterval < TimeInterval.leastNonzeroMagnitude
     }
 
     init(
@@ -24,26 +33,87 @@ final class LedgerTxConfirmPresenter: LedgerPerformOperationPresenter {
             baseWireframe: wireframe,
             localizationManager: localizationManager
         )
+
+        timer.addObserver(self)
+    }
+
+    private func performCancellation() {
+        wireframe?.complete(on: view)
+
+        completion(.failure(HardwareSigningError.signingCancelled))
+    }
+
+    override func handleAppConnection(error: Error, deviceId: UUID) {
+        if let ledgerError = error as? LedgerError, case let .response(code) = ledgerError {
+            switch code {
+            case .transactionRejected:
+                wireframe?.closeTransactionStatus(on: view)
+            default:
+                wireframe?.closeTransactionStatus(on: view)
+                super.handleAppConnection(error: error, deviceId: deviceId)
+            }
+        } else {
+            wireframe?.closeTransactionStatus(on: view)
+            super.handleAppConnection(error: error, deviceId: deviceId)
+        }
+    }
+
+    // MARK: Overriden
+
+    override func selectDevice(at index: Int) {
+        super.selectDevice(at: index)
+
+        if isConnecting {
+            wireframe?.transitToTransactionReview(on: view, timer: timer, deviceName: devices[index].name)
+        }
     }
 }
 
 extension LedgerTxConfirmPresenter: LedgerTxConfirmPresenterProtocol {
     func cancel() {
-        wireframe?.complete(on: view)
+        performCancellation()
+    }
+}
 
-        completion(.failure(HardwareSigningError.signingCancelled))
+extension LedgerTxConfirmPresenter: CountdownTimerDelegate {
+    func didStart(with _: TimeInterval) {}
+
+    func didCountdown(remainedInterval _: TimeInterval) {}
+
+    func didStop(with _: TimeInterval) {
+        if isExpired, let expirationTimeInterval = expirationTimeInterval {
+            wireframe?.transitToTransactionExpired(
+                on: view,
+                expirationTimeInterval: expirationTimeInterval
+            ) { [weak self] in
+                self?.performCancellation()
+            }
+        }
     }
 }
 
 extension LedgerTxConfirmPresenter: LedgerTxConfirmInteractorOutputProtocol {
     func didReceiveSigning(result: Result<IRSignatureProtocol, Error>, for deviceId: UUID) {
-        wireframe?.complete(on: view)
+        guard !isExpired else {
+            // ignore any signing result if transaction is expired
+            return
+        }
 
         switch result {
         case let .success(signature):
+            wireframe?.closeTransactionStatus(on: view)
+            wireframe?.complete(on: view)
             completion(.success(signature))
         case let .failure(error):
+            stopConnecting(to: deviceId)
+
             handleAppConnection(error: error, deviceId: deviceId)
         }
+    }
+
+    func didReceiveTransactionExpiration(timeInterval: TimeInterval) {
+        expirationTimeInterval = timeInterval
+
+        timer.start(with: timeInterval)
     }
 }
--- novawallet/Modules/Ledger/TxConfirmation/LedgerTxConfirmProtocols.swift
+++ novawallet/Modules/Ledger/TxConfirmation/LedgerTxConfirmProtocols.swift
@@ -6,8 +6,23 @@ protocol LedgerTxConfirmPresenterProtocol: LedgerPerformOperationPresenterProtoc
 
 protocol LedgerTxConfirmInteractorOutputProtocol: LedgerPerformOperationOutputProtocol {
     func didReceiveSigning(result: Result<IRSignatureProtocol, Error>, for deviceId: UUID)
+    func didReceiveTransactionExpiration(timeInterval: TimeInterval)
 }
 
 protocol LedgerTxConfirmWireframeProtocol: LedgerPerformOperationWireframeProtocol {
     func complete(on view: ControllerBackedProtocol?)
+
+    func transitToTransactionReview(
+        on view: ControllerBackedProtocol?,
+        timer: CountdownTimerMediator,
+        deviceName: String
+    )
+
+    func transitToTransactionExpired(
+        on view: ControllerBackedProtocol?,
+        expirationTimeInterval: TimeInterval,
+        completion: @escaping MessageSheetCallback
+    )
+
+    func closeTransactionStatus(on view: ControllerBackedProtocol?)
 }
