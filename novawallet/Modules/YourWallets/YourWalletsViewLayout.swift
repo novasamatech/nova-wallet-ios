b3990e548696061ecbb8d6602508a2958c28f0c2
Holyberry <666lynx666@mail.ru>
2022-08-29 16:17:45 +0400

    init

--- novawallet/Modules/YourWallets/YourWalletsViewLayout.swift
+++ novawallet/Modules/YourWallets/YourWalletsViewLayout.swift
@@ -1,6 +1,29 @@
 import UIKit
 
 final class YourWalletsViewLayout: UIView {
+    lazy var header: IconTitleHeaderView = .create {
+        $0.contentInsets = Constants.headerContentInsets
+    }
+
+    lazy var collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: compositionalLayout)
+        view.backgroundColor = .clear
+        view.contentInsetAdjustmentBehavior = .always
+        view.contentInset = Constants.collectionViewContentInset
+        return view
+    }()
+
+    private lazy var compositionalLayout: UICollectionViewCompositionalLayout = {
+        .init { sectionIndex, _ -> NSCollectionLayoutSection? in
+            switch sectionIndex {
+            case 0:
+                return Self.createCompositionalLayout(showHeader: false)
+            default:
+                return Self.createCompositionalLayout(showHeader: true)
+            }
+        }
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -14,5 +37,57 @@ final class YourWalletsViewLayout: UIView {
     }
 
     private func setupLayout() {
+        addSubview(header)
+        addSubview(collectionView)
+
+        header.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top)
+            $0.height.equalTo(46)
+            $0.leading.trailing.equalToSuperview()
+        }
+
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(header.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+
+    private static func createCompositionalLayout(showHeader: Bool) -> NSCollectionLayoutSection {
+        .createSectionLayoutWithFullWidthRow(settings:
+            .init(
+                estimatedRowHeight: Constants.estimatedRowHeight,
+                estimatedHeaderHeight: Constants.estimatedHeaderHeight,
+                sectionContentInsets: Constants.sectionContentInsets,
+                sectionInterGroupSpacing: Constants.interGroupSpacing,
+                header: showHeader ? .init(pinToVisibleBounds: true) : nil
+            ))
+    }
+}
+
+// MARK: - Constants
+
+extension YourWalletsViewLayout {
+    private enum Constants {
+        static let estimatedRowHeight: CGFloat = 56
+        static let estimatedHeaderHeight: CGFloat = 44
+        static let sectionContentInsets = NSDirectionalEdgeInsets(
+            top: 4,
+            leading: 16,
+            bottom: 16,
+            trailing: 16
+        )
+        static let interGroupSpacing: CGFloat = 0
+        static let collectionViewContentInset = UIEdgeInsets(
+            top: 8,
+            left: 0,
+            bottom: 16,
+            right: 0
+        )
+        static let headerContentInsets = UIEdgeInsets(
+            top: 12,
+            left: 0,
+            bottom: 0,
+            right: 0
+        )
     }
 }
