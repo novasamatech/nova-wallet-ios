7b70b7dcfd2af1183f1ccb698830b0f8f6ad88c4
ERussel <emkil.russel@gmail.com>
2022-08-30 13:27:08 +0300

    add new chains for ledger

--- novawallet/Common/Ledger/SupportedLedgerApps.swift
+++ novawallet/Common/Ledger/SupportedLedgerApps.swift
@@ -16,7 +16,10 @@ extension SupportedLedgerApp {
     static func all() -> [SupportedLedgerApp] {
         [
             SupportedLedgerApp(chainId: KnowChainId.polkadot, coin: 354, cla: 0x90, type: .substrate),
-            SupportedLedgerApp(chainId: KnowChainId.kusama, coin: 434, cla: 0x99, type: .substrate)
+            SupportedLedgerApp(chainId: KnowChainId.kusama, coin: 434, cla: 0x99, type: .substrate),
+            SupportedLedgerApp(chainId: KnowChainId.statemint, coin: 354, cla: 0x96, type: .substrate),
+            SupportedLedgerApp(chainId: KnowChainId.statemine, coin: 434, cla: 0x97, type: .substrate),
+            SupportedLedgerApp(chainId: KnowChainId.edgeware, coin: 523, cla: 0x94, type: .substrate)
         ]
     }
 
