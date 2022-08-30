f800bdbf04a8f9cd9f0ab9552649734f59ead37c
ERussel <emkil.russel@gmail.com>
2022-08-30 18:08:50 +0300

    move errors to bottom sheet

--- novawallet/Modules/Ledger/View/MessageSheetLedgerViewModel.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-import SoraFoundation
-
-struct MessageSheetLedgerViewModel {
-    let backgroundImage: UIImage
-    let text: LocalizableResource<String>
-    let icon: UIImage
-    let infoRenderSize: CGSize
-}
