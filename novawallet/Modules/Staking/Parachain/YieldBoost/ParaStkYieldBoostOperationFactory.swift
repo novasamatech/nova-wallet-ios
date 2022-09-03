48c8c5ffeb6db4ad27d65790dd8cbca4929a8732
ERussel <emkil.russel@gmail.com>
2022-09-03 15:59:19 +0300

    request autocompounding params

---
+++ novawallet/Modules/Staking/Parachain/YieldBoost/ParaStkYieldBoostOperationFactory.swift
@@ -0,0 +1,30 @@
+import Foundation
+import BigInt
+import RobinHood
+import SubstrateSdk
+
+protocol ParaStkYieldBoostOperationFactoryProtocol {
+    func createAutocompoundParamsOperation(
+        for connection: ChainConnection,
+        request: ParaStkYieldBoostRequest
+    ) -> CompoundOperationWrapper<ParaStkYieldBoostResponse>
+}
+
+final class ParaStkYieldBoostOperationFactory: ParaStkYieldBoostOperationFactoryProtocol {
+    enum RPCMethods {
+        static let calculateOptimalAutostaking = "automationTime_calculateOptimalAutostaking"
+    }
+
+    func createAutocompoundParamsOperation(
+        for connection: ChainConnection,
+        request: ParaStkYieldBoostRequest
+    ) -> CompoundOperationWrapper<ParaStkYieldBoostResponse> {
+        let operation: JSONRPCOperation<ParaStkYieldBoostRequest, ParaStkYieldBoostResponse> = JSONRPCOperation(
+            engine: connection,
+            method: RPCMethods.calculateOptimalAutostaking,
+            parameters: request
+        )
+
+        return CompoundOperationWrapper(targetOperation: operation)
+    }
+}
