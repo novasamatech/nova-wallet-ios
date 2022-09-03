e728aea6acd1ce65f2eee5a85a8096c6348d6b72
ERussel <emkil.russel@gmail.com>
2022-09-03 17:24:17 +0300

    fetch all account automation tasks

--- novawallet/Common/Substrate/Types/AutomationTime/AutomationTimeTask.swift
+++ novawallet/Common/Substrate/Types/AutomationTime/AutomationTimeTask.swift
@@ -50,4 +50,23 @@ extension AutomationTime {
             }
         }
     }
+
+    struct AccountTaskKey: Hashable, JSONListConvertible {
+        let accountId: AccountId
+        let taskId: AutomationTime.TaskId
+
+        init(jsonList: [JSON], context: [CodingUserInfoKey: Any]?) throws {
+            let expectedFieldsCount = 2
+            let actualFieldsCount = jsonList.count
+            guard expectedFieldsCount == actualFieldsCount else {
+                throw JSONListConvertibleError.unexpectedNumberOfItems(
+                    expected: expectedFieldsCount,
+                    actual: actualFieldsCount
+                )
+            }
+
+            accountId = try jsonList[0].map(to: AccountId.self, with: context)
+            taskId = try jsonList[1].map(to: BytesCodable.self, with: context).wrappedValue
+        }
+    }
 }
--- novawallet/Modules/Staking/Parachain/YieldBoost/AutomationTimeOperationFactory.swift
+++ novawallet/Modules/Staking/Parachain/YieldBoost/AutomationTimeOperationFactory.swift
@@ -1,10 +1,57 @@
 import Foundation
 import RobinHood
+import SubstrateSdk
 
 protocol AutomationTimeOperationFactoryProtocol {
     func createTasksFetchOperation(
-        for connections: ChainConnection,
+        for connection: JSONRPCEngine,
         runtimeProvider: RuntimeCodingServiceProtocol,
         account: AccountId
     ) -> CompoundOperationWrapper<[AutomationTime.TaskId: AutomationTime.Task]>
+}
+
+final class AutomationTimeOperationFactory {
+    let requestFactory: StorageRequestFactoryProtocol
+
+    init(requestFactory: StorageRequestFactoryProtocol) {
+        self.requestFactory = requestFactory
+    }
+}
+
+extension AutomationTimeOperationFactory: AutomationTimeOperationFactoryProtocol {
+    func createTasksFetchOperation(
+        for connection: JSONRPCEngine,
+        runtimeProvider: RuntimeCodingServiceProtocol,
+        account: AccountId
+    ) -> CompoundOperationWrapper<[AutomationTime.TaskId: AutomationTime.Task]> {
+        let request = MapRemoteStorageRequest(storagePath: AutomationTime.accountTasksPath) { account }
+
+        let codingFactoryOperation = runtimeProvider.fetchCoderFactoryOperation()
+
+        let fetchWrapper: CompoundOperationWrapper<[AutomationTime.AccountTaskKey: AutomationTime.Task]> =
+            requestFactory.queryByPrefix(
+                engine: connection,
+                request: request,
+                storagePath: AutomationTime.accountTasksPath,
+                factory: {
+                    try codingFactoryOperation.extractNoCancellableResultData()
+                }
+            )
+
+        fetchWrapper.addDependency(operations: [codingFactoryOperation])
+
+        let mapOperation = ClosureOperation<[AutomationTime.TaskId: AutomationTime.Task]> {
+            let response = try fetchWrapper.targetOperation.extractNoCancellableResultData()
+
+            return response.reduce(into: [AutomationTime.TaskId: AutomationTime.Task]()) { store, keyValue in
+                store[keyValue.key.taskId] = keyValue.value
+            }
+        }
+
+        mapOperation.addDependency(fetchWrapper.targetOperation)
+
+        let dependencies = [codingFactoryOperation] + fetchWrapper.allOperations
+
+        return CompoundOperationWrapper(targetOperation: mapOperation, dependencies: dependencies)
+    }
 }
