99d1adfa6def3200fff73d5a61f70199c23a31fd
ERussel <emkil.russel@gmail.com>
2022-09-08 18:32:35 +0300

    add action handlers

--- novawallet/Modules/Staking/Parachain/ParaStkYieldBoostSetup/ParaStkYieldBoostSetupWireframe.swift
+++ novawallet/Modules/Staking/Parachain/ParaStkYieldBoostSetup/ParaStkYieldBoostSetupWireframe.swift
@@ -1,3 +1,24 @@
 import Foundation
+import SoraFoundation
 
-final class ParaStkYieldBoostSetupWireframe: ParaStkYieldBoostSetupWireframeProtocol {}
+final class ParaStkYieldBoostSetupWireframe: ParaStkYieldBoostSetupWireframeProtocol {
+    func showDelegationSelection(
+        from view: ParaStkYieldBoostSetupViewProtocol?,
+        viewModels: [AccountDetailsPickerViewModel],
+        selectedIndex: Int,
+        delegate: ModalPickerViewControllerDelegate,
+        context: AnyObject?
+    ) {
+        guard let infoVew = ModalPickerFactory.createCollatorsPickingList(
+            viewModels,
+            actionViewModel: nil,
+            selectedIndex: selectedIndex,
+            delegate: delegate,
+            context: context
+        ) else {
+            return
+        }
+
+        view?.controller.present(infoVew, animated: true, completion: nil)
+    }
+}
