efa0f963a5ebfae247e0f9630f4ba6713dca23e3
ERussel <emkil.russel@gmail.com>
2022-09-16 00:45:54 +0300

    fix localization

--- novawallet/Common/Extension/Foundation/String+Helpers.swift
+++ novawallet/Common/Extension/Foundation/String+Helpers.swift
@@ -2,4 +2,8 @@ import Foundation
 
 extension String {
     static var returnKey: String { "\n" }
+
+    func firstLetterCapitalized() -> String {
+        prefix(1).capitalized + dropFirst()
+    }
 }
