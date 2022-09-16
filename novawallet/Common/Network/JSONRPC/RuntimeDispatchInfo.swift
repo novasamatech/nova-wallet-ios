c182a67a707ef9a3418e2985165096a6180c6db1
ERussel <emkil.russel@gmail.com>
2022-09-16 15:05:12 +0300

    fix fee

--- novawallet/Common/Network/JSONRPC/RuntimeDispatchInfo.swift
+++ novawallet/Common/Network/JSONRPC/RuntimeDispatchInfo.swift
@@ -1,14 +1,38 @@
 import Foundation
 import SubstrateSdk
 
-typealias WeightV1 = UInt64
+@propertyWrapper
+struct FeeWeight: Codable {
+    let wrappedValue: UInt64
 
-struct WeightV1p5: Codable {
-    enum CodingKeys: String, CodingKey {
-        case refTime = "ref_time"
+    typealias WeightV1 = UInt64
+
+    struct WeightV1p5: Codable {
+        enum CodingKeys: String, CodingKey {
+            case refTime = "ref_time"
+        }
+
+        let refTime: UInt64
     }
 
-    let refTime: UInt64
+    init(wrappedValue: UInt64) {
+        self.wrappedValue = wrappedValue
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let compoundWeight = try? container.decode(WeightV1p5.self) {
+            wrappedValue = compoundWeight.refTime
+        } else {
+            wrappedValue = try container.decode(WeightV1.self)
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(WeightV1p5(refTime: wrappedValue))
+    }
 }
 
 struct RuntimeDispatchInfo: Codable {
@@ -20,37 +44,5 @@ struct RuntimeDispatchInfo: Codable {
 
     let dispatchClass: String
     let fee: String
-    let weight: UInt64
-
-    init(
-        dispatchClass: String,
-        fee: String,
-        weight: UInt64
-    ) {
-        self.dispatchClass = dispatchClass
-        self.fee = fee
-        self.weight = weight
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        dispatchClass = try container.decode(String.self, forKey: .dispatchClass)
-        fee = try container.decode(String.self, forKey: .fee)
-
-        if let compoundWeight = try? container.decode(WeightV1p5.self, forKey: .weight) {
-            weight = compoundWeight.refTime
-        } else {
-            weight = try container.decode(WeightV1.self, forKey: .weight)
-        }
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-
-        try container.encode(dispatchClass, forKey: .dispatchClass)
-        try container.encode(fee, forKey: .fee)
-
-        try container.encode(WeightV1p5(refTime: weight), forKey: .weight)
-    }
+    @FeeWeight var weight: UInt64
 }
