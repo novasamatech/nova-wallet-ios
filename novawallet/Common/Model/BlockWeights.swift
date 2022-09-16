30807ad6ae7c2fcf60f4d013332b98b0403c9e88
ERussel <emkil.russel@gmail.com>
2022-09-16 18:09:30 +0300

    add weight compatability node

--- novawallet/Common/Model/BlockWeights.swift
+++ novawallet/Common/Model/BlockWeights.swift
@@ -1,31 +1,6 @@
 import Foundation
 import SubstrateSdk
 
-@propertyWrapper
-struct ScaleStorageWeight: Decodable {
-    let wrappedValue: UInt64
-
-    typealias WeightV1 = StringScaleMapper<UInt64>
-
-    struct WeightV1p5: Codable {
-        var refTime: StringScaleMapper<UInt64>
-    }
-
-    init(wrappedValue: UInt64) {
-        self.wrappedValue = wrappedValue
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-
-        if let compoundWeight = try? container.decode(WeightV1p5.self) {
-            wrappedValue = compoundWeight.refTime.value
-        } else {
-            wrappedValue = try container.decode(WeightV1.self).value
-        }
-    }
-}
-
 struct BlockWeights: Decodable {
-    @ScaleStorageWeight var maxBlock: UInt64
+    @StringCodable var maxBlock: UInt64
 }
