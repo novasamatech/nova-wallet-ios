9470a6fa7398087d44ba753c14966947563ee7ae
ERussel <emkil.russel@gmail.com>
2022-09-21 01:02:16 +0300

    fix transfer all for native asset

--- novawallet/Common/Substrate/Types/BalancesTransferEvent.swift
+++ novawallet/Common/Substrate/Types/BalancesTransferEvent.swift
@@ -3,13 +3,16 @@ import SubstrateSdk
 import BigInt
 
 struct BalancesTransferEvent: Decodable {
-    let accountId: AccountId
+    let sender: AccountId
+    let receiver: AccountId
     let amount: BigUInt
 
     init(from decoder: Decoder) throws {
         var unkeyedContainer = try decoder.unkeyedContainer()
 
-        accountId = try unkeyedContainer.decode(AccountIdCodingWrapper.self).wrappedValue
+        sender = try unkeyedContainer.decode(AccountIdCodingWrapper.self).wrappedValue
+
+        receiver = try unkeyedContainer.decode(AccountIdCodingWrapper.self).wrappedValue
 
         amount = try unkeyedContainer.decode(StringScaleMapper<BigUInt>.self).value
     }
