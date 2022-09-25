a630fe2b2a0b4d6bdaebd436b3631a65acbd09b9
ERussel <emkil.russel@gmail.com>
2022-09-25 13:50:17 +0500

    fix subscriptions

--- novawallet/Common/DataProvider/Subscription/WalletLocalSubscriptionHandler.swift
+++ novawallet/Common/DataProvider/Subscription/WalletLocalSubscriptionHandler.swift
@@ -26,6 +26,13 @@ protocol WalletLocalSubscriptionHandler {
         result: Result<[DataProviderChange<AssetLock>], Error>,
         accountId: AccountId
     )
+
+    func handleAccountLocks(
+        result: Result<[DataProviderChange<AssetLock>], Error>,
+        accountId: AccountId,
+        chainId: ChainModel.Id,
+        assetId: AssetModel.Id
+    )
 }
 
 extension WalletLocalSubscriptionHandler {
@@ -52,5 +59,12 @@ extension WalletLocalSubscriptionHandler {
     func handleAccountLocks(
         result _: Result<[DataProviderChange<AssetLock>], Error>,
         accountId _: AccountId
+    ) {}
+
+    func handleAccountLocks(
+        result _: Result<[DataProviderChange<AssetLock>], Error>,
+        accountId _: AccountId,
+        chainId _: ChainModel.Id,
+        assetId _: AssetModel.Id
     ) {}
 }
