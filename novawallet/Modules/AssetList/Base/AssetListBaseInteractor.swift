a512cff0d74d9508947f7a30002803fcd764f21b
ERussel <emkil.russel@gmail.com>
2022-09-25 17:44:18 +0500

    display total balance including crowdloans

--- novawallet/Modules/AssetList/Base/AssetListBaseInteractor.swift
+++ novawallet/Modules/AssetList/Base/AssetListBaseInteractor.swift
@@ -10,11 +10,17 @@ class AssetListBaseInteractor: WalletLocalStorageSubscriber, WalletLocalSubscrip
     let selectedWalletSettings: SelectedWalletSettings
     let chainRegistry: ChainRegistryProtocol
     let walletLocalSubscriptionFactory: WalletLocalSubscriptionFactoryProtocol
+    let crowdloansLocalSubscriptionFactory: CrowdloanContributionLocalSubscriptionFactoryProtocol
     let priceLocalSubscriptionFactory: PriceProviderFactoryProtocol
     let logger: LoggerProtocol?
 
     private(set) var assetBalanceSubscriptions: [AccountId: StreamableProvider<AssetBalance>] = [:]
     private(set) var assetBalanceIdMapping: [String: AssetBalanceId] = [:]
+
+    private var crowdloansSubscriptions: [ChainModel.Id: StreamableProvider<CrowdloanContributionData>] = [:]
+    private var crowdloans: [ChainModel.Id: [CrowdloanContributionData]] = [:]
+    private var crowdloanChainIds = Set<ChainModel.Id>()
+
     private(set) var priceSubscription: AnySingleValueProvider<[PriceData]>?
     private(set) var availableTokenPrice: [ChainAssetId: AssetModel.PriceId] = [:]
     private(set) var availableChains: [ChainModel.Id: ChainModel] = [:]
@@ -23,6 +29,7 @@ class AssetListBaseInteractor: WalletLocalStorageSubscriber, WalletLocalSubscrip
         selectedWalletSettings: SelectedWalletSettings,
         chainRegistry: ChainRegistryProtocol,
         walletLocalSubscriptionFactory: WalletLocalSubscriptionFactoryProtocol,
+        crowdloansLocalSubscriptionFactory: CrowdloanContributionLocalSubscriptionFactoryProtocol,
         priceLocalSubscriptionFactory: PriceProviderFactoryProtocol,
         currencyManager: CurrencyManagerProtocol,
         logger: LoggerProtocol? = nil
@@ -30,6 +37,7 @@ class AssetListBaseInteractor: WalletLocalStorageSubscriber, WalletLocalSubscrip
         self.selectedWalletSettings = selectedWalletSettings
         self.chainRegistry = chainRegistry
         self.walletLocalSubscriptionFactory = walletLocalSubscriptionFactory
+        self.crowdloansLocalSubscriptionFactory = crowdloansLocalSubscriptionFactory
         self.priceLocalSubscriptionFactory = priceLocalSubscriptionFactory
         self.logger = logger
         self.currencyManager = currencyManager
@@ -40,6 +48,13 @@ class AssetListBaseInteractor: WalletLocalStorageSubscriber, WalletLocalSubscrip
         assetBalanceSubscriptions = [:]
 
         assetBalanceIdMapping = [:]
+    }
+
+    func clearCrowdloansSubscription() {
+        crowdloansSubscriptions.values.forEach { $0.removeObserver(self) }
+        crowdloansSubscriptions = [:]
+        crowdloans = [:]
+        crowdloanChainIds = .init()
     }
 
     private func handle(changes: [DataProviderChange<ChainModel>]) {
@@ -67,6 +82,7 @@ class AssetListBaseInteractor: WalletLocalStorageSubscriber, WalletLocalSubscrip
         updateAvailableChains(from: allChanges)
         updateAccountInfoSubscription(from: accountDependentChanges)
         updatePriceSubscription(from: allChanges)
+        updateCrowdloansSubscription(from: Array(availableChains.values))
     }
 
     func updateAvailableChains(from changes: [DataProviderChange<ChainModel>]) {
@@ -206,6 +222,32 @@ class AssetListBaseInteractor: WalletLocalStorageSubscriber, WalletLocalSubscrip
         )
     }
 
+    func updateCrowdloansSubscription(from allChains: [ChainModel]) {
+        guard let selectedMetaAccount = selectedWalletSettings.value else {
+            return
+        }
+
+        let crowdloanChains = allChains.filter { $0.hasCrowdloans }
+        let newCrowdloanChainIds = Set(crowdloanChains.map(\.chainId))
+
+        guard !crowdloanChains.isEmpty, crowdloanChainIds != newCrowdloanChainIds else {
+            return
+        }
+
+        clearCrowdloansSubscription()
+        crowdloanChainIds = newCrowdloanChainIds
+
+        for chain in crowdloanChains {
+            let request = chain.accountRequest()
+
+            guard let accountId = selectedMetaAccount.fetch(for: request)?.accountId else {
+                return
+            }
+
+            crowdloansSubscriptions[chain.identifier] = subscribeToCrowdloansProvider(for: accountId, chain: chain)
+        }
+    }
+
     func subscribeChains() {
         chainRegistry.chainsSubscribe(self, runningInQueue: .main) { [weak self] changes in
             self?.handle(changes: changes)
@@ -303,6 +345,44 @@ extension AssetListBaseInteractor {
         }
 
         basePresenter?.didReceiveBalance(results: results)
+    }
+}
+
+extension AssetListBaseInteractor: CrowdloanContributionLocalSubscriptionHandler, CrowdloansLocalStorageSubscriber {
+    func handleCrowdloans(
+        result: Result<[DataProviderChange<CrowdloanContributionData>], Error>,
+        accountId: AccountId,
+        chain: ChainModel
+    ) {
+        guard let selectedMetaAccount = selectedWalletSettings.value else {
+            return
+        }
+        guard let chainAccountId = selectedMetaAccount.fetch(
+            for: chain.accountRequest()
+        )?.accountId, chainAccountId == accountId else {
+            logger?.warning("Crowdloans updates can't be handled because account for selected wallet for chain: \(chain.name) is different")
+            return
+        }
+
+        switch result {
+        case let .failure(error):
+            basePresenter?.didReceiveCrowdloans(result: .failure(error))
+        case let .success(changes):
+            crowdloans = changes.reduce(
+                into: crowdloans
+            ) { result, change in
+                switch change {
+                case let .insert(crowdloan), let .update(crowdloan):
+                    var items = result[chain.chainId] ?? []
+                    items.addOrReplaceSingle(crowdloan)
+                    result[chain.chainId] = items
+                case let .delete(deletedIdentifier):
+                    result[chain.chainId]?.removeAll(where: { $0.identifier == deletedIdentifier })
+                }
+            }
+
+            basePresenter?.didReceiveCrowdloans(result: .success(crowdloans))
+        }
     }
 }
 
