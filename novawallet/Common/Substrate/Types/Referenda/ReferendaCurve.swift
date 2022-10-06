6fb9d6273ac92ad70622d4221ea3a4c70fc997ce
ERussel <emkil.russel@gmail.com>
2022-10-06 11:39:07 +0500

    add threshold calculation logic

--- novawallet/Common/Substrate/Types/Referenda/ReferendaCurve.swift
+++ novawallet/Common/Substrate/Types/Referenda/ReferendaCurve.swift
@@ -3,12 +3,22 @@ import SubstrateSdk
 import BigInt
 
 extension Referenda {
+    /**
+     * Linear curve starting at `(0, ceil)`, proceeding linearly to `(length, floor)`, then
+     * remaining at `floor` until the end of the period.
+     */
     struct LinearDecreasingCurve: Decodable {
         @StringCodable var length: BigUInt
         @StringCodable var floor: BigUInt
         @StringCodable var ceil: BigUInt
     }
 
+    /**
+     *  Stepped curve, beginning at `(0, begin)`, then remaining constant for `period`, at which
+     *  point it steps down to `(period, begin - step)`. It then remains constant for another
+     * `period` before stepping down to `(period * 2, begin - step * 2)`. This pattern continues
+     *  but the `y` component has a lower limit of `end`.
+     */
     struct SteppedDecreasingCurve: Decodable {
         @StringCodable var begin: BigUInt
         @StringCodable var end: BigUInt
@@ -16,6 +26,7 @@ extension Referenda {
         @StringCodable var period: BigUInt
     }
 
+    /// A recipocal (`K/(x+S)+T`) curve: `factor` is `K` and `x_offset` is `S`, `y_offset` is `T`.
     struct ReciprocalCurve: Decodable {
         @StringCodable var factor: Int64
         @StringCodable var xOffset: Int64
