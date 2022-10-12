12e1300c18fdfd065fc062bed3cf0bd2bd0d2276
ERussel <emkil.russel@gmail.com>
2022-10-12 11:15:54 +0500

    referendum details update

--- novawallet/Modules/Vote/Governance/ReferendumDetails/ReferendumDetailsProtocols.swift
+++ novawallet/Modules/Vote/Governance/ReferendumDetails/ReferendumDetailsProtocols.swift
@@ -4,8 +4,19 @@ protocol ReferendumDetailsPresenterProtocol: AnyObject {
     func setup()
 }
 
-protocol ReferendumDetailsInteractorInputProtocol: AnyObject {}
+protocol ReferendumDetailsInteractorInputProtocol: AnyObject {
+    func setup()
+}
 
-protocol ReferendumDetailsInteractorOutputProtocol: AnyObject {}
+protocol ReferendumDetailsInteractorOutputProtocol: AnyObject {
+    func didReceiveReferendum(_ referendum: ReferendumLocal)
+    func didReceiveActionDetails(_ actionDetails: ReferendumActionLocal)
+    func didReceiveMetadata(_ referendumMetadata: ReferendumMetadataLocal?)
+    func didReceiveIdentities(_ identities: [AccountId: AccountIdentity])
+    func didReceivePrice(_ price: PriceData?)
+    func didReceiveBlockNumber(_ blockNumber: BlockNumber)
+    func didReceiveBlockTime(_ blockTime: BlockTime)
+    func didReceiveError(_ error: ReferendumDetailsInteractorError)
+}
 
 protocol ReferendumDetailsWireframeProtocol: AnyObject {}
