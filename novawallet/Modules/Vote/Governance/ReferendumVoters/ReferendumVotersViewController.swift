ed8f71dcabd4405e9cd79efe6359fd28297e09af
ERussel <emkil.russel@gmail.com>
2022-10-15 10:37:55 +0500

    add voters screen

--- novawallet/Modules/Vote/Governance/ReferendumVoters/ReferendumVotersViewController.swift
+++ novawallet/Modules/Vote/Governance/ReferendumVoters/ReferendumVotersViewController.swift
@@ -1,13 +1,29 @@
 import UIKit
+import SoraFoundation
 
-final class ReferendumVotersViewController: UIViewController {
+final class ReferendumVotersViewController: UIViewController, ViewHolder {
     typealias RootViewType = ReferendumVotersViewLayout
+    typealias DataSource = UITableViewDiffableDataSource<String, ReferendumVotersViewModel>
+
+    private var dataSource: DataSource?
+    private let votersType: ReferendumVotersType
+    private let quantityFormatter: LocalizableResource<NumberFormatter>
 
     let presenter: ReferendumVotersPresenterProtocol
 
-    init(presenter: ReferendumVotersPresenterProtocol) {
+    init(
+        presenter: ReferendumVotersPresenterProtocol,
+        votersType: ReferendumVotersType,
+        quantityFormatter: LocalizableResource<NumberFormatter>,
+        localizationManager: LocalizationManagerProtocol
+    ) {
         self.presenter = presenter
+        self.votersType = votersType
+        self.quantityFormatter = quantityFormatter
+
         super.init(nibName: nil, bundle: nil)
+
+        self.localizationManager = localizationManager
     }
 
     @available(*, unavailable)
@@ -22,8 +38,82 @@ final class ReferendumVotersViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupLocalization()
+        configureTableView()
+
         presenter.setup()
+    }
+
+    private func configureTableView() {
+        rootView.tableView.registerClassForCell(ReferendumVotersTableViewCell.self)
+        rootView.tableView.delegate = self
+
+        dataSource = DataSource(
+            tableView: rootView.tableView
+        ) { tableView, _, viewModel in
+            let cell = tableView.dequeueReusableCellWithType(ReferendumVotersTableViewCell.self)
+            cell?.bind(viewModel: viewModel)
+            return cell
+        }
+    }
+
+    private func setupLocalization() {
+        switch votersType {
+        case .ayes:
+            title = R.string.localizable.govVotersAye(preferredLanguages: selectedLocale.rLanguages)
+        case .nays:
+            title = R.string.localizable.govVotersNay(preferredLanguages: selectedLocale.rLanguages)
+        }
+    }
+
+    private func setupCounter(value: Int?) {
+        navigationItem.rightBarButtonItem = nil
+
+        let formatter = quantityFormatter.value(for: selectedLocale)
+
+        guard
+            let value = value,
+            let valueString = formatter.string(from: value as NSNumber) else {
+            return
+        }
+
+        rootView.totalVotersLabel.titleLabel.text = valueString
+        rootView.totalVotersLabel.sizeToFit()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rootView.totalVotersLabel)
     }
 }
 
-extension ReferendumVotersViewController: ReferendumVotersViewProtocol {}
+extension ReferendumVotersViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        presenter.selectVoter(at: indexPath.row)
+    }
+}
+
+extension ReferendumVotersViewController: ReferendumVotersViewProtocol {
+    func didReceiveViewModels(_ viewModels: LoadableViewModelState<[ReferendumVotersViewModel]>) {
+        var snapshot = NSDiffableDataSourceSnapshot<String, ReferendumVotersViewModel>()
+
+        switch viewModels {
+        case let .loaded(value), let .cached(value):
+            snapshot.reloadItems(value)
+
+            setupCounter(value: value.count)
+        case .loading:
+            snapshot.reloadItems([])
+            setupCounter(value: nil)
+        }
+
+        dataSource?.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+extension ReferendumVotersViewController: Localizable {
+    func applyLocalization() {
+        if isViewLoaded {
+            setupLocalization()
+        }
+    }
+}
--- novawallet/Modules/Vote/Governance/ReferendumVoters/ReferendumVotersViewLayout.swift
+++ novawallet/Modules/Vote/Governance/ReferendumVoters/ReferendumVotersViewLayout.swift
@@ -1,13 +1,34 @@
 import UIKit
 
 final class ReferendumVotersViewLayout: UIView {
+    let totalVotersLabel: BorderedLabelView = .create { view in
+        view.backgroundView.fillColor = R.color.colorWhite16()!
+        view.titleLabel.apply(style: .init(textColor: R.color.colorWhite80()!, font: .semiBoldFootnote))
+    }
+
+    let tableView: UITableView = .create { view in
+        view.separatorStyle = .none
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        backgroundColor = R.color.colorBlack()!
+
+        setupLayout()
     }
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        addSubview(tableView)
+
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
 }
