9c1356d939f90a1f0aaf4e68accc9073746645fa
ERussel <emkil.russel@gmail.com>
2022-10-16 15:09:35 +0500

    removed unused code

--- novawallet/Modules/Vote/Governance/ReferendumVoters/ViewModel/ReferendumVotersViewModel.swift
+++ novawallet/Modules/Vote/Governance/ReferendumVoters/ViewModel/ReferendumVotersViewModel.swift
@@ -5,13 +5,3 @@ struct ReferendumVotersViewModel {
     let votes: String
     let preConviction: String
 }
-
-extension ReferendumVotersViewModel: Hashable {
-    static func == (lhs: ReferendumVotersViewModel, rhs: ReferendumVotersViewModel) -> Bool {
-        lhs.displayAddress.address == rhs.displayAddress.address
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(displayAddress.address)
-    }
-}
