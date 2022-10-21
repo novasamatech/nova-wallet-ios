47bc18e8496876d6ee13f7f676b82af3d9e5b5a1
ERussel <emkil.russel@gmail.com>
2022-10-21 15:13:09 +0500

    base interactor

--- novawallet/Modules/Vote/Governance/ReferendumVoteConfirm/ReferendumVoteConfirmInteractor.swift
+++ novawallet/Modules/Vote/Governance/ReferendumVoteConfirm/ReferendumVoteConfirmInteractor.swift
@@ -1,7 +1,69 @@
 import UIKit
+import SubstrateSdk
 
-final class ReferendumVoteConfirmInteractor {
-    weak var presenter: ReferendumVoteConfirmInteractorOutputProtocol!
+final class ReferendumVoteConfirmInteractor: ReferendumVoteInteractor {
+    weak var presenter: ReferendumVoteConfirmInteractorOutputProtocol?
+
+    let signer: SigningWrapperProtocol
+
+    init(
+        referendumIndex: ReferendumIdLocal,
+        selectedAccount: MetaChainAccountResponse,
+        chain: ChainModel,
+        generalLocalSubscriptionFactory: GeneralStorageSubscriptionFactoryProtocol,
+        referendumsSubscriptionFactory: GovernanceSubscriptionFactoryProtocol,
+        walletLocalSubscriptionFactory: WalletLocalSubscriptionFactoryProtocol,
+        priceLocalSubscriptionFactory: PriceProviderFactoryProtocol,
+        blockTimeService: BlockTimeEstimationServiceProtocol,
+        connection: JSONRPCEngine,
+        runtimeProvider: RuntimeProviderProtocol,
+        currencyManager: CurrencyManagerProtocol,
+        extrinsicFactory: GovernanceExtrinsicFactoryProtocol,
+        extrinsicService: ExtrinsicServiceProtocol,
+        signer: SigningWrapperProtocol,
+        feeProxy: ExtrinsicFeeProxyProtocol,
+        lockStateFactory: GovernanceLockStateFactoryProtocol,
+        operationQueue: OperationQueue
+    ) {
+        self.signer = signer
+
+        super.init(
+            referendumIndex: referendumIndex,
+            selectedAccount: selectedAccount,
+            chain: chain,
+            generalLocalSubscriptionFactory: generalLocalSubscriptionFactory,
+            referendumsSubscriptionFactory: referendumsSubscriptionFactory,
+            walletLocalSubscriptionFactory: walletLocalSubscriptionFactory,
+            priceLocalSubscriptionFactory: priceLocalSubscriptionFactory,
+            blockTimeService: blockTimeService,
+            connection: connection,
+            runtimeProvider: runtimeProvider,
+            currencyManager: currencyManager,
+            extrinsicFactory: extrinsicFactory,
+            extrinsicService: extrinsicService,
+            feeProxy: feeProxy,
+            lockStateFactory: lockStateFactory,
+            operationQueue: operationQueue
+        )
+    }
 }
 
-extension ReferendumVoteConfirmInteractor: ReferendumVoteConfirmInteractorInputProtocol {}
+extension ReferendumVoteConfirmInteractor: ReferendumVoteConfirmInteractorInputProtocol {
+    func submit(vote: ReferendumVoteAction) {
+        let closure: ExtrinsicBuilderClosure = { [weak self] builder in
+            guard let strongSelf = self else {
+                return builder
+            }
+
+            return try strongSelf.extrinsicFactory.vote(
+                vote,
+                referendum: strongSelf.referendumIndex,
+                builder: builder
+            )
+        }
+
+        extrinsicService.submit(closure, signer: signer, runningIn: .main) { [weak self] result in
+
+        }
+    }
+}
