ef455a43ff444459a729578a16784e9d7a2eeab5
ERussel <emkil.russel@gmail.com>
2022-10-22 11:54:07 +0500

    add confirmation logic

--- novawallet/Modules/Vote/Governance/ReferendumVoteConfirm/ReferendumVoteConfirmViewController.swift
+++ novawallet/Modules/Vote/Governance/ReferendumVoteConfirm/ReferendumVoteConfirmViewController.swift
@@ -1,13 +1,18 @@
 import UIKit
+import SoraFoundation
 
-final class ReferendumVoteConfirmViewController: UIViewController {
+final class ReferendumVoteConfirmViewController: UIViewController, ViewHolder {
     typealias RootViewType = ReferendumVoteConfirmViewLayout
 
     let presenter: ReferendumVoteConfirmPresenterProtocol
 
-    init(presenter: ReferendumVoteConfirmPresenterProtocol) {
+    private var referendumNumber: String?
+
+    init(presenter: ReferendumVoteConfirmPresenterProtocol, localizationManager: LocalizationManagerProtocol) {
         self.presenter = presenter
         super.init(nibName: nil, bundle: nil)
+
+        self.localizationManager = localizationManager
     }
 
     @available(*, unavailable)
@@ -22,8 +27,124 @@ final class ReferendumVoteConfirmViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupHandlers()
+        setupLocalization()
+
         presenter.setup()
+    }
+
+    private func setupHandlers() {
+        rootView.actionLoadableView.actionButton.addTarget(
+            self,
+            action: #selector(actionConfirm),
+            for: .touchUpInside
+        )
+
+        rootView.accountCell.addTarget(
+            self,
+            action: #selector(actionSender),
+            for: .touchUpInside
+        )
+    }
+
+    private func setupLocalization() {
+        let languages = selectedLocale.rLanguages
+
+        applyReferendumNumber()
+
+        rootView.walletCell.titleLabel.text = R.string.localizable.commonWallet(preferredLanguages: languages)
+        rootView.accountCell.titleLabel.text = R.string.localizable.commonSender(preferredLanguages: languages)
+
+        rootView.feeCell.rowContentView.locale = selectedLocale
+
+        rootView.transferableTitleLabel.text = R.string.localizable.walletBalanceAvailable(
+            preferredLanguages: languages
+        )
+
+        rootView.lockAmountTitleLabel.text = R.string.localizable.commonGovLock(preferredLanguages: languages)
+        rootView.lockPeriodTitleLabel.text = R.string.localizable.commonLockingPeriod(preferredLanguages: languages)
+
+        let hint = R.string.localizable.govVoteSetupHint(preferredLanguages: languages)
+        rootView.hintsView.bind(texts: [hint])
+
+        rootView.actionLoadableView.actionButton.imageWithTitleView?.title = R.string.localizable
+            .commonConfirm(preferredLanguages: selectedLocale.rLanguages)
+    }
+
+    private func applyReferendumNumber() {
+        let languages = selectedLocale.rLanguages
+
+        title = R.string.localizable.govVoteSetupTitleFormat(
+            referendumNumber ?? "",
+            preferredLanguages: languages
+        )
+    }
+
+    @objc private func actionConfirm() {
+        presenter.confirm()
+    }
+
+    @objc private func actionSender() {
+        presenter.presentSenderDetails()
     }
 }
 
-extension ReferendumVoteConfirmViewController: ReferendumVoteConfirmViewProtocol {}
+extension ReferendumVoteConfirmViewController: ReferendumVoteConfirmViewProtocol {
+    func didReceive(referendumNumber: String) {
+        self.referendumNumber = referendumNumber
+
+        applyReferendumNumber()
+    }
+
+    func didReceiveAmount(viewModel: BalanceViewModelProtocol) {
+        rootView.amountView.bind(viewModel: viewModel)
+    }
+
+    func didReceiveWallet(viewModel: StackCellViewModel) {
+        rootView.walletCell.bind(viewModel: viewModel)
+    }
+
+    func didReceiveAccount(viewModel: DisplayAddressViewModel) {
+        rootView.accountCell.bind(viewModel: viewModel.cellViewModel)
+    }
+
+    func didReceiveFee(viewModel: BalanceViewModelProtocol?) {
+        rootView.feeCell.rowContentView.bind(viewModel: viewModel)
+    }
+
+    func didReceiveYourVote(viewModel: YourVoteRow.Model) {
+        rootView.yourVoteView.bind(viewModel: viewModel)
+    }
+
+    func didReceiveTransferableAmount(viewModel: ReferendumLockTransitionViewModel) {
+        rootView.transferableCell.bind(viewModel: viewModel)
+    }
+
+    func didReceiveLockedAmount(viewModel: ReferendumLockTransitionViewModel) {
+        rootView.lockedAmountCell.bind(viewModel: viewModel)
+    }
+
+    func didReceiveLockedPeriod(viewModel: ReferendumLockTransitionViewModel) {
+        rootView.lockedPeriodCell.bind(viewModel: viewModel)
+    }
+}
+
+extension ReferendumVoteConfirmViewController: LoadableViewProtocol {
+    func didStartLoading() {
+        rootView.actionLoadableView.startLoading()
+    }
+
+    func didStopLoading() {
+        rootView.actionLoadableView.stopLoading()
+    }
+}
+
+extension ReferendumVoteConfirmViewController: Localizable {
+    func applyLocalization() {
+        if isViewLoaded {
+            setupLocalization()
+        }
+    }
+}
+
+extension ReferendumVoteConfirmViewController: ImportantViewProtocol {}
--- novawallet/Modules/Vote/Governance/ReferendumVoteConfirm/ReferendumVoteConfirmWireframe.swift
+++ novawallet/Modules/Vote/Governance/ReferendumVoteConfirm/ReferendumVoteConfirmWireframe.swift
@@ -1,3 +1,14 @@
 import Foundation
 
-final class ReferendumVoteConfirmWireframe: ReferendumVoteConfirmWireframeProtocol {}
+final class ReferendumVoteConfirmWireframe: ReferendumVoteConfirmWireframeProtocol, ModalAlertPresenting {
+    func complete(on view: ReferendumVoteConfirmViewProtocol?, locale: Locale) {
+        let title = R.string.localizable
+            .commonTransactionSubmitted(preferredLanguages: locale.rLanguages)
+
+        let presenter = view?.controller.navigationController?.presentingViewController
+
+        presenter?.dismiss(animated: true) { [weak self] in
+            self?.presentSuccessNotification(title, from: presenter, completion: nil)
+        }
+    }
+}
