c7ab7f71b129930449e6cb8f140d5f6247ba90ce
ERussel <emkil.russel@gmail.com>
2022-10-30 10:48:39 +0500

    add helper functions

--- novawallet/Modules/Vote/Governance/Model/GovernanceUnlockSchedule.swift
+++ novawallet/Modules/Vote/Governance/Model/GovernanceUnlockSchedule.swift
@@ -14,7 +14,35 @@ struct GovernanceUnlockSchedule {
         let unlockAt: BlockNumber
 
         let actions: Set<Action>
+
+        var isEmpty: Bool {
+            amount == 0 && actions.isEmpty
+        }
+
+        static func emptyUnlock(at block: BlockNumber) -> Item {
+            .init(amount: 0, unlockAt: block, actions: [])
+        }
     }
 
     let items: [Item]
+
+    func lockedBalance() -> BigUInt {
+        items.reduce(BigUInt(0)) { $0 + $1.amount }
+    }
+
+    func availableUnlock(at block: BlockNumber) -> Item {
+        items
+            .filter { $0.unlockAt <= block }
+            .reduce(Item.emptyUnlock(at: block)) { (accum, unlock) in
+                Item(
+                    amount: accum.amount + unlock.amount,
+                    unlockAt: block,
+                    actions: accum.actions.union(unlock.actions)
+                )
+            }
+    }
+
+    func remainingLocks(after block: BlockNumber) -> [Item] {
+        items.filter { $0.unlockAt > block }
+    }
 }
