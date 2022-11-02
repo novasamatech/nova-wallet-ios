5650fbbe8ecd0652be36261008ecd81727b67a71
ERussel <emkil.russel@gmail.com>
2022-11-02 17:04:13 +0500

    interactor add

---
+++ novawallet/Modules/Vote/Governance/GovernanceUnlockConfirm/Model/GovernanceUnlockConfirmInteractorError.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum GovernanceUnlockConfirmInteractorError {
+    case locksSubscriptionFailed(_ internalError: Error)
+    case feeFetchFailed(_ internalError: Error)
+    case unlockFailed(_ internalError: Error)
+}
