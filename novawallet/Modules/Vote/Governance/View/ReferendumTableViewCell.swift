30826b983bc91c3bf05d94345b4a361081a52aaa
Holyberry <666lynx666@mail.ru>
2022-11-10 12:47:22 +0400

    pr fixes

--- novawallet/Modules/Vote/Governance/View/ReferendumTableViewCell.swift
+++ novawallet/Modules/Vote/Governance/View/ReferendumTableViewCell.swift
@@ -98,12 +98,11 @@ extension ReferendumView: SkeletonableView {
     }
 
     func updateLoadingState() {
-        guard let viewModel = viewModel, viewModel.value != nil else {
+        if viewModel?.isLoading == false {
+            stopLoadingIfNeeded()
+        } else {
             startLoadingIfNeeded()
-            return
         }
-
-        stopLoadingIfNeeded()
     }
 
     func createSkeletons(for spaceSize: CGSize) -> [Skeletonable] {
