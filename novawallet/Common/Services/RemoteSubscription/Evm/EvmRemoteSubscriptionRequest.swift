ca2f0205e699ef7ac29e33493939504f55feff7e
ERussel <emkil.russel@gmail.com>
2022-11-16 12:26:00 +0500

    add base erc20 subscription manager

---
+++ novawallet/Common/Services/RemoteSubscription/Evm/EvmRemoteSubscriptionRequest.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum EvmRemoteSubscriptionRequest {
+    case erc20Balace(ERC20BalanceSubscriptionRequest)
+}
+
+struct ERC20BalanceSubscriptionRequest {
+    let holder: AccountAddress
+    let contracts: Set<AccountAddress>
+}
