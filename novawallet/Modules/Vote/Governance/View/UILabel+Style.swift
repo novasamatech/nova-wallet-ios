519a333de20948568f25244a62678c847253c7d0
Holyberry <666lynx666@mail.ru>
2022-11-19 07:16:39 +0400

    update asset list

--- novawallet/Modules/Vote/Governance/View/UILabel+Style.swift
+++ novawallet/Modules/Vote/Governance/View/UILabel+Style.swift
@@ -22,7 +22,7 @@ extension UILabel {
 
 extension RoundedView {
     struct Style {
-        var shadowOpacity: CFloat?
+        var shadow: ShadowShapeView.Style?
         var strokeWidth: CGFloat?
         var strokeColor: UIColor?
         var highlightedStrokeColor: UIColor?
@@ -34,10 +34,55 @@ extension RoundedView {
             let radius: CGFloat
             let corners: UIRectCorner
         }
+
+        init(
+            shadowOpacity: Float? = nil,
+            strokeWidth: CGFloat? = nil,
+            strokeColor: UIColor? = nil,
+            highlightedStrokeColor: UIColor? = nil,
+            fillColor: UIColor,
+            highlightedFillColor: UIColor,
+            rounding: RoundedView.Style.Rounding? = nil
+        ) {
+            if let shadowOpacity = shadowOpacity {
+                shadow = ShadowShapeView.Style(
+                    shadowOpacity: shadowOpacity,
+                    shadowColor: nil,
+                    shadowRadius: nil,
+                    shadowOffset: nil
+                )
+            } else {
+                shadow = nil
+            }
+            self.strokeWidth = strokeWidth
+            self.strokeColor = strokeColor
+            self.highlightedStrokeColor = highlightedStrokeColor
+            self.fillColor = fillColor
+            self.highlightedFillColor = highlightedFillColor
+            self.rounding = rounding
+        }
+
+        init(
+            shadow: ShadowShapeView.Style,
+            strokeWidth: CGFloat? = nil,
+            strokeColor: UIColor? = nil,
+            highlightedStrokeColor: UIColor? = nil,
+            fillColor: UIColor,
+            highlightedFillColor: UIColor,
+            rounding: RoundedView.Style.Rounding? = nil
+        ) {
+            self.shadow = shadow
+            self.strokeWidth = strokeWidth
+            self.strokeColor = strokeColor
+            self.highlightedStrokeColor = highlightedStrokeColor
+            self.fillColor = fillColor
+            self.highlightedFillColor = highlightedFillColor
+            self.rounding = rounding
+        }
     }
 
     func apply(style: Style) {
-        style.shadowOpacity.map { shadowOpacity = $0 }
+        style.shadow.map { apply(style: $0) }
         style.strokeWidth.map { strokeWidth = $0 }
         style.strokeColor.map { strokeColor = $0 }
         style.highlightedStrokeColor.map { highlightedStrokeColor = $0 }
@@ -49,6 +94,22 @@ extension RoundedView {
             roundingCorners = $0.corners
             cornerRadius = $0.radius
         }
+    }
+}
+
+extension ShadowShapeView {
+    struct Style {
+        let shadowOpacity: Float?
+        let shadowColor: UIColor?
+        let shadowRadius: CGFloat?
+        let shadowOffset: CGSize?
+    }
+
+    func apply(style: Style) {
+        style.shadowOpacity.map { shadowOpacity = $0 }
+        style.shadowColor.map { shadowColor = $0 }
+        style.shadowRadius.map { shadowRadius = $0 }
+        style.shadowOffset.map { shadowOffset = $0 }
     }
 }
 
@@ -92,5 +153,36 @@ extension UILabel.Style {
     static let rowLink = UILabel.Style(
         textColor: R.color.colorAccent(),
         font: .p2Paragraph
+    )
+}
+
+extension RoundedView.Style {
+    static let chips = RoundedView.Style(
+        shadowOpacity: 0,
+        strokeWidth: 0,
+        fillColor: R.color.colorChipsBackground()!,
+        highlightedFillColor: R.color.colorChipsBackground()!
+    )
+
+    static let container = RoundedView.Style(
+        shadowOpacity: 0,
+        strokeWidth: 0.5,
+        strokeColor: R.color.colorContainerBorder(),
+        highlightedStrokeColor: R.color.colorContainerBorder(),
+        fillColor: R.color.colorContainerBackground()!,
+        highlightedFillColor: R.color.colorContainerBackground()!
+    )
+
+    static let nft = RoundedView.Style(
+        shadow: .init(
+            shadowOpacity: 1,
+            shadowColor: UIColor.black.withAlphaComponent(0.56),
+            shadowRadius: 4,
+            shadowOffset: .init(width: 4, height: 0)
+        ),
+        strokeWidth: 0,
+        fillColor: .clear,
+        highlightedFillColor: .clear,
+        rounding: .init(radius: 8, corners: .allCorners)
     )
 }
