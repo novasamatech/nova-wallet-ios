49cb5f09684856855355b2e58b8e10cab51cef1c
Holyberry <666lynx666@mail.ru>
2022-11-22 11:06:09 +0400

    clean up

--- novawallet/Common/View/Style/RoundedView+Style.swift
+++ novawallet/Common/View/Style/RoundedView+Style.swift
@@ -1,25 +1,6 @@
 import UIKit
 import SoraUI
 
-extension UILabel {
-    struct Style {
-        let textColor: UIColor?
-        let font: UIFont
-    }
-
-    convenience init(style: Style, textAlignment: NSTextAlignment = .left, numberOfLines: Int = 0) {
-        self.init()
-        self.textAlignment = textAlignment
-        self.numberOfLines = numberOfLines
-        apply(style: style)
-    }
-
-    func apply(style: Style) {
-        textColor = style.textColor
-        font = style.font
-    }
-}
-
 extension RoundedView {
     struct Style {
         var shadow: ShadowShapeView.Style?
@@ -97,70 +78,6 @@ extension RoundedView {
     }
 }
 
-extension ShadowShapeView {
-    struct Style {
-        let shadowOpacity: Float?
-        let shadowColor: UIColor?
-        let shadowRadius: CGFloat?
-        let shadowOffset: CGSize?
-    }
-
-    func apply(style: Style) {
-        style.shadowOpacity.map { shadowOpacity = $0 }
-        style.shadowColor.map { shadowColor = $0 }
-        style.shadowRadius.map { shadowRadius = $0 }
-        style.shadowOffset.map { shadowOffset = $0 }
-    }
-}
-
-extension IconDetailsView {
-    struct Style {
-        let tintColor: UIColor
-        let font: UIFont
-    }
-
-    func apply(style: Style) {
-        detailsLabel.apply(style: .init(textColor: style.tintColor, font: style.font))
-        imageView.tintColor = style.tintColor
-    }
-}
-
-extension IconDetailsView {
-    func bind(viewModel: TitleIconViewModel?) {
-        imageView.image = viewModel?.icon
-        detailsLabel.text = viewModel?.title
-    }
-}
-
-extension UILabel.Style {
-    static let footnotePrimary = UILabel.Style(
-        textColor: R.color.colorTextPrimary(),
-        font: .regularFootnote
-    )
-
-    static let footnoteSecondary = UILabel.Style(
-        textColor: R.color.colorTextSecondary(),
-        font: .regularFootnote
-    )
-
-    static let caption1Secondary = UILabel.Style(
-        textColor: R.color.colorTextSecondary(),
-        font: .caption1
-    )
-
-    static let regularSubhedlinePrimary = UILabel.Style(
-        textColor: R.color.colorTextPrimary(),
-        font: .regularSubheadline
-    )
-}
-
-extension UILabel.Style {
-    static let rowLink = UILabel.Style(
-        textColor: R.color.colorButtonTextAccent(),
-        font: .p2Paragraph
-    )
-}
-
 extension RoundedView.Style {
     static let chips = RoundedView.Style(
         shadowOpacity: 0,
@@ -191,27 +108,6 @@ extension RoundedView.Style {
         highlightedStrokeColor: R.color.colorContainerBorder(),
         fillColor: R.color.colorTokenContainerBackground()!,
         highlightedFillColor: R.color.colorTokenContainerBackground()!
-    )
-
-    static let nft = RoundedView.Style(
-        shadow: .init(
-            shadowOpacity: 1,
-            shadowColor: UIColor.black.withAlphaComponent(0.56),
-            shadowRadius: 4,
-            shadowOffset: .init(width: 4, height: 0)
-        ),
-        strokeWidth: 0,
-        fillColor: .clear,
-        highlightedFillColor: .clear,
-        rounding: .init(radius: 8, corners: .allCorners)
-    )
-
-    static let lastNft = RoundedView.Style(
-        shadowOpacity: 0,
-        strokeWidth: 0,
-        fillColor: .clear,
-        highlightedFillColor: .clear,
-        rounding: .init(radius: 8, corners: .allCorners)
     )
 
     static let textField = RoundedView.Style(
