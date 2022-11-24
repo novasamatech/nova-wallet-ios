0d5bc3f18322c379249fc20c27e197eededae878
ERussel <emkil.russel@gmail.com>
2022-11-24 16:28:50 +0500

    fix background gradient

--- novawallet/Common/View/MultigradientView+Default.swift
+++ novawallet/Common/View/MultigradientView+Default.swift
@@ -2,10 +2,17 @@ import UIKit
 
 extension MultigradientView {
     static var background: MultigradientView {
-        let view = MultigradientView()
+        createBackground(for: .init(x: 0.5, y: 0.13), radius: .init(x: 2.22, y: 0.87))
+    }
 
-        view.startPoint = CGPoint(x: 0.5, y: 0.0)
-        view.endPoint = CGPoint(x: 0.5, y: 1.0)
+    private static func createBackground(for center: CGPoint, radius: CGPoint) -> MultigradientView {
+        let view = MultigradientView()
+        view.gradientType = .radial
+
+        view.startPoint = center
+        view.endPoint = radius
+
+        view.locations = [0.0, 0.46, 1.0]
 
         view.colors = [
             R.color.colorGradientBlockBackgroundFirstPart()!,
