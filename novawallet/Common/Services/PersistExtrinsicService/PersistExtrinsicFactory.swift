4ea921fd48aac99da6aff17d2f9b8fa2e92a8401
ERussel <emkil.russel@gmail.com>
2022-11-30 17:35:53 +0500

    filter tx history by source

--- novawallet/Common/Services/PersistExtrinsicService/PersistExtrinsicFactory.swift
+++ novawallet/Common/Services/PersistExtrinsicService/PersistExtrinsicFactory.swift
@@ -4,24 +4,16 @@ import RobinHood
 
 protocol PersistExtrinsicFactoryProtocol {
     func createTransferSaveOperation(
+        source: TransactionHistoryItemSource,
         chainAssetId: ChainAssetId,
         details: PersistTransferDetails
     ) -> CompoundOperationWrapper<Void>
 
-    func createExecutedTransferSaveOperation(
-        chainAssetId: ChainAssetId,
-        details: PersistTransferDetails,
-        blockNumber: UInt64,
-        timestamp: Int64,
-        status: TransactionHistoryItem.Status
-    ) -> CompoundOperationWrapper<Void>
-
     func createExtrinsicSaveOperation(
+        source: TransactionHistoryItemSource,
         chainAssetId: ChainAssetId,
         details: PersistExtrinsicDetails
     ) -> CompoundOperationWrapper<Void>
-
-    func createExtrinsicDropOperation(for txHash: Data) -> CompoundOperationWrapper<Void>
 }
 
 final class PersistExtrinsicFactory: PersistExtrinsicFactoryProtocol {
@@ -32,6 +24,7 @@ final class PersistExtrinsicFactory: PersistExtrinsicFactoryProtocol {
     }
 
     func createTransferSaveOperation(
+        source: TransactionHistoryItemSource,
         chainAssetId: ChainAssetId,
         details: PersistTransferDetails
     ) -> CompoundOperationWrapper<Void> {
@@ -39,6 +32,7 @@ final class PersistExtrinsicFactory: PersistExtrinsicFactoryProtocol {
         let feeString = details.fee.map { String($0) }
 
         let transferItem = TransactionHistoryItem(
+            source: source,
             chainId: chainAssetId.chainId,
             assetId: chainAssetId.assetId,
             sender: details.sender,
@@ -59,37 +53,8 @@ final class PersistExtrinsicFactory: PersistExtrinsicFactoryProtocol {
         return CompoundOperationWrapper(targetOperation: operation)
     }
 
-    func createExecutedTransferSaveOperation(
-        chainAssetId: ChainAssetId,
-        details: PersistTransferDetails,
-        blockNumber: UInt64,
-        timestamp: Int64,
-        status: TransactionHistoryItem.Status
-    ) -> CompoundOperationWrapper<Void> {
-        let feeString = details.fee.map { String($0) }
-
-        let transferItem = TransactionHistoryItem(
-            chainId: chainAssetId.chainId,
-            assetId: chainAssetId.assetId,
-            sender: details.sender,
-            receiver: details.receiver,
-            amountInPlank: String(details.amount),
-            status: status,
-            txHash: details.txHash.toHex(includePrefix: true),
-            timestamp: timestamp,
-            fee: feeString,
-            blockNumber: blockNumber,
-            txIndex: nil,
-            callPath: details.callPath,
-            call: nil
-        )
-
-        let operation = repository.saveOperation({ [transferItem] }, { [] })
-
-        return CompoundOperationWrapper(targetOperation: operation)
-    }
-
     func createExtrinsicSaveOperation(
+        source: TransactionHistoryItemSource,
         chainAssetId: ChainAssetId,
         details: PersistExtrinsicDetails
     ) -> CompoundOperationWrapper<Void> {
@@ -97,6 +62,7 @@ final class PersistExtrinsicFactory: PersistExtrinsicFactoryProtocol {
         let feeString = details.fee.map { String($0) }
 
         let item = TransactionHistoryItem(
+            source: source,
             chainId: chainAssetId.chainId,
             assetId: chainAssetId.assetId,
             sender: details.sender,
@@ -113,13 +79,6 @@ final class PersistExtrinsicFactory: PersistExtrinsicFactoryProtocol {
         )
 
         let operation = repository.saveOperation({ [item] }, { [] })
-
-        return CompoundOperationWrapper(targetOperation: operation)
-    }
-
-    func createExtrinsicDropOperation(for txHash: Data) -> CompoundOperationWrapper<Void> {
-        let txHashHex = txHash.toHex(includePrefix: true)
-        let operation = repository.saveOperation({ [] }, { [txHashHex] })
 
         return CompoundOperationWrapper(targetOperation: operation)
     }
