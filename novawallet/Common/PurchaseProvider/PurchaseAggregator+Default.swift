9d11a23abb72c040d97a76e4078f6a567bfb4f48
Gulnaz <666lynx666@mail.ru>
2022-12-01 07:54:47 +0400

    Mercuryo buy provider (#469)
    * added new provider
    
    * change buy providers sort
    
    * add mercuryo
    
    * fix debug
    
    Co-authored-by: ERussel <emkil.russel@gmail.com>

--- novawallet/Common/PurchaseProvider/PurchaseAggregator+Default.swift
+++ novawallet/Common/PurchaseProvider/PurchaseAggregator+Default.swift
@@ -4,15 +4,8 @@ extension PurchaseAggregator {
     static func defaultAggregator() -> PurchaseAggregator {
         let config: ApplicationConfigProtocol = ApplicationConfig.shared
 
-        // TODO: Waiting for KYB to get secrets
-        /* let moonpaySecretKeyData = Data(MoonPayKeys.secretKey.utf8)
-         let moonpayProvider = MoonpayProviderFactory().createProvider(
-             with: moonpaySecretKeyData,
-             apiKey: config.moonPayApiKey
-         )*/
-
         let purchaseProviders: [PurchaseProviderProtocol] = [
-            RampProvider(),
+            MercuryoProvider(),
             TransakProvider()
         ]
         return PurchaseAggregator(providers: purchaseProviders)
