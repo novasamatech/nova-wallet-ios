e3b220b2eb9e19b09cb8c1920f3c3b556de71010
ERussel <emkil.russel@gmail.com>
2022-12-04 10:18:36 +0500

    multichain token conversion add

--- novawallet/Modules/TokensManage/TokensManageInteractor.swift
+++ novawallet/Modules/TokensManage/TokensManageInteractor.swift
@@ -1,7 +1,45 @@
 import UIKit
+import RobinHood
 
 final class TokensManageInteractor {
-    weak var presenter: TokensManageInteractorOutputProtocol!
+    weak var presenter: TokensManageInteractorOutputProtocol?
+
+    let chainRegistry: ChainRegistryProtocol
+    let repository: AnyDataProviderRepository<ChainModel>
+    let operationQueue: OperationQueue
+
+    init(
+        chainRegistry: ChainRegistryProtocol,
+        repository: AnyDataProviderRepository<ChainModel>,
+        operationQueue: OperationQueue
+    ) {
+        self.chainRegistry = chainRegistry
+        self.repository = repository
+        self.operationQueue = operationQueue
+    }
+
+    private func subscribeChains() {
+        chainRegistry.chainsSubscribe(
+            self,
+            runningInQueue: .main
+        ) { [weak self] changes in
+            self?.presenter?.didReceiveChainModel(changes: changes)
+        }
+    }
 }
 
-extension TokensManageInteractor: TokensManageInteractorInputProtocol {}
+extension TokensManageInteractor: TokensManageInteractorInputProtocol {
+    func setup() {
+        subscribeChains()
+    }
+
+    func save(chains: [ChainModel]) {
+        let saveOperation = repository.saveOperation({
+            chains
+        }, {
+            []
+        })
+
+        operationQueue.addOperation(saveOperation)
+    }
+}
