2a0d6120f91ebb64280f41822f64b889f6b19914
ERussel <emkil.russel@gmail.com>
2022-12-08 16:11:44 +0500

    fix scrolling

--- novawallet/Modules/TokensManage/ManageSingle/TokenManageSingleViewController.swift
+++ novawallet/Modules/TokensManage/ManageSingle/TokenManageSingleViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
 
 final class TokenManageSingleViewController: UIViewController, ViewHolder {
+    enum Constants {
+        static let bouncesOffset: CGFloat = 100
+    }
+
     typealias RootViewType = TokenManageSingleViewLayout
 
     typealias DataSource = UITableViewDiffableDataSource<UITableView.Section, TokenManageNetworkViewModel>
@@ -32,6 +36,7 @@ final class TokenManageSingleViewController: UIViewController, ViewHolder {
     }
 
     private func setupTableView() {
+        rootView.tableView.delegate = self
         rootView.tableView.rowHeight = TokenManageSingleMeasurement.cellHeight
         rootView.tableView.registerClassForCell(TokenManageInstanceTableViewCell.self)
     }
@@ -58,6 +63,12 @@ extension TokenManageSingleViewController: TokenManageSingleViewProtocol {
 
     func didReceiveTokenManage(viewModel: TokenManageViewModel) {
         rootView.tokenView.bind(viewModel: viewModel)
+    }
+}
+
+extension TokenManageSingleViewController: UITableViewDelegate {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        scrollView.bounces = scrollView.contentOffset.y > UIConstants.bouncesOffset
     }
 }
 
