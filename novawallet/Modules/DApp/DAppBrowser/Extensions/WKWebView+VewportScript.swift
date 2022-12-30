69a4c6ed73455230572d5de5b13aa98258f39f36
Holyberry <666lynx666@mail.ru>
2022-12-30 19:12:34 +0400

    cleanup

--- novawallet/Modules/DApp/DAppBrowser/Extensions/WKWebView+VewportScript.swift
+++ novawallet/Modules/DApp/DAppBrowser/Extensions/WKWebView+VewportScript.swift
@@ -2,7 +2,7 @@ import WebKit
 
 extension WKWebView {
     static var desktopWidth: CGFloat { 1100 }
-    
+
     func viewportScript(for width: CGFloat?, targetWidthInPixels: CGFloat) -> String {
         let scale = UIScreen.main.scale
         let width = width ?? bounds.width
