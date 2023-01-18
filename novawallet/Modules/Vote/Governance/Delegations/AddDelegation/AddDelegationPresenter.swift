389588624ea3d34b903841ee6aec785b9739ba57
Holyberry <666lynx666@mail.ru>
2023-01-19 02:08:58 +0400

    added mocks

--- novawallet/Modules/Vote/Governance/Delegations/AddDelegation/AddDelegationPresenter.swift
+++ novawallet/Modules/Vote/Governance/Delegations/AddDelegation/AddDelegationPresenter.swift
@@ -1,21 +1,100 @@
 import Foundation
+import SoraFoundation
+import RobinHood
+import BigInt
 
 final class AddDelegationPresenter {
     weak var view: AddDelegationViewProtocol?
     let wireframe: AddDelegationWireframeProtocol
     let interactor: AddDelegationInteractorInputProtocol
+    private var delegators: [String: DelegateMetadataLocal] = [:]
+    let numberFormatter = NumberFormatter.quantity.localizableResource()
+    var chain: ChainModel?
 
     init(
         interactor: AddDelegationInteractorInputProtocol,
-        wireframe: AddDelegationWireframeProtocol
+        wireframe: AddDelegationWireframeProtocol,
+        localizationManager: LocalizationManagerProtocol
     ) {
         self.interactor = interactor
         self.wireframe = wireframe
+        self.localizationManager = localizationManager
+    }
+
+    private func updateView() {
+        guard let chainAsset = chain?.utilityAsset() else {
+            return
+        }
+
+        let viewModel = delegators.values.map { convertMetadata($0, chainAsset: chainAsset) }
+        view?.update(viewModel: viewModel)
+    }
+
+    func convertMetadata(_ delegateMetadata: DelegateMetadataLocal, chainAsset: AssetModel) -> DelegateTableViewCell.Model {
+        let url = delegateMetadata.profileImageUrl
+            .map { URL(string: $0) }
+            .map { RemoteImageViewModel(url: $0) }
+
+        return DelegateTableViewCell.Model(
+            id: delegateMetadata.identifier,
+            icon: url,
+            name: delegateMetadata.name,
+            type: delegateMetadata.isOrganization ? .organization : .individual,
+            description: delegateMetadata.shortDescription,
+            delegations: delegateMetadata.stats.map { self.numberFormatter.value(for: self.selectedLocale).string(from: NSNumber(value: $0.delegations)) ?? "" },
+            votes: delegateMetadata.stats.map { formatVotes(
+                votesInPlank: $0.delegatedVotesInPlank,
+                precision: chainAsset.precision
+            ) },
+            lastVotes: delegateMetadata.stats.map { self.numberFormatter.value(for: self.selectedLocale).string(from: NSNumber(value: $0.recentVotes)) ?? "" }
+        )
+    }
+
+    func formatVotes(votesInPlank: BigUInt, precision: UInt16) -> String {
+        guard let votes = Decimal.fromSubstrateAmount(
+            votesInPlank,
+            precision: Int16(precision)
+        ) else {
+            return ""
+        }
+        return numberFormatter.value(for: selectedLocale).stringFromDecimal(votes) ?? ""
     }
 }
 
 extension AddDelegationPresenter: AddDelegationPresenterProtocol {
-    func setup() {}
+    func setup() {
+        interactor.setup()
+        view?.update(showValue: "All accounts")
+        view?.update(sortValue: "Delegations")
+    }
+
+    func selectDelegate(_: DelegateTableViewCell.Model) {}
+
+    func closeBanner() {}
+
+    func showAddDelegateInformation() {}
+
+    func showSortOptions() {}
+
+    func showFilters() {}
 }
 
-extension AddDelegationPresenter: AddDelegationInteractorOutputProtocol {}
+extension AddDelegationPresenter: AddDelegationInteractorOutputProtocol {
+    func didReceive(delegatorsChanges: [DataProviderChange<DelegateMetadataLocal>]) {
+        delegators = delegatorsChanges.mergeToDict(delegators)
+        updateView()
+    }
+
+    func didReceive(chain: ChainModel) {
+        self.chain = chain
+        updateView()
+    }
+}
+
+extension AddDelegationPresenter: Localizable {
+    func applyLocalization() {
+        if view?.isSetup == true {
+            updateView()
+        }
+    }
+}
--- novawallet/Modules/Vote/Governance/Delegations/AddDelegation/AddDelegationViewController.swift
+++ novawallet/Modules/Vote/Governance/Delegations/AddDelegation/AddDelegationViewController.swift
@@ -1,13 +1,20 @@
 import UIKit
+import SoraFoundation
 
 final class AddDelegationViewController: UIViewController, ViewHolder {
     typealias RootViewType = AddDelegationViewLayout
 
     let presenter: AddDelegationPresenterProtocol
+    typealias DataSource = UITableViewDiffableDataSource<UITableView.Section, DelegateTableViewCell.Model>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<UITableView.Section, DelegateTableViewCell.Model>
+    private lazy var dataSource = createDataSource()
+    private var viewModel: [DelegateTableViewCell.Model] = []
 
-    init(presenter: AddDelegationPresenterProtocol) {
+    init(presenter: AddDelegationPresenterProtocol, localizationManager: LocalizationManagerProtocol) {
         self.presenter = presenter
         super.init(nibName: nil, bundle: nil)
+
+        self.localizationManager = localizationManager
     }
 
     @available(*, unavailable)
@@ -22,8 +29,100 @@ final class AddDelegationViewController: UIViewController, ViewHolder {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        rootView.tableView.dataSource = dataSource
+        rootView.tableView.delegate = self
         presenter.setup()
+    }
+
+    private func createDataSource() -> DataSource {
+        .init(tableView: rootView.tableView) { [weak self] tableView, indexPath, _ -> UITableViewCell? in
+            guard let self = self else {
+                return nil
+            }
+
+            let cell: DelegateTableViewCell = tableView.dequeueReusableCell(for: indexPath)
+            let cellModel = self.viewModel[indexPath.row]
+            cell.bind(viewModel: cellModel)
+            return cell
+        }
+    }
+
+    private func setupHandlers() {
+        rootView.filterView.control.addTarget(
+            self,
+            action: #selector(didTapOnFilter),
+            for: .touchUpInside
+        )
+        rootView.sortView.control.addTarget(
+            self,
+            action: #selector(didTapOnSort),
+            for: .touchUpInside
+        )
+        rootView.bannerView.bannerView.linkButton?.addTarget(
+            self,
+            action: #selector(didTapOnBannerLink),
+            for: .touchUpInside
+        )
+        rootView.bannerView.closeButton.addTarget(
+            self,
+            action: #selector(didTapOnCloseBanner),
+            for: .touchUpInside
+        )
+    }
+
+    @objc private func didTapOnFilter() {
+        presenter.showFilters()
+    }
+
+    @objc private func didTapOnSort() {
+        presenter.showSortOptions()
+    }
+
+    @objc private func didTapOnBannerLink() {
+        presenter.showAddDelegateInformation()
+    }
+
+    @objc private func didTapOnCloseBanner() {
+        presenter.closeBanner()
     }
 }
 
-extension AddDelegationViewController: AddDelegationViewProtocol {}
+extension AddDelegationViewController: AddDelegationViewProtocol {
+    func update(viewModel: [DelegateTableViewCell.Model]) {
+        self.viewModel = viewModel
+
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(viewModel)
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+
+    func update(showValue: String) {
+        rootView.filterView.bind(
+            title: "Show:",
+            value: showValue
+        )
+    }
+
+    func update(sortValue: String) {
+        rootView.sortView.bind(
+            title: "Sort by:",
+            value: sortValue
+        )
+    }
+}
+
+extension AddDelegationViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let cellModel = viewModel[indexPath.row]
+        presenter.selectDelegate(cellModel)
+    }
+}
+
+extension AddDelegationViewController: Localizable {
+    func applyLocalization() {
+        // todo
+    }
+}
