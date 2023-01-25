2616bd3a790b5767b038435e2014e45639927999
ERussel <emkil.russel@gmail.com>
2023-01-25 13:47:46 +0300

    fix referendum proposal parsing

--- novawallet/Modules/Vote/Governance/Model/ReferendumLocal.swift
+++ novawallet/Modules/Vote/Governance/Model/ReferendumLocal.swift
@@ -183,7 +183,7 @@ enum ReferendumStateLocal {
 
     struct Preparing {
         let track: GovernanceTrackLocal
-        let proposal: SupportPallet.Bounded<RuntimeCall<JSON>>
+        let proposal: SupportPallet.Bounded<BytesCodable>
         let voting: Voting
         let deposit: BigUInt?
         let since: BlockNumber
@@ -230,7 +230,7 @@ enum ReferendumStateLocal {
         }
     }
 
-    var proposal: SupportPallet.Bounded<RuntimeCall<JSON>>? {
+    var proposal: SupportPallet.Bounded<BytesCodable>? {
         switch self {
         case let .preparing(model):
             return model.proposal
