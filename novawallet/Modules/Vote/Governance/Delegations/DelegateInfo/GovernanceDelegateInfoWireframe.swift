d579f2d27a2b7f9154cd186d6326eb4a18c56961
ERussel <emkil.russel@gmail.com>
2023-01-26 23:20:00 +0300

    add view model

--- novawallet/Modules/Vote/Governance/Delegations/DelegateInfo/GovernanceDelegateInfoWireframe.swift
+++ novawallet/Modules/Vote/Governance/Delegations/DelegateInfo/GovernanceDelegateInfoWireframe.swift
@@ -6,4 +6,32 @@ final class GovernanceDelegateInfoWireframe: GovernanceDelegateInfoWireframeProt
     init(state: GovernanceSharedState) {
         self.state = state
     }
+
+    func showFullDescription(
+        from view: GovernanceDelegateInfoViewProtocol?,
+        longDescription: String
+    ) {
+        // TODO: Task #860pmdtfg
+    }
+
+    func showDelegations(
+        from view: GovernanceDelegateInfoViewProtocol?,
+        delegateAddress: AccountAddress
+    ) {
+        // TODO: Task #860pmdtg1
+    }
+
+    func showRecentVotes(
+        from view: GovernanceDelegateInfoViewProtocol?,
+        delegateAddress: AccountAddress
+    ) {
+        // TODO: Task #860pmdtg6
+    }
+
+    func showAllVotes(
+        from view: GovernanceDelegateInfoViewProtocol?,
+        delegateAddress: AccountAddress
+    ) {
+        // TODO: Task #860pmdtg6
+    }
 }
