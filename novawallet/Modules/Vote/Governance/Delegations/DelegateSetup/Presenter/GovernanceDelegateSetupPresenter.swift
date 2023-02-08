13c42530396fe63866bffd42bc5bc90e6ed2ff4f
ERussel <emkil.russel@gmail.com>
2023-02-08 15:27:41 +0300

    refactoring

---
+++ novawallet/Modules/Vote/Governance/Delegations/DelegateSetup/Presenter/GovernanceDelegateSetupPresenter.swift
@@ -0,0 +1,164 @@
+import Foundation
+import SoraFoundation
+import BigInt
+
+final class GovernanceDelegateSetupPresenter {
+    weak var view: GovernanceDelegateSetupViewProtocol?
+    let wireframe: GovernanceDelegateSetupWireframeProtocol
+    let interactor: GovernanceDelegateSetupInteractorInputProtocol
+
+    let selectedAccountId: AccountId
+    let chain: ChainModel
+    let delegateId: AccountId
+    let tracks: [GovernanceTrackInfoLocal]
+
+    let balanceViewModelFactory: BalanceViewModelFactoryProtocol
+    let chainAssetViewModelFactory: ChainAssetViewModelFactoryProtocol
+    let referendumStringsViewModelFactory: ReferendumDisplayStringFactoryProtocol
+    let lockChangeViewModelFactory: ReferendumLockChangeViewModelFactoryProtocol
+    let dataValidatingFactory: GovernanceValidatorFactoryProtocol
+    let logger: LoggerProtocol
+
+    var assetBalance: AssetBalance?
+    var fee: BigUInt?
+    var priceData: PriceData?
+    var votesResult: CallbackStorageSubscriptionResult<ReferendumTracksVotingDistribution>?
+    var blockNumber: BlockNumber?
+    var blockTime: BlockTime?
+    var lockDiff: GovernanceDelegateStateDiff?
+
+    var inputResult: AmountInputResult?
+    var conviction: ConvictionVoting.Conviction = .none
+
+    init(
+        interactor: GovernanceDelegateSetupInteractorInputProtocol,
+        wireframe: GovernanceDelegateSetupWireframeProtocol,
+        selectedAccountId: AccountId,
+        chain: ChainModel,
+        delegateId: AccountId,
+        tracks: [GovernanceTrackInfoLocal],
+        dataValidatingFactory: GovernanceValidatorFactoryProtocol,
+        balanceViewModelFactory: BalanceViewModelFactoryProtocol,
+        chainAssetViewModelFactory: ChainAssetViewModelFactoryProtocol,
+        referendumStringsViewModelFactory: ReferendumDisplayStringFactoryProtocol,
+        lockChangeViewModelFactory: ReferendumLockChangeViewModelFactoryProtocol,
+        localizationManager: LocalizationManagerProtocol,
+        logger: LoggerProtocol
+    ) {
+        self.interactor = interactor
+        self.wireframe = wireframe
+        self.selectedAccountId = selectedAccountId
+        self.chain = chain
+        self.delegateId = delegateId
+        self.tracks = tracks
+        self.dataValidatingFactory = dataValidatingFactory
+        self.balanceViewModelFactory = balanceViewModelFactory
+        self.chainAssetViewModelFactory = chainAssetViewModelFactory
+        self.referendumStringsViewModelFactory = referendumStringsViewModelFactory
+        self.lockChangeViewModelFactory = lockChangeViewModelFactory
+        self.logger = logger
+        self.localizationManager = localizationManager
+    }
+
+    func balanceMinusFee() -> Decimal {
+        let balanceValue = assetBalance?.freeInPlank ?? 0
+        let feeValue = fee ?? 0
+
+        guard
+            let precision = chain.utilityAsset()?.displayInfo.assetPrecision,
+            let balance = Decimal.fromSubstrateAmount(balanceValue, precision: precision),
+            let fee = Decimal.fromSubstrateAmount(feeValue, precision: precision) else {
+            return 0
+        }
+
+        return balance - fee
+    }
+
+    func deriveNewDelegation() -> GovernanceNewDelegation? {
+        let amount = inputResult?.absoluteValue(from: balanceMinusFee()) ?? 0
+
+        guard
+            let precision = chain.utilityAsset()?.displayInfo.assetPrecision,
+            let amountInPlank = amount.toSubstrateAmount(precision: precision) else {
+            return nil
+        }
+
+        let trackIds = Set(tracks.map(\.trackId))
+
+        return .init(
+            delegateId: delegateId,
+            trackIds: trackIds,
+            balance: amountInPlank,
+            conviction: conviction
+        )
+    }
+
+    func deriveReuseLocks() -> ReferendumReuseLockModel? {
+        let governanceLocksInPlank = lockDiff?.before.maxLockedAmount ?? 0
+        let allLocksInPlank = assetBalance?.frozenInPlank ?? 0
+
+        guard
+            let precision = chain.utilityAssetDisplayInfo()?.assetPrecision,
+            let governanceLockDecimal = Decimal.fromSubstrateAmount(governanceLocksInPlank, precision: precision),
+            let allLockDecimal = Decimal.fromSubstrateAmount(allLocksInPlank, precision: precision) else {
+            return nil
+        }
+
+        return ReferendumReuseLockModel(governance: governanceLockDecimal, all: allLockDecimal)
+    }
+
+    func deriveDelegatorRequest(
+        from newDelegate: GovernanceNewDelegation,
+        voting: ReferendumTracksVotingDistribution
+    ) -> [GovernanceDelegatorAction] {
+        newDelegate.trackIds.map { trackId in
+            var actions: [GovernanceDelegatorAction] = []
+
+            if voting.votes.delegatings[trackId] != nil {
+                actions.append(
+                    .init(
+                        delegateId: newDelegate.delegateId,
+                        trackId: trackId,
+                        type: .undelegate
+                    )
+                )
+            }
+
+            actions.append(
+                .init(
+                    delegateId: newDelegate.delegateId,
+                    trackId: trackId,
+                    type: .delegate(.init(balance: newDelegate.balance, conviction: newDelegate.conviction))
+                )
+            )
+
+            return actions
+        }.flatMap { $0 }
+    }
+
+    func refreshFee() {
+        guard let newDelegation = deriveNewDelegation(), let voting = votesResult?.value else {
+            return
+        }
+
+        let actions = deriveDelegatorRequest(from: newDelegation, voting: voting)
+
+        interactor.estimateFee(for: actions)
+    }
+
+    func refreshLockDiff() {
+        guard let trackVoting = votesResult?.value, let newDelegation = deriveNewDelegation() else {
+            return
+        }
+
+        interactor.refreshDelegateStateDiff(for: trackVoting, newDelegation: newDelegation)
+    }
+
+    func updateAfterAmountChanged() {
+        refreshFee()
+        refreshLockDiff()
+
+        updateVotesView()
+        updateAmountPriceView()
+    }
+}
