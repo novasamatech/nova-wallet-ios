f3792a9999ef1d675ae70f431cdd690e9dc2e27b
ERussel <emkil.russel@gmail.com>
2023-02-15 16:17:51 +0300

    fix confirm navigation

--- novawallet/Modules/Vote/Governance/Delegations/RevokeDelegationConfirm/GovernanceRevokeDelegationConfirmWireframe.swift
+++ novawallet/Modules/Vote/Governance/Delegations/RevokeDelegationConfirm/GovernanceRevokeDelegationConfirmWireframe.swift
@@ -1,13 +1,49 @@
 import Foundation
 
-final class GovRevokeDelegationConfirmWireframe: GovernanceRevokeDelegationConfirmWireframeProtocol {
+final class GovRevokeDelegationConfirmWireframe: GovernanceRevokeDelegationConfirmWireframeProtocol,
+    ModalAlertPresenting {
     let state: GovernanceSharedState
 
     init(state: GovernanceSharedState) {
         self.state = state
     }
 
-    func showTracks(from _: GovernanceRevokeDelegationConfirmViewProtocol?, tracks _: [GovernanceTrackInfoLocal]) {}
+    func showTracks(
+        from _: GovernanceRevokeDelegationConfirmViewProtocol?,
+        tracks _: [GovernanceTrackInfoLocal]
+    ) {
+        // TODO: #860pmdth7
+    }
 
-    func complete(on _: GovernanceRevokeDelegationConfirmViewProtocol?, locale _: Locale) {}
+    func complete(
+        on view: GovernanceRevokeDelegationConfirmViewProtocol?,
+        allRemoved: Bool,
+        locale: Locale
+    ) {
+        let presenter = view?.controller.navigationController
+
+        if allRemoved {
+            presenter?.popToRootViewController(animated: true)
+        } else if
+            let yourDelegations = presenter?.viewControllers.first(
+                where: { $0 is GovernanceYourDelegationsViewProtocol }
+            ) {
+            presenter?.popToViewController(yourDelegations, animated: true)
+        } else {
+            guard let yourDelegations = GovernanceYourDelegationsViewFactory.createView(for: state) else {
+                return
+            }
+
+            yourDelegations.controller.hidesBottomBarWhenPushed = true
+
+            presenter?.popToRootViewController(animated: false)
+
+            presenter?.pushViewController(yourDelegations.controller, animated: true)
+        }
+
+        let title = R.string.localizable
+            .commonTransactionSubmitted(preferredLanguages: locale.rLanguages)
+
+        presentSuccessNotification(title, from: presenter, completion: nil)
+    }
 }
