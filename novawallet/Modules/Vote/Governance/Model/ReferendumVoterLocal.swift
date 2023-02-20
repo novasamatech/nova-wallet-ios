926f5c1deee832682aee2c915423d70b0fe9e39c
Holyberry <666lynx666@mail.ru>
2023-02-20 10:58:08 +0400

    add delegators to referendum voters

--- novawallet/Modules/Vote/Governance/Model/ReferendumVoterLocal.swift
+++ novawallet/Modules/Vote/Governance/Model/ReferendumVoterLocal.swift
@@ -3,4 +3,16 @@ import Foundation
 struct ReferendumVoterLocal {
     let accountId: AccountId
     let vote: ReferendumAccountVoteLocal
+    let delegators: [Delegator]
+
+    struct Delegator {
+        let address: AccountAddress
+        let power: GovernanceOffchainVoting.DelegatorPower
+    }
+
+    init(accountId: AccountId, vote: ReferendumAccountVoteLocal, delegators: [ReferendumVoterLocal.Delegator] = []) {
+        self.accountId = accountId
+        self.vote = vote
+        self.delegators = delegators
+    }
 }
