7f134c4a20e5f81290e71c2d5784cab4b0cf814e
ERussel <emkil.russel@gmail.com>
2023-02-28 10:19:50 +0300

    fetch new min stake data

---
+++ novawallet/Common/Substrate/Types/BagList/BagList+CodingPath.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension BagList {
+    static func bagThresholdsPath(for moduleName: String) -> ConstantCodingPath {
+        .init(moduleName: moduleName, constantName: "BagThresholds")
+    }
+
+    static func bagListSizePath(for moduleName: String) -> StorageCodingPath {
+        .init(moduleName: moduleName, itemName: "CounterForListNodes")
+    }
+}
