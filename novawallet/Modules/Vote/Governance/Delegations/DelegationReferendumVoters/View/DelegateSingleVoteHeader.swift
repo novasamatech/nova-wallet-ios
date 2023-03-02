7e7299d2c4c1da2498208b878d5d92c49024f1ca
Holyberry <666lynx666@mail.ru>
2023-03-02 22:31:45 +0400

    fix layouts

--- novawallet/Modules/Vote/Governance/Delegations/DelegationReferendumVoters/View/DelegateSingleVoteHeader.swift
+++ novawallet/Modules/Vote/Governance/Delegations/DelegationReferendumVoters/View/DelegateSingleVoteHeader.swift
@@ -1,41 +1,37 @@
 import UIKit
 
 final class DelegateSingleVoteHeader: UICollectionReusableView {
-    typealias ContentView = GenericTitleValueView<DelegateInfoView, MultiValueView>
-    let baseView = ContentView()
+    var delegateInfoView = DelegateInfoView()
 
-    var delegateInfoView: DelegateInfoView {
-        baseView.titleView
-    }
-
-    var votesView: MultiValueView {
-        baseView.valueView
+    var votesView: MultiValueView = .create {
+        $0.stackView.alignment = .fill
+        $0.valueTop.apply(style: .footnotePrimary)
+        $0.valueBottom.apply(style: .caption1Secondary)
+        $0.setContentCompressionResistancePriority(.required, for: .horizontal)
+        $0.valueBottom.setContentCompressionResistancePriority(.required, for: .horizontal)
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .clear
 
-        addSubview(baseView)
-        baseView.snp.makeConstraints {
-            $0.edges.equalToSuperview().inset(UIConstants.horizontalInset)
+        addSubview(delegateInfoView)
+        delegateInfoView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalToSuperview().inset(UIConstants.horizontalInset)
         }
 
-        applyStyle()
+        addSubview(votesView)
+        votesView.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
+            $0.centerY.equalToSuperview()
+            $0.leading.greaterThanOrEqualTo(delegateInfoView.snp.trailing).offset(8)
+        }
     }
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    private func applyStyle() {
-        backgroundColor = .clear
-
-        votesView.stackView.alignment = .fill
-        votesView.valueTop.apply(style: .footnotePrimary)
-        votesView.valueBottom.apply(style: .caption1Secondary)
-        votesView.setContentCompressionResistancePriority(.required, for: .horizontal)
-        votesView.valueBottom.setContentCompressionResistancePriority(.required, for: .horizontal)
     }
 }
 
