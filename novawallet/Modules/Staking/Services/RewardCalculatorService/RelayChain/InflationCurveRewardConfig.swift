913c017f160a9d183b25efa4cfc36becd7b4d284
ERussel <emkil.russel@gmail.com>
2023-03-16 12:00:37 +0300

    refactoring

--- novawallet/Modules/Staking/Services/RewardCalculatorService/RelayChain/InflationCurveRewardConfig.swift
+++ novawallet/Modules/Staking/Services/RewardCalculatorService/RelayChain/InflationCurveRewardConfig.swift
@@ -4,10 +4,14 @@ struct InflationCurveRewardConfig {
     let fallof: Decimal = 0.05
     let minAnnualInflation: Decimal = 0.025
     let maxAnnualInflation: Decimal = 0.1
+    let maxParachainsCount: Int = 60
+    let parachainsReserve: Decimal = 0.3
 
     func idealStakePortion(for parachainsCount: Int) -> Decimal {
         // 30% reserved for up to 60 slots
-        let auctionPortion = Decimal(min(parachainsCount, 60)) / 200
+
+        let cappedParachains = min(parachainsCount, maxParachainsCount)
+        let auctionPortion = Decimal(cappedParachains) / Decimal(maxParachainsCount) * parachainsReserve
 
         // Therefore the ideal amount at stake (as a percentage of total issuance) is 75% less the
         // amount that we expect to be taken up with auctions.
