1f79727dce8cde0e09674b740e94b3d4683ffa68
ERussel <emkil.russel@gmail.com>
2023-03-17 11:16:49 +0300

    noSubstrateRuntime flag added

--- novawallet/Common/Migration/SubstrateStorageVersion.swift
+++ novawallet/Common/Migration/SubstrateStorageVersion.swift
@@ -9,6 +9,7 @@ enum SubstrateStorageVersion: String, CaseIterable {
     case version8 = "SubstrateDataModel8"
     case version9 = "SubstrateDataModel9"
     case version10 = "SubstrateDataModel10"
+    case version11 = "SubstrateDataModel11"
 
     static var current: SubstrateStorageVersion {
         allCases.last!
@@ -35,6 +36,8 @@ enum SubstrateStorageVersion: String, CaseIterable {
         case .version9:
             return .version10
         case .version10:
+            return .version11
+        case .version11:
             return nil
         }
     }
