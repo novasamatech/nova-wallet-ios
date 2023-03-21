eb745378ec565e7ed15c0e085af8c460adfcae25
Gulnaz <666lynx666@mail.ru>
2023-03-22 00:34:54 +0300

    KiltTransferAssetRecipientV1 (#607)
    * init
    
    * added service
    
    * cleanup
    
    * pr fixes

---
+++ novawallet/Common/Model/CAIP/CAIP-2/Caip2.swift
@@ -0,0 +1,61 @@
+enum Caip2 {}
+
+extension Caip2 {
+    struct ChainId: Hashable {
+        let namespace: String
+        let reference: String
+
+        init(raw: String) throws {
+            let chain = raw.split(by: .colon)
+            guard chain.count == 2 else {
+                throw ParseError.invalidInputString
+            }
+            let parsedNamespace = chain[0]
+            let parsedReference = chain[1]
+
+            if let namespaceCheckError = parsedNamespace.checkLength(min: 3, max: 8) {
+                throw ParseError.invalidNamespace(namespaceCheckError)
+            }
+            if let referenceCheckError = parsedNamespace.checkLength(min: 1, max: 32) {
+                throw ParseError.invalidReference(referenceCheckError)
+            }
+
+            namespace = parsedNamespace
+            reference = parsedReference
+        }
+    }
+}
+
+extension Caip2 {
+    enum RegisteredChain: Equatable {
+        case polkadot(genesisHash: String)
+        case eip155(id: Int)
+
+        static func == (lhs: Self, rhs: Self) -> Bool {
+            switch (lhs, rhs) {
+            case let (.polkadot(hash), .polkadot(otherHash)):
+                return hash == otherHash
+            case let (.eip155(id), .eip155(otherId)):
+                return id == otherId
+            default:
+                return false
+            }
+        }
+    }
+}
+
+extension Caip2.ChainId {
+    var knownChain: Caip2.RegisteredChain? {
+        switch namespace {
+        case "eip155":
+            guard let id = Int(reference) else {
+                return nil
+            }
+            return .eip155(id: id)
+        case "polkadot":
+            return .polkadot(genesisHash: reference)
+        default:
+            return nil
+        }
+    }
+}
