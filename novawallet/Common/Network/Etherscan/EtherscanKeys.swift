f27cc7df46e746710853d277b9507c61bfd23425
ERussel <emkil.russel@gmail.com>
2023-03-22 23:40:17 +0300

    add infura api key support

--- novawallet/Common/Network/Etherscan/EtherscanKeys.swift
+++ novawallet/Common/Network/Etherscan/EtherscanKeys.swift
@@ -4,21 +4,13 @@ enum EtherscanKeys {
     static func getApiKey(for chainId: String) -> String? {
         switch chainId {
         case KnowChainId.moonbeam:
-            return Self.variable(named: "MOONBEAM_HISTORY_API_KEY") ?? EtherscanCIKeys.moonbeamApiKey
+            return EnviromentVariables.variable(named: "MOONBEAM_HISTORY_API_KEY") ?? EtherscanCIKeys.moonbeamApiKey
         case KnowChainId.moonriver:
-            return Self.variable(named: "MOONRIVER_HISTORY_API_KEY") ?? EtherscanCIKeys.moonriverApiKey
+            return EnviromentVariables.variable(named: "MOONRIVER_HISTORY_API_KEY") ?? EtherscanCIKeys.moonriverApiKey
         case KnowChainId.ethereum:
-            return Self.variable(named: "ETHERSCAN_HISTORY_API_KEY") ?? EtherscanCIKeys.etherscanApiKey
+            return EnviromentVariables.variable(named: "ETHERSCAN_HISTORY_API_KEY") ?? EtherscanCIKeys.etherscanApiKey
         default:
             return nil
         }
-    }
-
-    static func variable(named name: String) -> String? {
-        let processInfo = ProcessInfo.processInfo
-        guard let value = processInfo.environment[name] else {
-            return nil
-        }
-        return value
     }
 }
