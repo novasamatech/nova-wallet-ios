e0aa9d3ceafb34023161b0dfde88e83fa6857d86
ERussel <emkil.russel@gmail.com>
2023-03-23 22:56:59 +0300

    fix tests

--- novawalletTests/Mocks/DataProviders/PriceProviderFactoryStub.swift
+++ novawalletTests/Mocks/DataProviders/PriceProviderFactoryStub.swift
@@ -1,11 +1,57 @@
 import Foundation
 @testable import novawallet
+import RobinHood
 
 final class PriceProviderFactoryStub: PriceProviderFactoryProtocol {
     let priceData: PriceData?
+    let storageFacade: StorageFacadeProtocol
+    let operationQueue: OperationQueue
 
-    init(priceData: PriceData? = nil) {
+    init(
+        priceData: PriceData? = nil,
+        storageFacade: StorageFacadeProtocol = SubstrateStorageTestFacade(),
+        operationQueue: OperationQueue = OperationQueue()
+    ) {
         self.priceData = priceData
+        self.storageFacade = storageFacade
+        self.operationQueue = operationQueue
+    }
+
+    func getPriceStreamableProvider(
+        for priceId: AssetModel.PriceId,
+        currency: Currency
+    ) -> StreamableProvider<PriceData> {
+        createProvider(from: createRepository())
+    }
+
+    func getAllPricesStreamableProvider(
+        for priceIds: [AssetModel.PriceId],
+        currency: Currency
+    ) -> StreamableProvider<PriceData> {
+        createProvider(from: createRepository())
+    }
+
+    private func createRepository() -> AnyDataProviderRepository<PriceData> {
+        let repository = storageFacade.createRepository(mapper: AnyCoreDataMapper(PriceDataMapper()))
+
+        if let priceData = priceData {
+            let operation = repository.saveOperation({
+                [priceData]
+            }, { [] })
+
+            operationQueue.addOperations([operation], waitUntilFinished: true)
+        }
+
+        return AnyDataProviderRepository(repository)
+    }
+
+    private func createProvider(from repository: AnyDataProviderRepository<PriceData>) -> StreamableProvider<PriceData> {
+        return StreamableProvider(
+            source: AnyStreamableSource(EmptyStreamableSource()),
+            repository: repository,
+            observable: AnyDataProviderRepositoryObservable(DataProviderObservableStub()),
+            operationManager: OperationManager(operationQueue: operationQueue)
+        )
     }
 
     func getPriceProvider(for priceId: AssetModel.PriceId, currency: Currency) -> AnySingleValueProvider<PriceData> {
