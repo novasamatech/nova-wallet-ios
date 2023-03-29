2d2ea08f867fa5875c2822ca7e067b8d7f2f13fa
ERussel <emkil.russel@gmail.com>
2023-03-29 09:32:19 +0300

    add implementation for transaction provider

--- novawallet/Common/DataProvider/TransactionLocalSubscriptionFactory.swift
+++ novawallet/Common/DataProvider/TransactionLocalSubscriptionFactory.swift
@@ -6,6 +6,20 @@ protocol TransactionLocalSubscriptionFactoryProtocol {
         _ txId: String,
         chainId: ChainModel.Id
     ) -> StreamableProvider<TransactionHistoryItem>
+
+    func getUtilityAssetTransactionsProvider(
+        for source: TransactionHistoryItemSource,
+        address: AccountAddress,
+        chainAssetId: ChainAssetId,
+        filter: WalletHistoryFilter?
+    ) -> StreamableProvider<TransactionHistoryItem>
+
+    func getCustomAssetTransactionsProvider(
+        for source: TransactionHistoryItemSource,
+        address: AccountAddress,
+        chainAssetId: ChainAssetId,
+        filter: WalletHistoryFilter?
+    ) -> StreamableProvider<TransactionHistoryItem>
 }
 
 final class TransactionLocalSubscriptionFactory {
@@ -33,6 +47,21 @@ final class TransactionLocalSubscriptionFactory {
         chainId + txId
     }
 
+    private func createKey(
+        from source: TransactionHistoryItemSource,
+        address: AccountAddress,
+        chainAssetId: ChainAssetId,
+        filter: WalletHistoryFilter?
+    ) -> String {
+        let key = String(source.rawValue) + chainAssetId.chainId + String(chainAssetId.assetId) + address
+
+        if let filter = filter {
+            return key + String(filter.rawValue)
+        } else {
+            return key
+        }
+    }
+
     func getProvider(
         for txId: String,
         chainId: ChainModel.Id
@@ -49,6 +78,56 @@ final class TransactionLocalSubscriptionFactory {
         let key = createKey(from: txId, chainId: chainId)
         providerStore[key] = WeakWrapper(target: provider)
     }
+
+    func getProvider(
+        for source: TransactionHistoryItemSource,
+        address: AccountAddress,
+        chainAssetId: ChainAssetId,
+        filter: WalletHistoryFilter?
+    ) -> StreamableProvider<TransactionHistoryItem>? {
+        let key = createKey(from: source, address: address, chainAssetId: chainAssetId, filter: filter)
+        return providerStore[key]?.target as? StreamableProvider<TransactionHistoryItem>
+    }
+
+    func storeProvider(
+        _ provider: StreamableProvider<TransactionHistoryItem>,
+        source: TransactionHistoryItemSource,
+        address: AccountAddress,
+        chainAssetId: ChainAssetId,
+        filter: WalletHistoryFilter?
+    ) {
+        let key = createKey(from: source, address: address, chainAssetId: chainAssetId, filter: filter)
+        providerStore[key] = WeakWrapper(target: provider)
+    }
+
+    func createTransactionsProvider(
+        for modelFilter: NSPredicate,
+        entityFilter: @escaping (CDTransactionItem) -> Bool
+    ) -> StreamableProvider<TransactionHistoryItem> {
+        let repository: CoreDataRepository<TransactionHistoryItem, CDTransactionItem>
+            = storageFacade.createRepository(filter: modelFilter)
+
+        let observable = CoreDataContextObservable(
+            service: storageFacade.databaseService,
+            mapper: AnyCoreDataMapper(repository.dataMapper),
+            predicate: entityFilter
+        )
+
+        observable.start { [weak self] error in
+            if let error = error {
+                self?.logger?.error("Did receive error: \(error)")
+            }
+        }
+
+        let source = EmptyStreamableSource<TransactionHistoryItem>()
+
+        return StreamableProvider(
+            source: AnyStreamableSource(source),
+            repository: AnyDataProviderRepository(repository),
+            observable: AnyDataProviderRepositoryObservable(observable),
+            operationManager: OperationManager(operationQueue: operationQueue)
+        )
+    }
 }
 
 extension TransactionLocalSubscriptionFactory: TransactionLocalSubscriptionFactoryProtocol {
@@ -62,35 +141,105 @@ extension TransactionLocalSubscriptionFactory: TransactionLocalSubscriptionFacto
             return provider
         }
 
-        let filter = NSPredicate.filterTransactionsBy(transactionId: txId)
-        let repository: CoreDataRepository<TransactionHistoryItem, CDTransactionItem>
-            = storageFacade.createRepository(filter: filter)
-
-        let observable = CoreDataContextObservable(
-            service: storageFacade.databaseService,
-            mapper: AnyCoreDataMapper(repository.dataMapper),
-            predicate: { entity in
-                entity.chainId == chainId &&
-                    entity.identifier == txId
+        let provider = createTransactionsProvider(
+            for: NSPredicate.filterTransactionsBy(transactionId: txId),
+            entityFilter: { entity in
+                entity.chainId == chainId && entity.identifier == txId
             }
-        )
-
-        observable.start { [weak self] error in
-            if let error = error {
-                self?.logger?.error("Did receive error: \(error)")
-            }
-        }
-
-        let source = EmptyStreamableSource<TransactionHistoryItem>()
-
-        let provider = StreamableProvider(
-            source: AnyStreamableSource(source),
-            repository: AnyDataProviderRepository(repository),
-            observable: AnyDataProviderRepositoryObservable(observable),
-            operationManager: OperationManager(operationQueue: operationQueue)
         )
 
         storeProvider(provider, txId: txId, chainId: chainId)
+
+        return provider
+    }
+
+    func getUtilityAssetTransactionsProvider(
+        for source: TransactionHistoryItemSource,
+        address: AccountAddress,
+        chainAssetId: ChainAssetId,
+        filter: WalletHistoryFilter?
+    ) -> StreamableProvider<TransactionHistoryItem> {
+        runStoreCleaner()
+
+        if let provider = getProvider(for: source, address: address, chainAssetId: chainAssetId, filter: filter) {
+            return provider
+        }
+
+        let predicate = NSPredicate.filterUtilityAssetTransactionsBy(
+            address: address,
+            chainId: chainAssetId.chainId,
+            utilityAssetId: chainAssetId.assetId,
+            source: source,
+            filter: filter
+        )
+
+        let provider = createTransactionsProvider(
+            for: predicate,
+            entityFilter: { entity in
+                predicate.evaluate(with: entity)
+                /* let baseMatching = entity.chainId == chainAssetId.chainId &&
+                     entity.source == source.rawValue &&
+                     (entity.sender == address || (entity.assetId == chainAssetId.assetId && entity.receiver == address))
+
+                 if let filter = filter {
+                     let filterMatching = CallCodingPath(
+                         moduleName: entity.moduleName ?? "",
+                         callName: entity.callName ?? ""
+                     ).matches(filter: filter)
+                     return baseMatching && filterMatching
+                 } else {
+                     return baseMatching
+                 } */
+            }
+        )
+
+        storeProvider(provider, source: source, address: address, chainAssetId: chainAssetId, filter: filter)
+
+        return provider
+    }
+
+    func getCustomAssetTransactionsProvider(
+        for source: TransactionHistoryItemSource,
+        address: AccountAddress,
+        chainAssetId: ChainAssetId,
+        filter: WalletHistoryFilter?
+    ) -> StreamableProvider<TransactionHistoryItem> {
+        runStoreCleaner()
+
+        if let provider = getProvider(for: source, address: address, chainAssetId: chainAssetId, filter: filter) {
+            return provider
+        }
+
+        let predicate = NSPredicate.filterTransactionsBy(
+            address: address,
+            chainId: chainAssetId.chainId,
+            assetId: chainAssetId.assetId,
+            source: source,
+            filter: filter
+        )
+
+        let provider = createTransactionsProvider(
+            for: predicate,
+            entityFilter: { entity in
+                predicate.evaluate(with: entity)
+                /* let baseMatching = entity.chainId == chainAssetId.chainId &&
+                     entity.assetId == chainAssetId.assetId &&
+                     entity.source == source.rawValue &&
+                     (entity.sender == address || entity.receiver == address)
+
+                 if let filter = filter {
+                     let filterMatching = CallCodingPath(
+                         moduleName: entity.moduleName ?? "",
+                         callName: entity.callName ?? ""
+                     ).matches(filter: filter)
+                     return baseMatching && filterMatching
+                 } else {
+                     return baseMatching
+                 } */
+            }
+        )
+
+        storeProvider(provider, source: source, address: address, chainAssetId: chainAssetId, filter: filter)
 
         return provider
     }
