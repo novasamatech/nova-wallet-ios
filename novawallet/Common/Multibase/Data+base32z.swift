5e14a585898e0b600d91a1e6de5ccded8b43c2a5
Holyberry <666lynx666@mail.ru>
2023-03-31 23:43:20 +0400

    fixes

---
+++ novawallet/Common/Multibase/Data+base32z.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+extension String {
+    func base32zDecodedData() -> Data? {
+        let base32zAlphabet = "ybndrfg8ejkmcpqxot1uwisza345h769"
+        var bits: UInt64 = 0
+        var bitsRemaining = 0
+        var decodedBytes = [UInt8]()
+
+        for character in self {
+            guard let value = base32zAlphabet.firstIndex(of: character) else { return nil }
+            bits = (bits << 5) | UInt64(base32zAlphabet.position(value))
+            bitsRemaining += 5
+
+            if bitsRemaining >= 8 {
+                let byte = UInt8(truncatingIfNeeded: bits >> (bitsRemaining - 8))
+                decodedBytes.append(byte)
+                bitsRemaining -= 8
+            }
+        }
+
+        if bitsRemaining > 0 {
+            let finalByte = UInt8(truncatingIfNeeded: bits << (8 - bitsRemaining))
+            decodedBytes.append(finalByte)
+        }
+
+        return Data(decodedBytes)
+    }
+}
