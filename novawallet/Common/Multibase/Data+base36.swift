9308be929a83e14e12eaeb84e83cba1f75c50fbb
Holyberry <666lynx666@mail.ru>
2023-04-05 06:26:51 +0400

    clean up

--- novawallet/Common/Multibase/Data+base36.swift
+++ novawallet/Common/Multibase/Data+base36.swift
@@ -3,26 +3,23 @@ import BigInt
 
 extension Data {
     init?(base36Encoded input: String) {
-        let charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let charset = [UInt8]("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".utf8)
         let base = BigUInt(charset.count)
         var result = BigUInt(0)
         var power = BigUInt(1)
 
-        let leadingZeroCount = Swift.max(input.prefix(while: { $0 == "0" }).count, 0)
+        let leadingZeroCount = input.prefix(while: { $0 == "0" }).count
 
-        for char in input.uppercased().reversed() {
+        for char in input.uppercased().utf8.reversed() {
             guard let index = charset.firstIndex(of: char) else {
                 return nil
             }
-            result += power * BigUInt(charset.position(index))
+            result += power * BigUInt(index)
             power *= base
         }
 
-        var bytes = result.serialize().drop(while: { $0 == 0 })
-        if leadingZeroCount > 0 {
-            bytes = Array(repeating: 0, count: leadingZeroCount) + bytes
-        }
+        let bytes = result.serialize().drop(while: { $0 == 0 })
 
-        self = bytes
+        self = Array(repeating: 0, count: leadingZeroCount) + bytes
     }
 }
