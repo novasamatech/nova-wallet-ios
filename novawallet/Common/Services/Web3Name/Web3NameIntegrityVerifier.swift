6b9a551cbea7052b3c4afe3062b4691bf600a875
Holyberry <666lynx666@mail.ru>
2023-04-06 15:25:47 +0400

    PR fixes, fix retrieve check interity hash

--- novawallet/Common/Services/Web3Name/Web3NameIntegrityVerifier.swift
+++ novawallet/Common/Services/Web3Name/Web3NameIntegrityVerifier.swift
@@ -6,14 +6,11 @@ protocol Web3NameIntegrityVerifierProtocol {
 
 final class Web3NameIntegrityVerifier: Web3NameIntegrityVerifierProtocol {
     func verify(serviceEndpointId: String, serviceEndpointContent: String) -> Bool {
-        guard let expectedHash = serviceEndpointId.split(by: .hashtag, maxSplits: 1)[safe: 1] else {
-            return false
-        }
         guard let actualHash = try? serviceEndpointContent.data(using: .utf8)?.blake2b32() else {
             return false
         }
 
-        let decodedExpectedHash = Data(multibaseEncoded: expectedHash)
+        let decodedExpectedHash = Data(multibaseEncoded: serviceEndpointId)
 
         return decodedExpectedHash == actualHash
     }
