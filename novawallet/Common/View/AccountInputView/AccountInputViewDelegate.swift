0683030294cc1790eba4def227b9db47eec2ab02
Gulnaz <666lynx666@mail.ru>
2023-04-07 15:19:24 +0400

    Bug fixes (#631)
    * init
    
    * bugfixes
    
    * fixes
    
    * bugfixes, renaming

--- novawallet/Common/View/AccountInputView/AccountInputViewDelegate.swift
+++ novawallet/Common/View/AccountInputView/AccountInputViewDelegate.swift
@@ -4,4 +4,5 @@ protocol AccountInputViewDelegate: AnyObject {
     func accountInputViewWillStartEditing(_ inputView: AccountInputView)
     func accountInputViewShouldReturn(_ inputView: AccountInputView) -> Bool
     func accountInputViewDidEndEditing(_ inputView: AccountInputView)
+    func accountInputViewDidPaste(_ inputView: AccountInputView)
 }
