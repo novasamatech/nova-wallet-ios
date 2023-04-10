2e2acb6963e120bed59cc816f0fdb2f7403fce37
ERussel <emkil.russel@gmail.com>
2023-04-10 17:37:32 +0300

    refactor tokens matching

--- novawallet/Common/Services/Web3Name/Slip44CoinList.swift
+++ novawallet/Common/Services/Web3Name/Slip44CoinList.swift
@@ -4,3 +4,18 @@ struct Slip44Coin: Codable, Equatable {
     let index: String
     let symbol: String
 }
+
+extension Slip44CoinList {
+    func matchFirstCaip19(of symbols: [String]) -> Caip19.RegisteredToken? {
+        first { coin in
+            symbols.contains { $0.caseInsensitiveCompare(coin.symbol) == .orderedSame }
+        }
+        .flatMap { coin in
+            guard let index = Caip19.Slip44CoinIndex(coin.index) else {
+                return nil
+            }
+
+            return .slip44(coin: index)
+        }
+    }
+}
