95e527ba9464b14bf9e2d8d4576f1c7b24d463dc
ERussel <emkil.russel@gmail.com>
2023-04-17 15:39:38 +0300

    use more specific fallbacks

---
+++ novawallet/Common/Services/ExtrinsicService/Evm/EvmFallbackGasLimit.swift
@@ -0,0 +1,20 @@
+import Foundation
+import BigInt
+
+enum EvmFallbackGasLimit {
+    static let erc20: BigUInt = 200_000
+    static let native: BigUInt = 21000
+
+    static func value(for asset: AssetModel) -> BigUInt {
+        let type = asset.type.flatMap { AssetType(rawValue: $0) }
+
+        switch type {
+        case .evmAsset:
+            return Self.erc20
+        case .evmNative:
+            return Self.native
+        case .none, .statemine, .orml:
+            return 0
+        }
+    }
+}
