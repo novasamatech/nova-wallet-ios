b70693afdc6084b3f1d618687659b2627900be05
Holyberry <666lynx666@mail.ru>
2023-04-18 07:07:43 +0400

    fix reserves

--- novawallet/Common/Services/RemoteSubscription/Equilibrium/Subscriptions/EquilibriumReservedSubscription.swift
+++ novawallet/Common/Services/RemoteSubscription/Equilibrium/Subscriptions/EquilibriumReservedSubscription.swift
@@ -3,20 +3,23 @@ import Foundation
 final class EquilibriumReservedSubscription: StorageChildSubscribing {
     let remoteStorageKey: Data
     let balanceUpdater: EquillibriumAssetsBalanceUpdaterProtocol
+    let assetId: EquilibriumAssetId
     let logger: LoggerProtocol
 
     init(
         remoteStorageKey: Data,
+        assetId: EquilibriumAssetId,
         balanceUpdater: EquillibriumAssetsBalanceUpdaterProtocol,
         logger: LoggerProtocol
     ) {
         self.remoteStorageKey = remoteStorageKey
         self.balanceUpdater = balanceUpdater
+        self.assetId = assetId
         self.logger = logger
     }
 
     func processUpdate(_ data: Data?, blockHash: Data?) {
         logger.debug("Did receive asset account update")
-        balanceUpdater.handleReservedBalance(value: data, blockHash: blockHash)
+        balanceUpdater.handleReservedBalance(value: data, assetId: assetId, blockHash: blockHash)
     }
 }
