8e51c1e900c0384ac6c0f67ae6d7b76e5faa596e
Holyberry <666lynx666@mail.ru>
2023-04-22 16:19:43 +0400

    cleanup

--- novawallet/Modules/Settings/View/SwitchSettingsTableViewCell.swift
+++ novawallet/Modules/Settings/View/SwitchSettingsTableViewCell.swift
@@ -9,19 +9,21 @@ final class SwitchSettingsTableViewCell: CommonSettingsTableViewCell<UISwitch> {
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        rightView.addTarget(self, action: #selector(actionSwitch), for: .valueChanged)
+        setup()
     }
 
     private var viewModel: SwitchSettingsCellViewModel?
 
-    @objc private func actionSwitch(_: UISwitch) {
+    @objc private func actionSwitch() {
         delegate?.didChangeSwitchValue(model: viewModel)
     }
 
-    func setup() {
+    private func setup() {
         rightView.tintColor = R.color.colorSwitchBackground()
         rightView.onTintColor = R.color.colorIndicatorActive()
         rightView.thumbTintColor = R.color.colorIconPrimary()
+        rightView.addTarget(self, action: #selector(actionSwitch), for: .valueChanged)
+        selectionStyle = .none
     }
 
     func bind(viewModel: SwitchSettingsCellViewModel) {
