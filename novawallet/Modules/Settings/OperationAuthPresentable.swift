47f04c8b2d7635d91952b63c6948f7ccd02ee373
Holyberry <666lynx666@mail.ru>
2023-04-26 06:30:58 +0400

    PR fixes

---
+++ novawallet/Modules/Settings/OperationAuthPresentable.swift
@@ -0,0 +1,9 @@
+//
+//  OperationAuthPresentable.swift
+//  novawallet
+//
+//  Created by Gulnaz Almuhametova on 26.04.2023.
+//  Copyright © 2023 Nova Foundation. All rights reserved.
+//
+
+import Foundation
