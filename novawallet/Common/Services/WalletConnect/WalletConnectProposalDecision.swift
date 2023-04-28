da2ac1531075700811475aff5bf8741d6f054be9
ERussel <emkil.russel@gmail.com>
2023-04-28 11:02:37 +0500

    add signing logic

--- novawallet/Common/Services/WalletConnect/WalletConnectProposalDecision.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import WalletConnectSwiftV2
-
-enum WalletConnectProposalDecision {
-    case approve(proposal: Session.Proposal, namespaces: [String: SessionNamespace])
-    case reject(proposal: Session.Proposal)
-}
