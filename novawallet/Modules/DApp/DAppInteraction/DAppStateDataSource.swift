90906f0adf454a2f36166d96a37c157e0c46f48e
ERussel <emkil.russel@gmail.com>
2023-05-03 18:02:25 +0500

    save wallet connect session

--- novawallet/Modules/DApp/DAppInteraction/DAppStateDataSource.swift
+++ novawallet/Modules/DApp/DAppInteraction/DAppStateDataSource.swift
@@ -4,17 +4,20 @@ import RobinHood
 final class DAppStateDataSource {
     let chainsStore: ChainsStoreProtocol
     let dAppSettingsRepository: AnyDataProviderRepository<DAppSettings>
+    let walletsRepository: AnyDataProviderRepository<MetaAccountModel>
     let operationQueue: OperationQueue
     let walletSettings: SelectedWalletSettings
 
     init(
         chainsStore: ChainsStoreProtocol,
         dAppSettingsRepository: AnyDataProviderRepository<DAppSettings>,
+        walletsRepository: AnyDataProviderRepository<MetaAccountModel>,
         walletSettings: SelectedWalletSettings,
         operationQueue: OperationQueue
     ) {
         self.chainsStore = chainsStore
         self.dAppSettingsRepository = dAppSettingsRepository
+        self.walletsRepository = walletsRepository
         self.walletSettings = walletSettings
         self.operationQueue = operationQueue
     }
