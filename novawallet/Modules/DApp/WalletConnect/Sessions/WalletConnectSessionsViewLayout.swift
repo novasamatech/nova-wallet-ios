1b21a547de31765af82ca76506073683cbce4a68
ERussel <emkil.russel@gmail.com>
2023-05-03 18:02:25 +0500

    save wallet connect session

--- novawallet/Modules/DApp/WalletConnect/Sessions/WalletConnectSessionsViewLayout.swift
+++ novawallet/Modules/DApp/WalletConnect/Sessions/WalletConnectSessionsViewLayout.swift
@@ -1,12 +1,51 @@
 import UIKit
+import SoraUI
 
 final class WalletConnectSessionsViewLayout: UIView {
+    let scanButton: TriangularedButton = {
+        let button = TriangularedButton()
+        button.applyDefaultStyle()
+        button.changesContentOpacityWhenHighlighted = true
+        button.imageWithTitleView?.iconImage = R.image.iconButtonScan()
+        button.imageWithTitleView?.spacingBetweenLabelAndIcon = 8
+        return button
+    }()
+
+    let tableView: UITableView = {
+        let view = UITableView()
+        view.separatorStyle = .none
+        view.backgroundColor = .clear
+        return view
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        backgroundColor = R.color.colorSecondaryScreenBackground()
+
+        setupLayout()
     }
 
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+
+        addSubview(scanButton)
+        scanButton.snp.makeConstraints { make in
+            make.height.equalTo(UIConstants.actionHeight)
+            make.leading.trailing.equalToSuperview().inset(UIConstants.horizontalInset)
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(UIConstants.actionBottomInset)
+        }
+
+        let bottomInset = UIConstants.actionBottomInset + UIConstants.actionHeight + 16.0
+        tableView.contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: bottomInset, right: 0.0)
     }
 }
--- novawallet/Modules/DApp/WalletConnect/States/WalletConnectStateAuthorizing.swift
+++ novawallet/Modules/DApp/WalletConnect/States/WalletConnectStateAuthorizing.swift
@@ -15,6 +15,47 @@ class WalletConnectStateAuthorizing: WalletConnectBaseState {
 
         super.init(stateMachine: stateMachine)
     }
+
+    private func save(
+        authResponse: DAppAuthResponse,
+        pairingId: String,
+        dataSource: DAppStateDataSource,
+        completion: @escaping (Error?) -> Void
+    ) {
+        let saveOperation = dataSource.dAppSettingsRepository.saveOperation({
+            if authResponse.approved {
+                let settings = DAppSettings(
+                    identifier: pairingId,
+                    metaId: authResponse.wallet.metaId,
+                    source: DAppTransports.walletConnect
+                )
+
+                return [settings]
+            } else {
+                return []
+            }
+        }, {
+            if !authResponse.approved {
+                return [pairingId]
+            } else {
+                return []
+            }
+        })
+
+        saveOperation.completionBlock = {
+            DispatchQueue.main.async {
+                do {
+                    try saveOperation.extractNoCancellableResultData()
+
+                    completion(nil)
+                } catch {
+                    completion(error)
+                }
+            }
+        }
+
+        dataSource.operationQueue.addOperation(saveOperation)
+    }
 }
 
 extension WalletConnectStateAuthorizing: WalletConnectStateProtocol {
@@ -30,28 +71,44 @@ extension WalletConnectStateAuthorizing: WalletConnectStateProtocol {
         emitUnexpected(message: response, nextState: self)
     }
 
-    func handleAuth(response: DAppAuthResponse, dataSource _: DAppStateDataSource) {
+    func handleAuth(response: DAppAuthResponse, dataSource: DAppStateDataSource) {
         guard let stateMachine = stateMachine else {
             return
         }
 
         let nextState = WalletConnectStateReady(stateMachine: stateMachine)
 
-        guard response.approved else {
-            stateMachine.emit(proposalDecision: .reject(proposal: proposal), nextState: nextState)
-            return
+        save(
+            authResponse: response,
+            pairingId: proposal.pairingTopic,
+            dataSource: dataSource
+        ) { [weak self] optError in
+            guard let proposal = self?.proposal, let resolution = self?.resolution else {
+                return
+            }
+
+            guard optError == nil else {
+                // TODO: Also notify error
+                stateMachine.emit(proposalDecision: .reject(proposal: proposal), nextState: nextState)
+                return
+            }
+
+            guard response.approved else {
+                stateMachine.emit(proposalDecision: .reject(proposal: proposal), nextState: nextState)
+                return
+            }
+
+            let namespaces = WalletConnectModelFactory.createSessionNamespaces(
+                from: proposal,
+                wallet: response.wallet,
+                resolvedChains: resolution.allResolvedChains().resolved
+            )
+
+            stateMachine.emit(
+                proposalDecision: .approve(proposal: proposal, namespaces: namespaces),
+                nextState: nextState
+            )
         }
-
-        let namespaces = WalletConnectModelFactory.createSessionNamespaces(
-            from: proposal,
-            wallet: response.wallet,
-            resolvedChains: resolution.allResolvedChains().resolved
-        )
-
-        stateMachine.emit(
-            proposalDecision: .approve(proposal: proposal, namespaces: namespaces),
-            nextState: nextState
-        )
     }
 
     func proceed(with _: DAppStateDataSource) {}
