3cd4f592d49ab555017e430331a0aa1029ae1b83
ERussel <emkil.russel@gmail.com>
2023-05-06 11:33:47 +0500

    fix wallet connect chip

---
+++ novawallet/Modules/Settings/View/SettingsBoxTableViewCell.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+final class SettingsBoxTableViewCell: SettingsAccessoryTableViewCell<BorderedIconLabelView> {
+    override func setupStyle() {
+        super.setupStyle()
+
+        accessoryDisplayView.backgroundView.apply(style: .chips)
+        accessoryDisplayView.backgroundView.cornerRadius = 8
+        accessoryDisplayView.iconDetailsView.detailsLabel.numberOfLines = 1
+        accessoryDisplayView.iconDetailsView.detailsLabel.apply(style: .footnoteChip)
+        accessoryDisplayView.iconDetailsView.iconWidth = 12
+        accessoryDisplayView.contentInsets = UIEdgeInsets(
+            top: 2,
+            left: 8,
+            bottom: 2,
+            right: 8
+        )
+        accessoryDisplayView.iconDetailsView.spacing = 4
+    }
+
+    override func setupLayout() {
+        super.setupLayout()
+
+        accessoryDisplayView.snp.makeConstraints { make in
+            make.height.equalTo(22)
+        }
+    }
+
+    func bind(titleViewModel: TitleIconViewModel, accessoryViewModel: TitleIconViewModel) {
+        super.bind(titleViewModel: titleViewModel)
+
+        accessoryDisplayView.bind(viewModel: accessoryViewModel)
+    }
+}
