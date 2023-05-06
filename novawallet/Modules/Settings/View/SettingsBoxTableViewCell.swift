84310f9593d8ebaeba738171d9cbfe75d6ea922c
ERussel <emkil.russel@gmail.com>
2023-05-06 11:55:48 +0500

    settings style

--- novawallet/Modules/Settings/View/SettingsBoxTableViewCell.swift
+++ novawallet/Modules/Settings/View/SettingsBoxTableViewCell.swift
@@ -5,25 +5,17 @@ final class SettingsBoxTableViewCell: SettingsAccessoryTableViewCell<BorderedIco
         super.setupStyle()
 
         accessoryDisplayView.backgroundView.apply(style: .chips)
-        accessoryDisplayView.backgroundView.cornerRadius = 8
+        accessoryDisplayView.backgroundView.cornerRadius = 7
         accessoryDisplayView.iconDetailsView.detailsLabel.numberOfLines = 1
         accessoryDisplayView.iconDetailsView.detailsLabel.apply(style: .footnoteChip)
         accessoryDisplayView.iconDetailsView.iconWidth = 12
         accessoryDisplayView.contentInsets = UIEdgeInsets(
-            top: 2,
+            top: 4,
             left: 8,
-            bottom: 2,
+            bottom: 4,
             right: 8
         )
         accessoryDisplayView.iconDetailsView.spacing = 4
-    }
-
-    override func setupLayout() {
-        super.setupLayout()
-
-        accessoryDisplayView.snp.makeConstraints { make in
-            make.height.equalTo(22)
-        }
     }
 
     func bind(titleViewModel: TitleIconViewModel, accessoryViewModel: TitleIconViewModel) {
