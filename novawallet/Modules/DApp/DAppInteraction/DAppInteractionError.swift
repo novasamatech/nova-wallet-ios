725c5bc7639e50eedbe8e435cbc916a5ceefae25
ERussel <emkil.russel@gmail.com>
2023-05-13 13:55:19 +0500

    fix pr comments

--- novawallet/Modules/DApp/DAppInteraction/DAppInteractionError.swift
+++ novawallet/Modules/DApp/DAppInteraction/DAppInteractionError.swift
@@ -6,18 +6,15 @@ enum DAppInteractionError: Error {
 
 extension DAppInteractionError: ErrorContentConvertible {
     func toErrorContent(for locale: Locale?) -> ErrorContent {
-        let title: String
-        let message: String
-
         switch self {
         case let .unexpected(reason, _):
-            title = R.string.localizable.commonErrorGeneralTitle(preferredLanguages: locale?.rLanguages)
-            message = R.string.localizable.dappUnexpectedErrorFormat(
-                reason,
-                preferredLanguages: locale?.rLanguages
+            return .init(
+                title: R.string.localizable.commonErrorGeneralTitle(preferredLanguages: locale?.rLanguages),
+                message: R.string.localizable.dappUnexpectedErrorFormat(
+                    reason,
+                    preferredLanguages: locale?.rLanguages
+                )
             )
         }
-
-        return ErrorContent(title: title, message: message)
     }
 }
