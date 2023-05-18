e8240bd64f4f269d6da616ecd7e9ef041540a76f
ERussel <emkil.russel@gmail.com>
2023-05-18 11:15:21 +0500

    add data provider and price search algorithm

---
+++ novawallet/Common/DataProvider/Sources/CoingeckoPriceHistorySource.swift
@@ -0,0 +1,39 @@
+import Foundation
+import RobinHood
+
+final class CoingeckoPriceHistoryProviderSource {
+    let operationFactory: CoingeckoOperationFactoryProtocol
+    let currency: Currency
+    let priceId: AssetModel.PriceId
+
+    init(
+        priceId: AssetModel.PriceId,
+        currency: Currency,
+        operationFactory: CoingeckoOperationFactoryProtocol
+    ) {
+        self.priceId = priceId
+        self.currency = currency
+        self.operationFactory = operationFactory
+    }
+}
+
+extension CoingeckoPriceHistoryProviderSource: SingleValueProviderSourceProtocol {
+    typealias Model = PriceHistory
+
+    func fetchOperation() -> CompoundOperationWrapper<Model?> {
+        let historyOperation = operationFactory.fetchPriceHistory(
+            for: priceId,
+            currency: currency,
+            startDate: Date(timeIntervalSince1970: 0),
+            endDate: Date(timeIntervalSince1970: TimeInterval(UInt64.max))
+        )
+
+        let mapOperation = ClosureOperation<Model?> {
+            try historyOperation.extractNoCancellableResultData()
+        }
+
+        mapOperation.addDependency(historyOperation)
+
+        return CompoundOperationWrapper(targetOperation: mapOperation, dependencies: [historyOperation])
+    }
+}
