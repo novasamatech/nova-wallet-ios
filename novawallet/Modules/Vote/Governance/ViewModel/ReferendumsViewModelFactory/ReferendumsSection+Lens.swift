b790fa31e10318a23951efbdc15c81ae7f60c3cb
Holyberry <666lynx666@mail.ru>
2023-05-19 13:50:31 +0400

    fix referenda search

---
+++ novawallet/Modules/Vote/Governance/ViewModel/ReferendumsViewModelFactory/ReferendumsSection+Lens.swift
@@ -0,0 +1,27 @@
+extension ReferendumsSection {
+    enum Lens {
+        static let referendums = GenericLens<ReferendumsSection, [ReferendumsCellViewModel]>(
+            get: { whole in
+                switch whole {
+                case .personalActivities, .settings:
+                    return []
+                case let .active(_, activeReferendums):
+                    return activeReferendums
+                case let .completed(_, completedReferendums):
+                    return completedReferendums
+                }
+            }, set: { part, whole in
+                switch whole {
+                case .personalActivities:
+                    return whole
+                case .settings:
+                    return whole
+                case let .active(title, _):
+                    return .active(title, part)
+                case let .completed(title, _):
+                    return .completed(title, part)
+                }
+            }
+        )
+    }
+}
