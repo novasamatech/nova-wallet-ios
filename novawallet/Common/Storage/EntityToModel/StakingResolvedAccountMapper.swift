0b3ffdd07744ef6e73fadebead373c8cfc6bc669
ERussel <emkil.russel@gmail.com>
2023-05-26 13:28:55 +0500

    resolved accounts datasource

---
+++ novawallet/Common/Storage/EntityToModel/StakingResolvedAccountMapper.swift
@@ -0,0 +1,52 @@
+import Foundation
+import CoreData
+import RobinHood
+
+extension Multistaking.ResolvedAccount: Identifiable {
+    var identifier: String {
+        walletAccountId.toHex() + "-" + stakingOption.stringValue
+    }
+}
+
+final class StakingResolvedAccountMapper {
+    var entityIdentifierFieldName: String { #keyPath(CDStakingAccount.identifier) }
+
+    typealias DataProviderModel = Multistaking.ResolvedAccount
+    typealias CoreDataEntity = CDStakingAccount
+}
+
+extension StakingResolvedAccountMapper: CoreDataMapperProtocol {
+    func populate(
+        entity: CoreDataEntity,
+        from model: DataProviderModel,
+        using _: NSManagedObjectContext
+    ) throws {
+        entity.chainId = model.stakingOption.chainAssetId.chainId
+        entity.assetId = Int32(bitPattern: model.stakingOption.chainAssetId.assetId)
+        entity.stakingType = model.stakingOption.type.rawValue
+        entity.walletAccountId = model.walletAccountId.toHex()
+        entity.resolvedAccountId = model.resolvedAccountId.toHex()
+    }
+
+    func transform(entity: CoreDataEntity) throws -> DataProviderModel {
+        let chainAssetId = ChainAssetId(
+            chainId: entity.chainId!,
+            assetId: AssetModel.Id(bitPattern: entity.assetId)
+        )
+
+        let stakingType = StakingType(rawType: entity.stakingType)
+        let stakingOption = Multistaking.Option(
+            chainAssetId: chainAssetId,
+            type: stakingType
+        )
+
+        let walletAccountId = try Data(hexString: entity.walletAccountId!)
+        let resolvedAccountId = try Data(hexString: entity.resolvedAccountId!)
+
+        return .init(
+            stakingOption: stakingOption,
+            walletAccountId: walletAccountId,
+            resolvedAccountId: resolvedAccountId
+        )
+    }
+}
