5dc137406a542eda3d2154e12a18efde0b8335a8
Holyberry <666lynx666@mail.ru>
2023-05-30 11:37:31 +0400

    clean up

--- novawallet/Modules/AssetList/AssetListInteractor.swift
+++ novawallet/Modules/AssetList/AssetListInteractor.swift
@@ -216,7 +216,7 @@ extension AssetListInteractor: AssetListInteractorInputProtocol {
         }
     }
 
-    func retryFetchWalletConnectSessions() {
+    func retryFetchWalletConnectSessionsCount() {
         provideWalletConnectSessionsCount()
     }
 }
