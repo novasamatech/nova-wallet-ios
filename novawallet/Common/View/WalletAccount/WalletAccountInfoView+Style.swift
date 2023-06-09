f3534bbe9123f8f81a83ff4c64abe09d2b3e4e3b
ERussel <emkil.russel@gmail.com>
2023-06-09 14:27:53 +0300

    fix layout

--- novawallet/Common/View/WalletAccount/WalletAccountInfoView+Style.swift
+++ novawallet/Common/View/WalletAccount/WalletAccountInfoView+Style.swift
@@ -5,6 +5,6 @@ extension WalletAccountInfoView {
         roundedBackgroundView.apply(style: .selectableContainer(radius: 12))
 
         contentInsets = UIEdgeInsets(top: 7.0, left: 12.0, bottom: 7.0, right: 16.0)
-        actionIcon = R.image.iconInfo()?.tinted(with: R.color.colorIconSecondary()!)
+        actionIcon = R.image.iconMore()?.tinted(with: R.color.colorIconSecondary()!)
     }
 }
