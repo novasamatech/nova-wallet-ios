8a9531357de47156519f06966fffa2d4cd9889af
ERussel <emkil.russel@gmail.com>
2023-06-23 11:55:14 +0500

    staking items order

--- novawallet/Modules/Staking/Dashboard/Model/StakingDashboardModel.swift
+++ novawallet/Modules/Staking/Dashboard/Model/StakingDashboardModel.swift
@@ -64,50 +64,72 @@ struct StakingDashboardModel: Equatable {
 }
 
 extension Array where Element == StakingDashboardItemModel {
+    static var totalStakeOrder: [ChainModel.Id: Int] {
+        [
+            KnowChainId.polkadot: 0,
+            KnowChainId.kusama: 1,
+            KnowChainId.alephZero: 2,
+            KnowChainId.moonbeam: 3,
+            KnowChainId.moonriver: 4,
+            KnowChainId.ternoa: 5,
+            KnowChainId.polkadex: 6,
+            KnowChainId.calamari: 7,
+            KnowChainId.zeitgeist: 8
+        ]
+    }
+
     func sortedByStake() -> [StakingDashboardItemModel] {
         sorted { item1, item2 in
-            let stake1 = item1.stakeValue()
-            let stake2 = item2.stakeValue()
+            CompoundComparator.compare(list: [{
+                let stake1 = item1.stakeValue()
+                let stake2 = item2.stakeValue()
 
-            if stake1 > 0, stake2 > 0 {
-                return stake1 > stake2
-            } else if stake1 > 0 {
-                return true
-            } else if stake2 > 0 {
-                return false
-            } else {
+                return CompoundComparator.compare(item1: stake1, item2: stake2, isAsc: false)
+            }, {
                 let chain1 = item1.stakingOption.chainAsset.chain
                 let chain2 = item2.stakingOption.chainAsset.chain
 
-                return chain1.name.lexicographicallyPrecedes(chain2.name)
-            }
+                return ChainModelCompator.priorityAndTestnetComparator(chain1: chain1, chain2: chain2)
+            }, {
+                let chain1 = item1.stakingOption.chainAsset.chain
+                let chain2 = item2.stakingOption.chainAsset.chain
+
+                return chain1.name.localizedCaseInsensitiveCompare(chain2.name)
+            }])
         }
     }
 
     func sortedByBalance() -> [StakingDashboardItemModel] {
         sorted { item1, item2 in
-            let chain1 = item1.stakingOption.chainAsset.chain
-            let chain2 = item2.stakingOption.chainAsset.chain
+            CompoundComparator.compare(list: [{
+                let chain1 = item1.stakingOption.chainAsset.chain
+                let chain2 = item2.stakingOption.chainAsset.chain
 
-            let chainPriority1 = ChainModelCompator.chainPriority(for: chain1.chainId)
-            let chainPriority2 = ChainModelCompator.chainPriority(for: chain2.chainId)
+                return ChainModelCompator.priorityAndTestnetComparator(chain1: chain1, chain2: chain2)
+            }, {
+                let balance1 = item1.balanceValue()
+                let balance2 = item2.balanceValue()
 
-            guard chainPriority1 == chainPriority2 else {
-                return chainPriority1 < chainPriority2
-            }
+                return CompoundComparator.compare(item1: balance1, item2: balance2, isAsc: false)
+            }, {
+                let hasBalance1 = item1.balance != nil ? 0 : 1
+                let hasBalance2 = item2.balance != nil ? 0 : 1
 
-            let balance1 = item1.balanceValue()
-            let balance2 = item2.balanceValue()
+                return CompoundComparator.compare(item1: hasBalance1, item2: hasBalance2, isAsc: true)
+            }, {
+                let chain1 = item1.stakingOption.chainAsset.chain
+                let chain2 = item2.stakingOption.chainAsset.chain
 
-            if balance1 > 0, balance2 > 0 {
-                return balance1 > balance2
-            } else if balance1 > 0 {
-                return true
-            } else if balance2 > 0 {
-                return false
-            } else {
-                return chain1.name.lexicographicallyPrecedes(chain2.name)
-            }
+                let totalStaked1 = Self.totalStakeOrder[chain1.chainId] ?? Int.max
+                let totalStaked2 = Self.totalStakeOrder[chain2.chainId] ?? Int.max
+
+                return CompoundComparator.compare(item1: totalStaked1, item2: totalStaked2, isAsc: true)
+            }, {
+                let chain1 = item1.stakingOption.chainAsset.chain
+                let chain2 = item2.stakingOption.chainAsset.chain
+
+                return chain1.name.localizedCaseInsensitiveCompare(chain2.name)
+            }])
         }
     }
 }
