0bcc674650ada721c172ed6a3ee1e00a3786051a
ERussel <emkil.russel@gmail.com>
2023-08-22 11:05:03 +0300

    hide rebond action for pool staking

--- novawallet/Modules/Staking/StakingMain/NominationPools/ViewModel/StakingNominationPoolsStatics.swift
+++ novawallet/Modules/Staking/StakingMain/NominationPools/ViewModel/StakingNominationPoolsStatics.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 struct StakingNominationPoolsStatics: StakingMainStaticViewModelProtocol {
+    var canCancelUnbonding: Bool { false }
+
     func networkInfoTitle(for locale: Locale) -> String {
         R.string.localizable.stakingPoolNetworkInfo(preferredLanguages: locale.rLanguages)
     }
