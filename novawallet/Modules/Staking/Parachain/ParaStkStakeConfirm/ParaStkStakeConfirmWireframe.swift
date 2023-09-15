b694a99d0b74ce258a50e12bc9ffa550dc08c74b
ERussel <emkil.russel@gmail.com>
2023-09-15 16:28:48 +0500

    fix parachain staking confirm navigation

--- novawallet/Modules/Staking/Parachain/ParaStkStakeConfirm/ParaStkStakeConfirmWireframe.swift
+++ novawallet/Modules/Staking/Parachain/ParaStkStakeConfirm/ParaStkStakeConfirmWireframe.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 final class ParaStkStakeConfirmWireframe: ParaStkStakeConfirmWireframeProtocol,
-                                          ModalAlertPresenting,
-                                          ExtrinsicSubmissionPresenting {
+    ModalAlertPresenting,
+    ExtrinsicSubmissionPresenting {
     func complete(on view: ParaStkStakeConfirmViewProtocol?, locale: Locale) {
         let navigationController = view?.controller.navigationController
         let viewControllers = navigationController?.viewControllers ?? []
 
-        if viewControllers.contains({ $0 is StartStakingInfoViewProtocol }) {
+        if viewControllers.contains(where: { $0 is StartStakingInfoViewProtocol }) {
             presentExtrinsicSubmission(
                 from: view,
                 completionAction: .popBaseAndDismiss,
