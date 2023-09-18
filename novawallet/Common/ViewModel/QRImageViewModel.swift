8fbb5094c43eb8dfaac9a9364f03d78fe7d49e9b
ERussel <emkil.russel@gmail.com>
2023-09-18 12:40:50 +0500

    add support for animated qr codes

---
+++ novawallet/Common/ViewModel/QRImageViewModel.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+enum QRImageViewModel {
+    case single(UIImage)
+    case animated(AnimatedImageViewModel)
+}
+
+extension UIImageView {
+    func bindQr(viewModel: QRImageViewModel) {
+        stopAnimating()
+
+        image = nil
+        animationImages = nil
+
+        switch viewModel {
+        case let .single(image):
+            self.image = image
+        case let .animated(viewModel):
+            animationImages = viewModel.images
+            animationDuration = TimeInterval(viewModel.images.count) * viewModel.durationPerFrame
+            animationRepeatCount = 0
+        }
+    }
+}
