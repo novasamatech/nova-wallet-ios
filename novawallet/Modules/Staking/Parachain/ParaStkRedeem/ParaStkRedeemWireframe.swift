773a7b00305124f7d71c0e12c98553c2b10cc062
ERussel <emkil.russel@gmail.com>
2023-09-22 13:44:18 +0500

    navigate to dashboard after redeem all

--- novawallet/Modules/Staking/Parachain/ParaStkRedeem/ParaStkRedeemWireframe.swift
+++ novawallet/Modules/Staking/Parachain/ParaStkRedeem/ParaStkRedeemWireframe.swift
@@ -1,8 +1,3 @@
 import Foundation
 
-final class ParaStkRedeemWireframe: ParaStkRedeemWireframeProtocol,
-    ModalAlertPresenting, ExtrinsicSubmissionPresenting {
-    func complete(on view: ParaStkRedeemViewProtocol?, locale: Locale) {
-        presentExtrinsicSubmission(from: view, completionAction: .dismiss, locale: locale)
-    }
-}
+final class ParaStkRedeemWireframe: ParaStkRedeemWireframeProtocol, ModalAlertPresenting {}
