47c647859cdceb2c3d1223c1988f433e7d4dbacc
Holyberry <666lynx666@mail.ru>
2023-10-27 17:35:33 +0300

    cleanup

--- novawallet/Modules/Swaps/Base/View/SwapInfoViewCell.swift
+++ novawallet/Modules/Swaps/Base/View/SwapInfoViewCell.swift
@@ -8,3 +8,29 @@ final class SwapInfoViewCell: RowView<SwapInfoView>, StackTableViewCellProtocol 
         rowContentView.bind(loadableViewModel: loadableViewModel)
     }
 }
+
+extension SwapInfoViewCell {
+    func bind(attention: AttentionState) {
+        switch attention {
+        case .high:
+            valueLabel.textColor = R.color.colorTextNegative()
+        case .medium:
+            valueLabel.textColor = R.color.colorTextWarning()
+        case .low:
+            valueLabel.textColor = R.color.colorTextPrimary()
+        }
+    }
+
+    func bind(differenceViewModel: LoadableViewModelState<DifferenceViewModel>) {
+        switch differenceViewModel {
+        case .loading:
+            bind(loadableViewModel: .loading)
+        case let .cached(value):
+            bind(attention: value.attention)
+            bind(loadableViewModel: .cached(value: value.details))
+        case let .loaded(value):
+            bind(attention: value.attention)
+            bind(loadableViewModel: .loaded(value: value.details))
+        }
+    }
+}
--- novawallet/Modules/Swaps/Confirm/View/SwapElementView.swift
+++ novawallet/Modules/Swaps/Confirm/View/SwapElementView.swift
@@ -122,29 +122,3 @@ extension SwapElementView {
         priceLabel.text = viewModel.balance.price ?? " "
     }
 }
-
-extension SwapInfoViewCell {
-    func bind(attention: AttentionState) {
-        switch attention {
-        case .high:
-            valueLabel.textColor = R.color.colorTextNegative()
-        case .medium:
-            valueLabel.textColor = R.color.colorTextWarning()
-        case .low:
-            valueLabel.textColor = R.color.colorTextPrimary()
-        }
-    }
-
-    func bind(differenceViewModel: LoadableViewModelState<DifferenceViewModel>) {
-        switch differenceViewModel {
-        case .loading:
-            bind(loadableViewModel: .loading)
-        case let .cached(value):
-            bind(attention: value.attention)
-            bind(loadableViewModel: .cached(value: value.details))
-        case let .loaded(value):
-            bind(attention: value.attention)
-            bind(loadableViewModel: .loaded(value: value.details))
-        }
-    }
-}
