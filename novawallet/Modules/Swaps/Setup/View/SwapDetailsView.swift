6c0147f9bd4a3352df2d5681c042aa7f401cb932
Holyberry <666lynx666@mail.ru>
2023-11-16 17:43:25 +0300

    fix selection

--- novawallet/Modules/Swaps/Setup/View/SwapDetailsView.swift
+++ novawallet/Modules/Swaps/Setup/View/SwapDetailsView.swift
@@ -5,11 +5,22 @@ final class SwapDetailsView: CollapsableContainerView {
         $0.titleButton.imageWithTitleView?.titleColor = R.color.colorTextSecondary()
         $0.titleButton.imageWithTitleView?.titleFont = .regularFootnote
         $0.contentInsets = .init(top: 8, left: 16, bottom: 8, right: 16)
+        $0.borderView.borderType = .bottom
+        $0.roundedBackgroundView.cornerRadius = 12
+        $0.roundedBackgroundView.roundingCorners = [.topLeft, .topRight]
     }
 
     let networkFeeCell: SwapNetworkFeeViewCell = .create {
         $0.contentInsets = .init(top: 8, left: 16, bottom: 8, right: 16)
         $0.borderView.borderType = .none
+        $0.roundedBackgroundView.cornerRadius = 12
+        $0.roundedBackgroundView.roundingCorners = [.bottomLeft, .bottomRight]
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        backgroundView.sideLength = 12
     }
 
     override var rows: [UIView] {
