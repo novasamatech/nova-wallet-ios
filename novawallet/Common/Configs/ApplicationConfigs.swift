cb860218b94cd5cd7e737b5fa676ec0c72cebecc
ERussel <emkil.russel@gmail.com>
2023-12-23 06:31:06 +0100

    move dev to prod multistaking

--- novawallet/Common/Configs/ApplicationConfigs.swift
+++ novawallet/Common/Configs/ApplicationConfigs.swift
@@ -158,7 +158,7 @@ extension ApplicationConfig: ApplicationConfigProtocol {
         #if F_RELEASE
             URL(string: "https://api.subquery.network/sq/nova-wallet/subquery-staking")!
         #else
-            URL(string: "https://api.subquery.network/sq/nova-wallet/subquery-staking__bm92Y")!
+            URL(string: "https://api.subquery.network/sq/nova-wallet/subquery-staking")!
         #endif
     }
 
