d636c09b89ab5171615ba8fb0a9b565d6e0b1418
ERussel <emkil.russel@gmail.com>
2023-12-28 06:49:56 +0100

    improve multiaddress detectino

---
+++ novawallet/Common/Extension/SubstrateSdk/TypeRegistry+Node.swift
@@ -0,0 +1,18 @@
+import Foundation
+import SubstrateSdk
+
+extension TypeRegistryCatalogProtocol {
+    func nodeMatches(closure: (Node) -> Bool, typeName: String, version: UInt64) -> Bool {
+        guard let node = node(for: typeName, version: version) else {
+            return false
+        }
+
+        if let proxyNode = node as? ProxyNode {
+            return nodeMatches(closure: closure, typeName: proxyNode.typeName, version: version)
+        } else if let aliasNode = node as? AliasNode {
+            return nodeMatches(closure: closure, typeName: aliasNode.underlyingTypeName, version: version)
+        } else {
+            return closure(node)
+        }
+    }
+}
