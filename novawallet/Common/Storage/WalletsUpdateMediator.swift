4f50be3ed5b7b3c5830afffc2c593fff425eca60
ERussel <emkil.russel@gmail.com>
2024-01-11 15:26:18 +0100

    add wallet update mediator

--- novawallet/Common/Storage/WalletsUpdateMediator.swift
+++ novawallet/Common/Storage/WalletsUpdateMediator.swift
@@ -1,82 +1,92 @@
 import Foundation
 import RobinHood
 
-typealias NewSelectedWalletClosure = (ManagedMetaAccountModel?) -> Void
+typealias NewSelectedWalletClosure = (Result<ManagedMetaAccountModel?, Error>) -> Void
+
+struct WalletUpdateMediatingResult {
+    let selectedWallet: ManagedMetaAccountModel?
+    let isWalletSwitched: Bool
+}
 
 protocol WalletUpdateMediating {
     func saveChanges(
-        _ newOrUpdated: [ManagedMetaAccountModel],
-        deleted: [ManagedMetaAccountModel],
-        completion: NewSelectedWalletClosure?
-    )
+        _ changes: @escaping () throws -> SyncChanges<ManagedMetaAccountModel>
+    ) -> CompoundOperationWrapper<WalletUpdateMediatingResult>
 }
 
 final class WalletUpdateMediator {
+    struct ProcessingResult {
+        let changes: SyncChanges<ManagedMetaAccountModel>
+        let selectedWallet: ManagedMetaAccountModel?
+    }
+
     let selectedWalletSettings: SelectedWalletSettings
-    let repository: AnyDataProviderRepository<MetaAccountModel>
-    let eventCenter: EventCenterProtocol
+    let repository: AnyDataProviderRepository<ManagedMetaAccountModel>
     let operationQueue: OperationQueue
-    
+
     init(
         selectedWalletSettings: SelectedWalletSettings,
-        repository: AnyDataProviderRepository<MetaAccountModel>,
-        eventCenter: EventCenterProtocol,
+        repository: AnyDataProviderRepository<ManagedMetaAccountModel>,
         operationQueue: OperationQueue
     ) {
         self.selectedWalletSettings = selectedWalletSettings
         self.repository = repository
-        self.eventCenter = eventCenter
         self.operationQueue = operationQueue
     }
-    
-    private func createProxiedRemovalOperation(
+
+    private func proxiedRemovalOperation(
         dependingOn allWalletsOperation: BaseOperation<[ManagedMetaAccountModel]>,
-        changes: SyncChanges<ManagedMetaAccountModel>
+        changesClosure: @escaping () throws -> SyncChanges<ManagedMetaAccountModel>
     ) -> BaseOperation<SyncChanges<ManagedMetaAccountModel>> {
         ClosureOperation<SyncChanges<ManagedMetaAccountModel>> {
             let allWallets = try allWalletsOperation.extractNoCancellableResultData()
-            
-            let allRemovedIds = Set(changes.removedItems.map({ $0.identifier }))
-            
-            let allProxieds = try allWallets.filter { $0.info.type == .proxied && !allRemovedIds.contains($0.identifier) }
-            
+            let changes = try changesClosure()
+
+            let allRemovedIds = Set(changes.removedItems.map(\.identifier))
+
+            let allProxieds = allWallets.filter {
+                $0.info.type == .proxied && !allRemovedIds.contains($0.identifier)
+            }
+
             let proxiedsToRemove = allProxieds.filter { proxiedWallet in
                 guard let proxyChainAccount = proxiedWallet.info.chainAccounts.first else {
                     return false
                 }
-                
+
                 return allWallets.allSatisfy { wallet in
                     guard !allRemovedIds.contains(wallet.identifier) else {
                         return true
                     }
-                    
+
                     return !wallet.info.has(
                         accountId: proxyChainAccount.accountId,
                         chainId: proxyChainAccount.chainId
                     )
                 }
             }
-            
+
             let newRemovedWallets = changes.removedItems + proxiedsToRemove
-            let newRemovedWalletsIds = Set(newRemovedWallets.map({ $0.identifier }))
-            
-            let newUpdatedWallets = changes.newOrUpdatedItems.filter({ !newRemovedWalletsIds.contains($0.info.identifier) })
-            
+            let newRemovedWalletsIds = Set(newRemovedWallets.map(\.identifier))
+
+            let newUpdatedWallets = changes.newOrUpdatedItems.filter {
+                !newRemovedWalletsIds.contains($0.info.identifier)
+            }
+
             return SyncChanges(newOrUpdatedItems: newUpdatedWallets, removedItems: newRemovedWallets)
         }
     }
-    
+
     func newSelectedWalletOperation(
         dependingOn changesOperation: BaseOperation<SyncChanges<ManagedMetaAccountModel>>,
         allWalletsOperation: BaseOperation<[ManagedMetaAccountModel]>
-    ) -> BaseOperation<SyncChanges> {
-        ClosureOperation<SyncChanges> {
-            let changes = changesOperation.extractNoCancellableResultData()
-            
+    ) -> BaseOperation<ProcessingResult> {
+        ClosureOperation<ProcessingResult> {
+            let changes = try changesOperation.extractNoCancellableResultData()
+
             // we want to change selected wallet if current one is removed or revoked as proxied
-            
-            let newUpdatedWallets = changes.newOrUpdatedItems.map { accum, wallet in
-                if wallet.isSelected, wallet.proxy()?.status == .revoked {
+
+            let newUpdatedWallets = changes.newOrUpdatedItems.map { wallet in
+                if wallet.isSelected, wallet.info.proxy()?.status == .revoked {
                     return ManagedMetaAccountModel(
                         info: wallet.info,
                         isSelected: false,
@@ -86,23 +96,24 @@ final class WalletUpdateMediator {
                     return wallet
                 }
             }
-            
+
             let allWallets = try allWalletsOperation.extractNoCancellableResultData()
             var newState = allWallets.reduce(into: [MetaAccountModel.Id: ManagedMetaAccountModel]()) { accum, wallet in
                 accum[wallet.identifier] = wallet
             }
-            
+
             newState = newUpdatedWallets.reduce(into: newState) { accum, wallet in
                 accum[wallet.identifier] = wallet
             }
-            
+
             newState = changes.removedItems.reduce(into: newState) { accum, wallet in
                 accum[wallet.identifier] = nil
             }
-            
-            let hasSelectedWallet = newState.values.contains { $0.isSelected }
-            
-            if !hasSelectedWallet {
+
+            if let selectedWallet = newState.values.first(where: { $0.isSelected }) {
+                let newChanges = SyncChanges(newOrUpdatedItems: newUpdatedWallets, removedItems: changes.removedItems)
+                return ProcessingResult(changes: newChanges, selectedWallet: selectedWallet)
+            } else {
                 let existingWallets = Set(newState.keys)
                 let newSelectedWallet = allWallets.first {
                     $0.info.type != .proxied && existingWallets.contains($0.identifier)
@@ -113,13 +124,30 @@ final class WalletUpdateMediator {
                         order: $0.order
                     )
                 }
-                
-                return SyncChanges(
-                    newOrUpdatedItems: newUpdatedWallets + ([newSelectedWallet] ?? []),
+
+                let newChanges = SyncChanges(
+                    newOrUpdatedItems: newUpdatedWallets + (newSelectedWallet.map { [$0] } ?? []),
                     removedItems: changes.removedItems
                 )
+
+                return ProcessingResult(changes: newChanges, selectedWallet: newSelectedWallet)
+            }
+        }
+    }
+
+    func selectedWalletUpdateOperation(
+        in settings: SelectedWalletSettings,
+        dependingOn processingOperation: BaseOperation<ProcessingResult>
+    ) -> BaseOperation<Bool> {
+        ClosureOperation<Bool> {
+            let result = try processingOperation.extractNoCancellableResultData()
+
+            if result.selectedWallet?.identifier != settings.value?.identifier || settings.value == nil {
+                settings.setup()
+
+                return true
             } else {
-                return SyncChanges(newOrUpdatedItems: newUpdatedWallets, removedItems: changes.removedItems)
+                return false
             }
         }
     }
@@ -127,16 +155,65 @@ final class WalletUpdateMediator {
 
 extension WalletUpdateMediator: WalletUpdateMediating {
     func saveChanges(
-        _ changes: SyncChanges<ManagedMetaAccountModel>,
-        completion: NewSelectedWalletClosure?
-    ) {
-        let allWalletsOperation = repository.fetchAllOperation(with: .init(includesProperties: true, includesSubentities: true))
-        
-        let proxiedsRemovalOperation = createProxiedRemovalOperation(
-            dependingOn: allWalletsOperation,
-            changes: changes
+        _ changes: @escaping () throws -> SyncChanges<ManagedMetaAccountModel>
+    ) -> CompoundOperationWrapper<WalletUpdateMediatingResult> {
+        let allWalletsOperation = repository.fetchAllOperation(
+            with: .init(includesProperties: true, includesSubentities: true)
         )
-        
+
+        let proxiedsRemovalOperation = proxiedRemovalOperation(
+            dependingOn: allWalletsOperation,
+            changesClosure: changes
+        )
+
         proxiedsRemovalOperation.addDependency(allWalletsOperation)
+
+        let newSelectedWalletOperation = newSelectedWalletOperation(
+            dependingOn: proxiedsRemovalOperation,
+            allWalletsOperation: allWalletsOperation
+        )
+
+        newSelectedWalletOperation.addDependency(proxiedsRemovalOperation)
+
+        let saveOperation = repository.saveOperation({
+            let changesResult = try newSelectedWalletOperation.extractNoCancellableResultData()
+
+            return changesResult.changes.newOrUpdatedItems
+        }, {
+            let changesResult = try newSelectedWalletOperation.extractNoCancellableResultData()
+
+            return changesResult.changes.removedItems.map(\.identifier)
+        })
+
+        saveOperation.addDependency(newSelectedWalletOperation)
+
+        let selectedWalletUpdateOperation = selectedWalletUpdateOperation(
+            in: selectedWalletSettings,
+            dependingOn: newSelectedWalletOperation
+        )
+
+        selectedWalletUpdateOperation.addDependency(saveOperation)
+
+        let resultOperation = ClosureOperation<WalletUpdateMediatingResult> {
+            try saveOperation.extractNoCancellableResultData()
+            let isWalletSwitched = try selectedWalletUpdateOperation.extractNoCancellableResultData()
+            let currentWallet = try newSelectedWalletOperation.extractNoCancellableResultData().selectedWallet
+
+            return .init(selectedWallet: currentWallet, isWalletSwitched: isWalletSwitched)
+        }
+
+        resultOperation.addDependency(saveOperation)
+        resultOperation.addDependency(selectedWalletUpdateOperation)
+        resultOperation.addDependency(newSelectedWalletOperation)
+
+        let dependencies = [
+            allWalletsOperation,
+            proxiedsRemovalOperation,
+            newSelectedWalletOperation,
+            saveOperation,
+            selectedWalletUpdateOperation
+        ]
+
+        return CompoundOperationWrapper(targetOperation: resultOperation, dependencies: dependencies)
     }
 }
