ff7a54f0fe76aa28fc8282f3258aee74a2db3cf6
Holyberry <666lynx666@mail.ru>
2024-03-14 08:26:29 +0300

    buildfix

--- novawallet/Modules/Notifications/NotificationHandlers/Payload.swift
+++ novawallet/Modules/Notifications/NotificationHandlers/Payload.swift
@@ -11,7 +11,7 @@ struct NewReleasePayload: Codable {
 }
 
 struct NewReferendumPayload: Codable {
-    let referendumId: UInt
+    let referendumId: Referenda.ReferendumIndex
 
     var referendumNumber: String {
         "#\(referendumId)"
@@ -25,7 +25,7 @@ struct ReferendumStateUpdatePayload: Codable {
         case toStatus = "to"
     }
 
-    let referendumId: UInt
+    let referendumId: Referenda.ReferendumIndex
     let fromStatus: Status?
     let toStatus: Status
 
@@ -38,27 +38,6 @@ struct ReferendumStateUpdatePayload: Codable {
         case cancelled
         case timedOut
         case killed
-
-        func description(for locale: Locale?) -> String {
-            switch self {
-            case .created:
-                return R.string.localizable.pushNotificationReferendumCreated(preferredLanguages: locale?.rLanguages)
-            case .deciding:
-                return R.string.localizable.pushNotificationReferendumDeciding(preferredLanguages: locale?.rLanguages)
-            case .confirming:
-                return R.string.localizable.pushNotificationReferendumConfirming(preferredLanguages: locale?.rLanguages)
-            case .approved:
-                return R.string.localizable.pushNotificationReferendumApproved(preferredLanguages: locale?.rLanguages)
-            case .rejected:
-                return R.string.localizable.pushNotificationReferendumRejected(preferredLanguages: locale?.rLanguages)
-            case .cancelled:
-                return R.string.localizable.pushNotificationReferendumCancelled(preferredLanguages: locale?.rLanguages)
-            case .timedOut:
-                return R.string.localizable.pushNotificationReferendumTimedOut(preferredLanguages: locale?.rLanguages)
-            case .killed:
-                return R.string.localizable.pushNotificationReferendumKilled(preferredLanguages: locale?.rLanguages)
-            }
-        }
     }
 
     var referendumNumber: String {
