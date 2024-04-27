a3bffba8f3e3e3b6fcce636301009e5e29c15082
ERussel <emkil.russel@gmail.com>
2024-04-27 07:58:43 +0200

    add layout

--- novawallet/Modules/ImportWallet/OptionsList/WalletImportOptionsViewLayout.swift
+++ novawallet/Modules/ImportWallet/OptionsList/WalletImportOptionsViewLayout.swift
@@ -1,12 +1,55 @@
 import UIKit
 
-final class WalletImportOptionsViewLayout: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+class WalletImportOptionsViewLayout: ScrollableContainerLayoutView {
+    enum Constants {
+        static let itemSpacing: CGFloat = 12
     }
 
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    let titleLabel: UILabel = .create { label in
+        label.apply(style: .title3Primary)
+        label.numberOfLines = 0
+    }
+
+    var rows: [UIStackView] = []
+
+    override func setupStyle() {
+        backgroundColor = R.color.colorSecondaryScreenBackground()
+    }
+
+    func apply(viewModel: WalletImportOptionViewModel) {
+        rows.forEach { $0.removeFromSuperview() }
+
+        rows = viewModel.rows.map { rowItems in
+            let stackView = UIStackView()
+            stackView.spacing = Constants.itemSpacing
+            stackView.distribution = .fillEqually
+
+            for item in rowItems {
+                switch item {
+                case let .primary(primary):
+                    let view = WalletImportOptionPrimaryView()
+                    stackView.addArrangedSubview(view)
+
+                    view.bind(viewModel: primary)
+                case let .secondary(secondary):
+                    let view = WalletImportOptionSecondaryView()
+                    stackView.addArrangedSubview(view)
+
+                    view.bind(viewModel: secondary)
+                }
+            }
+
+            return stackView
+        }
+
+        for rowView in rows {
+            addArrangedSubview(rowView, spacingAfter: Constants.itemSpacing)
+        }
+    }
+
+    override func setupLayout() {
+        super.setupLayout()
+
+        addArrangedSubview(titleLabel, spacingAfter: 20)
     }
 }
