c626f3c1da1b72ce322a92d526c61d9c712c4d6e
ERussel <emkil.russel@gmail.com>
2024-05-27 06:05:20 +0200

    fix ledger size

--- novawallet/Common/Model/ChainRegistry/Account/MetaAccountModelType+ExtrinsicLimit.swift
+++ novawallet/Common/Model/ChainRegistry/Account/MetaAccountModelType+ExtrinsicLimit.swift
@@ -4,8 +4,8 @@ extension MetaAccountModelType {
     var maxCallsPerExtrinsic: Int? {
         switch self {
         case .ledger:
-            // took min value from this https://github.com/Zondax/ledger-polkadot/blob/main/app/src/parser_txdef.h#L31
-            return 5
+            // https://github.com/Zondax/ledger-polkadot/blob/main/app/src/parser_txdef.h#L28
+            return 6
         case .secrets, .watchOnly, .paritySigner, .polkadotVault, .proxied:
             return nil
         }
