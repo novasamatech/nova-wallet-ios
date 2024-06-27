939ea0ddd7c8a0896efb499c25389076143b57ed
ERussel <emkil.russel@gmail.com>
2024-06-27 09:27:36 +0200

    fix generic ledger wallets display

--- novawallet/Modules/YourWallets/YourWalletsPresenter.swift
+++ novawallet/Modules/YourWallets/YourWalletsPresenter.swift
@@ -13,6 +13,7 @@ final class YourWalletsPresenter {
         .secrets,
         .polkadotVault,
         .paritySigner,
+        .genericLedger,
         .ledger,
         .proxied,
         .watchOnly
