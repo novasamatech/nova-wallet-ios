31772b83f494da93fe86f862cad096f04578f6af
svojsu <svojsuu@gmail.com>
2024-07-05 16:58:39 +0300

    chain filter strategy fix

--- novawallet/Common/Services/ChainRegistry/ChainFilterStrategy.swift
+++ novawallet/Common/Services/ChainRegistry/ChainFilterStrategy.swift
@@ -1,9 +1,33 @@
-//
-//  ChainFilterStrategy.swift
-//  novawallet
-//
-//  Created by Levan Chikvaidze on 05/07/2024.
-//  Copyright © 2024 Nova Foundation. All rights reserved.
-//
-
 import Foundation
+
+enum ChainFilterStrategy {
+    typealias Filter = (DataProviderChange<ChainModel>) -> Bool
+    
+    case combined([ChainFilterStrategy])
+    
+    case enabledChains
+    case hasProxy
+    case chainId(ChainModel.Id)
+    case noFilter
+    
+    var filter: Filter {
+        switch self {
+        case .enabledChains: { $0.item?.syncMode.enabled() == true }
+        case .hasProxy: { change in
+            #if F_RELEASE
+                return change.item?.hasProxy == true
+                    && change.item?.isTestnet == false
+            #else
+                return change.item?.hasProxy == true
+            #endif
+        }
+        case let .chainId(chainId): { $0.item?.chainId == chainId }
+        case let .combined(strategies): { change in
+            let resultSet = Set(strategies.map { $0.filter(change) })
+            
+            return !resultSet.contains(false)
+        }
+        case .noFilter: { _ in true }
+        }
+    }
+}
