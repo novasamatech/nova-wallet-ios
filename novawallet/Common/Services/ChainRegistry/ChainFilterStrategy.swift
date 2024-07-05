621dadde2909a094188e39a4d93e02f238a35ff7
svojsu <svojsuu@gmail.com>
2024-07-05 16:58:14 +0300

    move chain filter strategy to separate file

---
+++ novawallet/Common/Services/ChainRegistry/ChainFilterStrategy.swift
@@ -0,0 +1,9 @@
+//
+//  ChainFilterStrategy.swift
+//  novawallet
+//
+//  Created by Levan Chikvaidze on 05/07/2024.
+//  Copyright © 2024 Nova Foundation. All rights reserved.
+//
+
+import Foundation
--- novawallet/Common/Services/ChainRegistry/ChainRegistry.swift
+++ novawallet/Common/Services/ChainRegistry/ChainRegistry.swift
@@ -2,38 +2,6 @@ import Foundation
 import Operation_iOS
 import SubstrateSdk
 
-enum ChainFilterStrategy {
-    typealias Filter = (DataProviderChange<ChainModel>) -> Bool
-    
-    case combined([ChainFilterStrategy])
-    
-    case enabledChains
-    case hasProxy
-    case chainId(ChainModel.Id)
-    case noFilter
-    
-    var filter: Filter {
-        switch self {
-        case .enabledChains: { $0.item?.syncMode.enabled() == true }
-        case .hasProxy: { change in
-            #if F_RELEASE
-                return change.item?.hasProxy == true 
-                    && change.item?.isTestnet == false
-            #else
-                return change.item?.hasProxy == true
-            #endif
-        }
-        case let .chainId(chainId): { $0.item?.chainId == chainId }
-        case let .combined(strategies): { change in
-            let resultSet = Set(strategies.map { $0.filter(change) })
-            
-            return !resultSet.contains(false)
-        }
-        case .noFilter: { _ in true }
-        }
-    }
-}
-
 protocol ChainRegistryProtocol: AnyObject {
     var availableChainIds: Set<ChainModel.Id>? { get }
 
