7d9d6aa9fdedd81fbdd871d4798525bf3a309730
ERussel <emkil.russel@gmail.com>
2024-07-11 13:09:11 +0200

    fix migration app name

--- novawallet/Modules/Ledger/Discovery/LedgerDiscoverViewFactory.swift
+++ novawallet/Modules/Ledger/Discovery/LedgerDiscoverViewFactory.swift
@@ -8,18 +8,7 @@ struct LedgerDiscoverViewFactory {
     ) -> ControllerBackedProtocol? {
         let ledgerConnection = LedgerConnectionManager(logger: Logger.shared)
 
-        let ledgerApplication: LedgerAccountRetrievable = if chain.supportsGenericLedgerApp {
-            MigrationLedgerSubstrateApplication(
-                connectionManager: ledgerConnection,
-                chainRegistry: ChainRegistryFacade.sharedRegistry,
-                supportedApps: SupportedLedgerApp.substrate()
-            )
-        } else {
-            LedgerApplication(
-                connectionManager: ledgerConnection,
-                supportedApps: SupportedLedgerApp.substrate()
-            )
-        }
+        let ledgerApplication = createLegacyLedgerApplication(for: chain, connection: ledgerConnection)
 
         let interactor = LedgerDiscoverInteractor(
             chain: chain,
@@ -35,9 +24,7 @@ struct LedgerDiscoverViewFactory {
             chain: chain
         )
 
-        let appName = chain.supportsGenericLedgerApp ?
-            LedgerSubstrateApp.migration.displayName(for: nil) :
-            chain.name
+        let appName = createLedgerAppName(for: chain)
 
         return createView(interactor: interactor, wireframe: wireframe, appName: appName)
     }
@@ -48,9 +35,9 @@ struct LedgerDiscoverViewFactory {
     ) -> ControllerBackedProtocol? {
         let ledgerConnection = LedgerConnectionManager(logger: Logger.shared)
 
-        let ledgerApplication = LedgerApplication(
-            connectionManager: ledgerConnection,
-            supportedApps: SupportedLedgerApp.substrate()
+        let ledgerApplication = createLegacyLedgerApplication(
+            for: chain,
+            connection: ledgerConnection
         )
 
         let interactor = LedgerDiscoverInteractor(
@@ -67,7 +54,9 @@ struct LedgerDiscoverViewFactory {
             chain: chain
         )
 
-        return createView(interactor: interactor, wireframe: wireframe, appName: chain.name)
+        let appName = createLedgerAppName(for: chain)
+
+        return createView(interactor: interactor, wireframe: wireframe, appName: appName)
     }
 
     static func createGenericLedgerView(for flow: WalletCreationFlow) -> ControllerBackedProtocol? {
@@ -112,5 +101,29 @@ struct LedgerDiscoverViewFactory {
         interactor.basePresenter = presenter
 
         return view
+    }
+
+    private static func createLegacyLedgerApplication(
+        for chain: ChainModel,
+        connection: LedgerConnectionManagerProtocol
+    ) -> LedgerAccountRetrievable {
+        if chain.supportsGenericLedgerApp {
+            return MigrationLedgerSubstrateApplication(
+                connectionManager: connection,
+                chainRegistry: ChainRegistryFacade.sharedRegistry,
+                supportedApps: SupportedLedgerApp.substrate()
+            )
+        } else {
+            return LedgerApplication(
+                connectionManager: connection,
+                supportedApps: SupportedLedgerApp.substrate()
+            )
+        }
+    }
+
+    private static func createLedgerAppName(for chain: ChainModel) -> String {
+        chain.supportsGenericLedgerApp ?
+            LedgerSubstrateApp.migration.displayName(for: nil) :
+            chain.name
     }
 }
