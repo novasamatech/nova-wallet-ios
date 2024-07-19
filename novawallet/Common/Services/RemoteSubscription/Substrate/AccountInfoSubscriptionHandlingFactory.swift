9392748a44e8144edb0b84086c2c30f2a4ed75fa
ERussel <emkil.russel@gmail.com>
2024-07-19 06:56:59 +0200

    holds subscription

--- novawallet/Common/Services/RemoteSubscription/Substrate/AccountInfoSubscriptionHandlingFactory.swift
+++ novawallet/Common/Services/RemoteSubscription/Substrate/AccountInfoSubscriptionHandlingFactory.swift
@@ -4,15 +4,18 @@ import Operation_iOS
 final class AccountInfoSubscriptionHandlingFactory: RemoteSubscriptionHandlingFactoryProtocol {
     let accountLocalStorageKey: String
     let locksLocalStorageKey: String
+    let holdsLocalStorageKey: String
     let factory: NativeTokenSubscriptionFactoryProtocol
 
     init(
         accountLocalStorageKey: String,
         locksLocalStorageKey: String,
+        holdsLocalStorageKey: String,
         factory: NativeTokenSubscriptionFactoryProtocol
     ) {
         self.accountLocalStorageKey = accountLocalStorageKey
         self.locksLocalStorageKey = locksLocalStorageKey
+        self.holdsLocalStorageKey = holdsLocalStorageKey
         self.factory = factory
     }
 
@@ -25,6 +28,12 @@ final class AccountInfoSubscriptionHandlingFactory: RemoteSubscriptionHandlingFa
     ) -> StorageChildSubscribing {
         if locksLocalStorageKey == localStorageKey {
             return factory.createBalanceLocksSubscription(
+                remoteStorageKey: remoteStorageKey,
+                operationManager: operationManager,
+                logger: logger
+            )
+        } else if holdsLocalStorageKey == localStorageKey {
+            return factory.createBalanceHoldsSubscription(
                 remoteStorageKey: remoteStorageKey,
                 operationManager: operationManager,
                 logger: logger
