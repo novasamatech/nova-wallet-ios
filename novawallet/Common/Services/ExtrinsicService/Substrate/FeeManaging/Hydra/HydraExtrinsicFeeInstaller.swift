8f1b226b9ea3285d480a1ba7716fa69559ff3d31
svojsu <svojsuu@gmail.com>
2024-08-16 17:56:05 +0300

    code clean up

--- novawallet/Common/Services/ExtrinsicService/Substrate/FeeManaging/Hydra/HydraExtrinsicFeeInstaller.swift
+++ novawallet/Common/Services/ExtrinsicService/Substrate/FeeManaging/Hydra/HydraExtrinsicFeeInstaller.swift
@@ -9,6 +9,13 @@ final class HydraExtrinsicFeeInstaller {
     }
 }
 
+extension HydraExtrinsicFeeInstaller {
+    struct TransferFeeInstallingCalls {
+        let setCurrencyCall: HydraDx.SetCurrencyCall?
+        let revertCurrencyCall: HydraDx.SetCurrencyCall?
+    }
+}
+
 extension HydraExtrinsicFeeInstaller: ExtrinsicFeeInstalling {
     func installingFeeSettings(
         to builder: ExtrinsicBuilderProtocol,
@@ -19,35 +26,39 @@ extension HydraExtrinsicFeeInstaller: ExtrinsicFeeInstalling {
             codingFactory: coderFactory
         )
 
-        // set
+        let calls = createTransferFeeCalls(using: assetId)
 
+        guard
+            let setCurrencyCall = calls.setCurrencyCall,
+            let revertCurrencyCall = calls.revertCurrencyCall
+        else {
+            return builder
+        }
+
+        return try builder
+            .with(batchType: .ignoreFails)
+            .adding(call: setCurrencyCall.runtimeCall(), at: 0)
+            .adding(call: revertCurrencyCall.runtimeCall())
+    }
+
+    private func createTransferFeeCalls(using assetId: HydraDx.LocalRemoteAssetId) -> TransferFeeInstallingCalls {
         let shouldSetCurrency = feeAsset.chain.utilityAsset()?.assetId != feeAsset.asset.assetId
-
         let setCurrencyCall: HydraDx.SetCurrencyCall? = {
             guard shouldSetCurrency else { return nil }
 
             return .init(currency: assetId.remoteAssetId)
         }()
 
-        // revert
-
         let shouldRevertCurrency = feeAsset.asset.assetId != HydraDx.nativeAssetId
-
         let revertCurrencyCall: HydraDx.SetCurrencyCall? = {
             guard shouldRevertCurrency else { return nil }
 
             return .init(currency: HydraDx.nativeAssetId)
         }()
 
-        var mutBuilder = builder
-
-        if let setCurrencyCall, let revertCurrencyCall {
-            mutBuilder = try builder
-                .with(batchType: .ignoreFails)
-                .adding(call: setCurrencyCall.runtimeCall(), at: 0)
-                .adding(call: revertCurrencyCall.runtimeCall())
-        }
-
-        return mutBuilder
+        return TransferFeeInstallingCalls(
+            setCurrencyCall: setCurrencyCall,
+            revertCurrencyCall: revertCurrencyCall
+        )
     }
 }
