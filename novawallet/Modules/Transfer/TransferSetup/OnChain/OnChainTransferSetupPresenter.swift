f44892218e2c86098c0b17dda357407451b732b2
svojsu <svojsuu@gmail.com>
2024-08-19 22:40:56 +0300

    switch from utility fee asset if needed

--- novawallet/Modules/Transfer/TransferSetup/OnChain/OnChainTransferSetupPresenter.swift
+++ novawallet/Modules/Transfer/TransferSetup/OnChain/OnChainTransferSetupPresenter.swift
@@ -9,6 +9,7 @@ final class OnChainTransferSetupPresenter: OnChainTransferPresenter, OnChainTran
     let interactor: OnChainTransferSetupInteractorInputProtocol
 
     private(set) var partialRecepientAddress: AccountAddress?
+    private var isManualFeeSet: Bool = false
 
     let phishingValidatingFactory: PhishingAddressValidatorFactoryProtocol
     let chainAssetViewModelFactory: ChainAssetViewModelFactoryProtocol
@@ -102,6 +103,29 @@ final class OnChainTransferSetupPresenter: OnChainTransferPresenter, OnChainTran
 
     private func provideCanSendMySelf() {
         view?.didReceiveCanSendMySelf(false)
+    }
+
+    private func switchFeeChainAssetIfNecessary() {
+        guard
+            let fee,
+            !isManualFeeSet,
+            let utilityAssetMinBalance,
+            let senderUtilityAssetBalance,
+            feeAsset.isUtilityAsset
+        else {
+            return
+        }
+
+        if senderUtilityAssetBalance.transferable - fee.value.amount < utilityAssetMinBalance {
+            changeFeeAsset(to: chainAsset)
+        }
+    }
+
+    func changeFeeAsset(to chainAsset: ChainAsset) {
+        feeAsset = chainAsset
+
+        interactor.change(feeAsset: chainAsset)
+        refreshFee()
     }
 
     private func updateFeeView() {
@@ -252,6 +276,7 @@ final class OnChainTransferSetupPresenter: OnChainTransferPresenter, OnChainTran
         super.didReceiveFee(result: result)
 
         if case .success = result {
+            switchFeeChainAssetIfNecessary()
             updateFeeView()
             provideAmountInputViewModelIfRate()
             updateAmountPriceView()
@@ -297,13 +322,16 @@ final class OnChainTransferSetupPresenter: OnChainTransferPresenter, OnChainTran
 }
 
 extension OnChainTransferSetupPresenter: TransferSetupChildPresenterProtocol {
+    func getFeeAsset() -> ChainAsset? {
+        feeAsset
+    }
+
     func changeFeeAsset(to chainAsset: ChainAsset?) {
         guard let chainAsset else { return }
 
-        feeAsset = chainAsset
+        isManualFeeSet = true
 
-        interactor.change(feeAsset: chainAsset)
-        refreshFee()
+        changeFeeAsset(to: chainAsset)
     }
 
     var inputState: TransferSetupInputState {
