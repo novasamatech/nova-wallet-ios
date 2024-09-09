a5f2da9c7f750817e09d566179c5ec703e59cc0e
svojsu <svojsuu@gmail.com>
2024-09-09 10:09:35 +0300

    remove outdated code

--- novawallet/Modules/Vote/Parent/VoteChildPresenterFactory.swift
+++ novawallet/Modules/Vote/Parent/VoteChildPresenterFactory.swift
@@ -103,7 +103,6 @@ final class VoteChildPresenterFactory {
 
     private func createGovernanceInteractor(
         for state: GovernanceSharedState,
-        referendumStore _: ReferendumObservableStoreProtocol,
         wallet: MetaAccountModel
     ) -> ReferendumsInteractor {
         let serviceFactory = GovernanceServiceFactory(
@@ -170,11 +169,9 @@ extension VoteChildPresenterFactory: VoteChildPresenterFactoryProtocol {
         referendumsInitState: ReferendumsInitState?
     ) -> VoteChildPresenterProtocol? {
         let state = GovernanceSharedState()
-        let store = ReferendumObservableSource.shared
 
         let interactor = createGovernanceInteractor(
             for: state,
-            referendumStore: store,
             wallet: wallet
         )
         let wireframe = ReferendumsWireframe(state: state)
