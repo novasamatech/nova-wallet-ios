4a7759436f156b8ea9ffb10380caf9b730be02f4
ERussel <emkil.russel@gmail.com>
2024-10-02 05:21:02 +0200

    cookie extraction

--- novawallet/Modules/PayCard/PayCardViewController.swift
+++ novawallet/Modules/PayCard/PayCardViewController.swift
@@ -15,10 +15,13 @@ private struct WebViewScript {
 private enum CallbackNames: String {
     case onStatusChange
     case onSellTransferEnabled
+    case onRequestIntercept
 }
 
 final class PayCardViewController: UIViewController, ViewHolder {
     typealias RootViewType = PayCardViewLayout
+
+    let userContentController = WKUserContentController()
 
     lazy var webView: WKWebView = {
         let configuration = WKWebViewConfiguration()
@@ -74,6 +77,9 @@ final class PayCardViewController: UIViewController, ViewHolder {
 
         webView.navigationDelegate = self
         webView.uiDelegate = self
+
+        addWidgetScript()
+        addRequestInterceptingScript()
     }
 
     func startWebView() {
@@ -89,13 +95,62 @@ final class PayCardViewController: UIViewController, ViewHolder {
             baseURL: URL(string: "https://exchange.mercuryo.io")!
         )
     }
-}
 
-extension PayCardViewController {
-    var userContentController: WKUserContentController {
-        let userController = WKUserContentController()
-        userController.add(self, name: CallbackNames.onSellTransferEnabled.rawValue)
-        userController.add(self, name: CallbackNames.onStatusChange.rawValue)
+    private func addRequestInterceptingScript() {
+        userContentController.add(self, name: CallbackNames.onRequestIntercept.rawValue)
+
+        let specificUrl = "https://api.mercuryo.io/v1.6/cards"
+
+        let scriptSource = """
+        (function() {
+                let originalXhrOpen = XMLHttpRequest.prototype.open;
+                let originalXhrSend = XMLHttpRequest.prototype.send;
+                let originalSetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
+
+                XMLHttpRequest.prototype.open = function(method, url) {
+                    this.interceptedUrl = url;
+                    this.interceptedMethod = method;
+                    this.interceptedHeaders = {};
+                    originalXhrOpen.apply(this, arguments);
+                };
+
+                XMLHttpRequest.prototype.setRequestHeader = function(header, value) {
+                    this.interceptedHeaders[header] = value;
+                    originalSetRequestHeader.apply(this, arguments);
+                };
+
+                XMLHttpRequest.prototype.send = function(body) {
+                    if (this.interceptedUrl && this.interceptedUrl.includes('\(specificUrl)')) {
+                        let cookieHeader = document.cookie; // Capture cookies manually
+
+                        window.webkit.messageHandlers.onRequestIntercept.postMessage({
+                            url: this.interceptedUrl,
+                            method: this.interceptedMethod,
+                            headers: this.interceptedHeaders,
+                            cookies: cookieHeader,
+                            body: body,
+                            referer: document.referrer,
+                            origin: window.location.origin,
+                            userAgent: navigator.userAgent
+                        });
+                    }
+                    originalXhrSend.apply(this, arguments);
+                };
+            })();
+        """
+
+        let wkScript = WKUserScript(
+            source: scriptSource,
+            injectionTime: .atDocumentEnd,
+            forMainFrameOnly: false
+        )
+
+        userContentController.addUserScript(wkScript)
+    }
+
+    private func addWidgetScript() {
+        userContentController.add(self, name: CallbackNames.onSellTransferEnabled.rawValue)
+        userContentController.add(self, name: CallbackNames.onStatusChange.rawValue)
 
         let script = WebViewScript(
             content: """
@@ -114,7 +169,7 @@ extension PayCardViewController {
                 hideRefundAddress: true,
                 refundAddress: '14iKGFDp5EBXe3sdX765ngrERMrYUdxmFfayNCGkq7f6tm9w',
                 onStatusChange: data => {
-                    window.webkit.messageHandlers.onSellTransferEnabled.postMessage(JSON.stringify(data))
+                    window.webkit.messageHandlers.onStatusChange.postMessage(JSON.stringify(data))
                 },
                 onSellTransferEnabled: data => {
                     window.webkit.messageHandlers.onSellTransferEnabled.postMessage(JSON.stringify(data))
@@ -129,9 +184,82 @@ extension PayCardViewController {
             injectionTime: .atDocumentEnd,
             forMainFrameOnly: false
         )
-        userController.addUserScript(wkScript)
 
-        return userController
+        userContentController.addUserScript(wkScript)
+    }
+
+    private func handleJavaScriptInterceptedRequest(data: [String: Any]) {
+        guard let urlString = data["url"] as? String,
+              let url = URL(string: urlString),
+              let method = data["method"] as? String,
+              let headers = data["headers"] as? [String: String] else {
+            print("Invalid data received from JavaScript")
+            return
+        }
+
+        // Get cookies from WKWebView's cookie store
+        getCookiesForUrl(url: url) { cookies in
+            // Create a URLRequest
+            var request = URLRequest(url: url)
+            request.httpMethod = method
+
+            // Set headers
+            for (headerField, headerValue) in headers {
+                request.setValue(headerValue, forHTTPHeaderField: headerField)
+            }
+
+            // Add cookies to the request
+            if let origin = data["origin"] as? String {
+                request.setValue(origin, forHTTPHeaderField: "Origin")
+                request.setValue(origin, forHTTPHeaderField: "Referer")
+            }
+            if let userAgent = data["userAgent"] as? String {
+                request.setValue(userAgent, forHTTPHeaderField: "User-Agent")
+            }
+
+            // Set the body if present and if the method allows it
+            if let body = data["body"] as? String, !body.isEmpty,
+               let httpBody = body.data(using: .utf8), method != "GET" {
+                request.httpBody = httpBody
+            }
+
+            Logger.shared.debug("Header: \(request.allHTTPHeaderFields)")
+
+            // Perform the request using URLSession
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    Logger.shared.error("Error occurred during URLSession request: \(error)")
+                    return
+                }
+
+                if let response = response as? HTTPURLResponse {
+                    Logger.shared.debug("Response status code: \(response.statusCode)")
+                }
+
+                if let data = data {
+                    if let responseString = String(data: data, encoding: .utf8) {
+                        Logger.shared.debug("Response data: \(responseString)")
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+
+    private func getCookiesForUrl(url: URL, completion: @escaping ([HTTPCookie]) -> Void) {
+        let cookieStore = webView.configuration.websiteDataStore.httpCookieStore
+        var cookies: [HTTPCookie] = []
+
+        cookieStore.getAllCookies { allCookies in
+            for cookie in allCookies {
+                let domainMatches = url.host?.contains(cookie.domain) == true
+
+                if domainMatches {
+                    cookies.append(cookie)
+                }
+            }
+            completion(cookies)
+        }
     }
 }
 
@@ -149,6 +277,12 @@ extension PayCardViewController: WKScriptMessageHandler {
             presenter.processWidgetState(data: data)
         case .onSellTransferEnabled:
             presenter.processTransferData(data: data)
+        case .onRequestIntercept:
+            Logger.shared.debug("Intercepted: \(message.body)")
+
+            if let body = message.body as? [String: Any] {
+                handleJavaScriptInterceptedRequest(data: body)
+            }
         }
     }
 }
