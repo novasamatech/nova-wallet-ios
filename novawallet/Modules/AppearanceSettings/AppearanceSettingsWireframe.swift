632d87ef5e36d69228249f2f35fc012176b9a6de
svojsu <svojsuu@gmail.com>
2024-11-06 11:48:16 +0200

    use MainTransitionHelper.transitToMainTabBarController

--- novawallet/Modules/AppearanceSettings/AppearanceSettingsWireframe.swift
+++ novawallet/Modules/AppearanceSettings/AppearanceSettingsWireframe.swift
@@ -3,42 +3,13 @@ import UIKit
 
 final class AppearanceSettingsWireframe: AppearanceSettingsWireframeProtocol {
     func presentAppearanceChanged(from view: ControllerBackedProtocol?) {
-        let tabBar = view?.controller.tabBarController
-
-        guard
-            let window = view?.controller.view.window,
-            let selectedViewController = tabBar?.selectedViewController,
-            let navigationController = view?.controller.navigationController,
-            let snapshot = view?.controller.navigationController?.view.snapshotView(
-                afterScreenUpdates: false
-            ) else {
+        guard let navigationController = view?.controller.navigationController else {
             return
         }
 
-        snapshot.frame = navigationController.view.frame
-        window.addSubview(snapshot)
-
-        navigationController.view.isHidden = true
-        navigationController.popViewController(animated: false)
-
-        tabBar?.selectedIndex = 0
-
-        selectedViewController.view.isHidden = true
-
-        UIView.animate(
-            withDuration: 0.3,
-            animations: {
-                snapshot.alpha = 0
-            }, completion: { _ in
-                snapshot.removeFromSuperview()
-
-                selectedViewController.view.isHidden = false
-                selectedViewController.view.alpha = 0
-
-                UIView.animate(withDuration: 0.2) {
-                    selectedViewController.view.alpha = 1
-                }
-            }
+        MainTransitionHelper.transitToMainTabBarController(
+            closing: navigationController,
+            animated: true
         )
     }
 }
