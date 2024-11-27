f17e10579c78f9f4efc487c5ced17acb21685160
svojsu <svojsuu@gmail.com>
2024-11-27 14:48:08 +0200

    factory fix

--- novawallet/Modules/DAppBrowserWidget/DAppBrowserWidgetViewFactory.swift
+++ novawallet/Modules/DAppBrowserWidget/DAppBrowserWidgetViewFactory.swift
@@ -2,10 +2,16 @@ import Foundation
 
 struct DAppBrowserWidgetViewFactory {
     static func createView() -> DAppBrowserWidgetViewProtocol? {
-        let interactor = DAppBrowserWidgetInteractor()
+        let interactor = DAppBrowserWidgetInteractor(
+            tabManager: DAppBrowserTabManager.shared
+        )
         let wireframe = DAppBrowserWidgetWireframe()
 
-        let presenter = DAppBrowserWidgetPresenter(interactor: interactor, wireframe: wireframe)
+        let presenter = DAppBrowserWidgetPresenter(
+            interactor: interactor,
+            wireframe: wireframe,
+            browserTabsViewModelFactory: DAppBrowserWidgetViewModelFactory()
+        )
 
         let view = DAppBrowserWidgetViewController(presenter: presenter)
 
