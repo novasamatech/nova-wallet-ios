79239f4493892a7343a54a114e97a7739a116cec
svojsu <svojsuu@gmail.com>
2024-12-12 22:06:40 +0200

    fix tests

--- novawalletTests/Modules/DApps/DAppBrowser/DAppBrowserTests.swift
+++ novawalletTests/Modules/DApps/DAppBrowser/DAppBrowserTests.swift
@@ -81,6 +81,7 @@ class DAppBrowserTests: XCTestCase {
             operationQueue: OperationQueue(),
             sequentialPhishingVerifier: phishingVerifier,
             tabManager: tabManager,
+            applicationHandler: ApplicationHandler(),
             logger: Logger.shared
         )
 
