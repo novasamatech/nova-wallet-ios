f70cf4d81f57b491c47319255e7a60571b56e63c
svojsu <svojsuu@gmail.com>
2024-12-27 14:32:37 +0200

    show and hide categories

--- novawallet/Modules/DApp/DAppSearch/DAppSearchViewLayout.swift
+++ novawallet/Modules/DApp/DAppSearch/DAppSearchViewLayout.swift
@@ -3,7 +3,10 @@ import SoraUI
 
 final class DAppSearchViewLayout: UIView {
     let searchBar = CustomSearchBar()
-    let categoriesView = DAppCategoriesView()
+
+    let categoriesView: DAppCategoriesView = .create { view in
+        view.alpha = 0.0
+    }
 
     let topContainerView = UIView()
 
@@ -26,6 +29,22 @@ final class DAppSearchViewLayout: UIView {
         return item
     }()
 
+    private let appearanceAnimator: ViewAnimatorProtocol = FadeAnimator(
+        from: 0.0,
+        to: 1.0,
+        duration: 0.15
+    )
+    private let disappearanceAnimator: ViewAnimatorProtocol = FadeAnimator(
+        from: 1.0,
+        to: 0.0,
+        duration: 0.15
+    )
+    private let blockAnimator: BlockViewAnimatorProtocol = BlockViewAnimator(
+        duration: 0.2,
+        delay: 0.0,
+        options: [.curveLinear]
+    )
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -46,7 +65,7 @@ final class DAppSearchViewLayout: UIView {
 
         categoriesView.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide)
-            make.height.equalTo(DAppCategoriesView.preferredHeight)
+            make.height.equalTo(0.0)
             make.leading.trailing.equalToSuperview()
         }
 
@@ -59,5 +78,56 @@ final class DAppSearchViewLayout: UIView {
             make.top.equalTo(categoriesView.snp.bottom)
             make.leading.trailing.bottom.equalToSuperview()
         }
+    }
+
+    func updateCategoriesView(with viewModels: [DAppCategoryViewModel]) {
+        guard viewModels.isEmpty != categoriesView.viewModels.isEmpty else {
+            categoriesView.bind(categories: viewModels)
+
+            return
+        }
+
+        if viewModels.isEmpty {
+            hideCategoriesView { [weak self] in
+                self?.categoriesView.bind(categories: viewModels)
+            }
+        } else {
+            categoriesView.bind(categories: viewModels)
+            showCategoriesView()
+        }
+    }
+
+    func hideCategoriesView(updateClosure: @escaping () -> Void) {
+        categoriesView.snp.updateConstraints { make in
+            make.height.equalTo(0.0)
+        }
+
+        disappearanceAnimator.animate(
+            view: categoriesView,
+            completionBlock: nil
+        )
+
+        blockAnimator.animate(
+            block: { [weak self] in self?.layoutIfNeeded() },
+            completionBlock: { _ in updateClosure() }
+        )
+    }
+
+    func showCategoriesView() {
+        categoriesView.snp.updateConstraints { make in
+            make.height.equalTo(DAppCategoriesView.preferredHeight)
+        }
+
+        blockAnimator.animate(
+            block: { [weak self] in self?.layoutIfNeeded() },
+            completionBlock: { [weak self] _ in
+                guard let self else { return }
+
+                appearanceAnimator.animate(
+                    view: categoriesView,
+                    completionBlock: nil
+                )
+            }
+        )
     }
 }
