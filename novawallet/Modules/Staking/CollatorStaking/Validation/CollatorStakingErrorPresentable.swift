0c1fcd00d105f346ebe86f0bad2bfd5a25e7a5c4
ERussel <emkil.russel@gmail.com>
2025-01-27 11:26:07 +0100

    fix review

--- novawallet/Modules/Staking/CollatorStaking/Validation/CollatorStakingErrorPresentable.swift
+++ novawallet/Modules/Staking/CollatorStaking/Validation/CollatorStakingErrorPresentable.swift
@@ -1,26 +1,11 @@
 import Foundation
 
 protocol CollatorStakingErrorPresentable: BaseErrorPresentable {
-    func presentDelegatorExists(_ view: ControllerBackedProtocol, locale: Locale?)
     func presentStakeAmountTooLow(_ view: ControllerBackedProtocol, minStake: String, locale: Locale?)
     func presentDelegatorFull(_ view: ControllerBackedProtocol, maxAllowed: String, locale: Locale?)
 }
 
 extension CollatorStakingErrorPresentable where Self: AlertPresentable & ErrorPresentable {
-    func presentDelegatorExists(_ view: ControllerBackedProtocol, locale: Locale?) {
-        let title = R.string.localizable.parachainStakingDelegatorExistsTitle(
-            preferredLanguages: locale?.rLanguages
-        )
-
-        let message = R.string.localizable.parachainStakingDelegatorExistsMessage(
-            preferredLanguages: locale?.rLanguages
-        )
-
-        let closeAction = R.string.localizable.commonClose(preferredLanguages: locale?.rLanguages)
-
-        present(message: message, title: title, closeAction: closeAction, from: view)
-    }
-
     func presentDelegatorFull(_ view: ControllerBackedProtocol, maxAllowed: String, locale: Locale?) {
         let title = R.string.localizable.parachainStakingFullTitle(
             preferredLanguages: locale?.rLanguages
