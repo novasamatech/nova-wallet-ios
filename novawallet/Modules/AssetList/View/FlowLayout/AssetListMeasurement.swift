214be33156473df1544f27332c8c6de9154bd289
svojsu <svojsuu@gmail.com>
2025-03-10 14:16:15 +0200

    asset list insets fix

--- novawallet/Modules/AssetList/View/FlowLayout/AssetListMeasurement.swift
+++ novawallet/Modules/AssetList/View/FlowLayout/AssetListMeasurement.swift
@@ -11,11 +11,11 @@ enum AssetListMeasurement {
     static let assetHeaderHeight: CGFloat = 45.0
     static let emptyStateCellHeight: CGFloat = 230
     static let decorationInset: CGFloat = 8.0
-    static let promotionInsets = UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0)
+    static let promotionInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0.0, right: 0)
     static let summaryInsets = UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0)
-    static let nftsInsets = UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0)
+    static let nftsInsets = UIEdgeInsets(top: 0, left: 0, bottom: 4, right: 0)
     static let settingsInsets = UIEdgeInsets.zero
-    static let assetGroupInsets = UIEdgeInsets(top: 2.0, left: 0, bottom: 16.0, right: 0)
+    static let assetGroupInsets = UIEdgeInsets(top: 0.0, left: 0, bottom: 16.0, right: 0)
 
     static let underneathViewHeight: CGFloat = 4
     static let decorationContentInset: CGFloat = 4
