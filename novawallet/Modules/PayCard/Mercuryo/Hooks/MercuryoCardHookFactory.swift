64e319a664f28130ba1839d508dcef20d47afb99
ERussel <emkil.russel@gmail.com>
2025-03-13 14:54:22 +0100

    remove not needed TODO

--- novawallet/Modules/PayCard/Mercuryo/Hooks/MercuryoCardHookFactory.swift
+++ novawallet/Modules/PayCard/Mercuryo/Hooks/MercuryoCardHookFactory.swift
@@ -7,7 +7,7 @@ struct MercuryoCardParams {
 
 enum MercuryoCardApi {
     static let widgetUrl = URL(string: "https://exchange.mercuryo.io/")!
-    static let widgetId = "4ce98182-ed76-4933-ba1b-b85e4a51d75a" // TODO: Change for production
+    static let widgetId = "4ce98182-ed76-4933-ba1b-b85e4a51d75a"
     static let theme = "nova"
     static let type = "sell"
     static let fixFiatCurrency = "true"
