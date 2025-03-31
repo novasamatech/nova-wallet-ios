73c0752c1051c9716b82e1db20d65f29ce3f74ea
ERussel <emkil.russel@gmail.com>
2025-03-31 10:16:45 +0200

    simplify xcmv5 definition

--- novawallet/Common/Substrate/Types/Xcm/V5/XcmV5LocatableAsset.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension XcmV5 {
-    struct LocatableAsset: Equatable, Codable {
-        let location: XcmV3.Multilocation
-        let assetId: XcmV3.Multilocation
-    }
-}
