fc0b6de0306aef2364f6763d4758ff254a1cb2df
svojsu <svojsuu@gmail.com>
2025-05-07 11:25:35 +0300

    multisigs cache fix

--- novawallet/Common/Services/Proxy/DelegatedAccountsRepository/MultisigAccountsRepository.swift
+++ novawallet/Common/Services/Proxy/DelegatedAccountsRepository/MultisigAccountsRepository.swift
@@ -11,6 +11,10 @@ protocol MultisigAccountsRepositoryProtocol {
 final class MultisigAccountsRepository {
     private let chain: ChainModel
 
+    private let mutex = NSLock()
+    
+    private var multisigsBySignatories: [AccountId: [DiscoveredMultisig]] = [:]
+    
     init(chain: ChainModel) {
         self.chain = chain
     }
@@ -22,6 +26,17 @@ extension MultisigAccountsRepository: MultisigAccountsRepositoryProtocol {
     func fetchMultisigsWrapper(
         for accountIds: Set<AccountId>
     ) -> CompoundOperationWrapper<[AccountId: [DiscoveredMultisig]]> {
+        let cachedMultisigsForSignatories = accountIds
+            .map { (signatory: $0, multisigs: multisigsBySignatories[$0]) }
+            .reduce(into: [:]) { $0[$1.signatory] = $1.multisigs }
+        
+        let cachedSignatories = Set(cachedMultisigsForSignatories.keys)
+        let nonCachedSignatories = accountIds.subtracting(cachedSignatories)
+        
+        guard nonCachedSignatories.isEmpty else {
+            return .createWithResult(cachedMultisigsForSignatories)
+        }
+        
         guard let apiURL = chain.externalApis?.getApis(for: .multisig)?.first?.url else {
             return .createWithResult([:])
         }
@@ -30,25 +45,38 @@ extension MultisigAccountsRepository: MultisigAccountsRepositoryProtocol {
             url: apiURL
         )
 
-        let fetchWrapper = fetchFactory.createDiscoverMultisigsOperation(for: accountIds)
+        let fetchWrapper = fetchFactory.createDiscoverMultisigsOperation(for: nonCachedSignatories)
 
-        let mapOperation = ClosureOperation<[AccountId: [DiscoveredMultisig]]> {
+        let mapOperation = ClosureOperation<[AccountId: [DiscoveredMultisig]]> { [weak self] in
+            guard let self else {
+                throw BaseOperationError.parentOperationCancelled
+            }
+            
             let fetchResult = try fetchWrapper.targetOperation.extractNoCancellableResultData()
 
             guard let fetchResult else { return [:] }
 
-            return fetchResult.reduce(into: [:]) { acc, multisig in
-                let signatories = Set(multisig.signatories)
-                let knownAccountIds = accountIds.intersection(signatories)
-
-                knownAccountIds.forEach { accountId in
-                    if acc[accountId] == nil {
-                        acc[accountId]?.append(multisig)
-                    } else {
-                        acc[accountId] = [multisig]
+            let mappedFetchResult: [AccountId: [DiscoveredMultisig]] = fetchResult
+                .reduce(into: [:]) { acc, multisig in
+                    nonCachedSignatories.forEach { accountId in
+                        guard multisig.signatories.contains(accountId) else {
+                            return
+                        }
+                        
+                        if acc[accountId] == nil {
+                            acc[accountId]?.append(multisig)
+                        } else {
+                            acc[accountId] = [multisig]
+                        }
                     }
                 }
-            }
+            
+            mutex.lock()
+            multisigsBySignatories.merge(mappedFetchResult) { $0 + $1 }
+            mutex.lock()
+            
+            let result = cachedMultisigsForSignatories.merging(mappedFetchResult) { $0 + $1 }
+            return result
         }
 
         mapOperation.addDependency(fetchWrapper.targetOperation)
