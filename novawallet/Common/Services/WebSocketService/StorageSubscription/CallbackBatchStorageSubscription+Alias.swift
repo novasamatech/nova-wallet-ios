4400d885332597d23f113b3ce0dfc2332d24d037
ERussel <emkil.russel@gmail.com>
2025-05-21 19:04:13 +0200

    fix Mythos chain validation

---
+++ novawallet/Common/Services/WebSocketService/StorageSubscription/CallbackBatchStorageSubscription+Alias.swift
@@ -0,0 +1,9 @@
+//
+//  CallbackBatchStorageSubscription+Alias.swift
+//  novawallet
+//
+//  Created by Ruslan Rezin on 21.05.2025.
+//  Copyright © 2025 Nova Foundation. All rights reserved.
+//
+
+import Foundation
