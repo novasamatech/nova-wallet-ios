e8acb09374c88f51b5aff680a8b8bd9de18fd1e0
ERussel <emkil.russel@gmail.com>
2025-05-30 17:21:28 +0200

    adopt account management

--- novawallet/Modules/ManageWallets/AccountManagement/ViewModel/ChainAccountListViewModel.swift
+++ novawallet/Modules/ManageWallets/AccountManagement/ViewModel/ChainAccountListViewModel.swift
@@ -12,9 +12,12 @@ enum ChainAccountSectionType {
     case sharedSecret
     case customSecret
     case noSection
+    case custom(LocalizableResource<String>)
 
     var title: LocalizableResource<String>? {
         switch self {
+        case let .custom(title):
+            return title
         case .customSecret:
             return LocalizableResource { locale in
                 R.string.localizable.chainAccountsSectionTitleCustomSecret(preferredLanguages: locale.rLanguages)
