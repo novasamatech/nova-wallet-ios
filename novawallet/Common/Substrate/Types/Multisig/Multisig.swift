a7f2600b5712fb956975d679fb79ceaa0ea00047
svojsu <svojsuu@gmail.com>
2025-06-11 11:50:41 +0300

    fix multisig definition struct

--- novawallet/Common/Substrate/Types/Multisig/Multisig.swift
+++ novawallet/Common/Substrate/Types/Multisig/Multisig.swift
@@ -17,19 +17,17 @@ enum Multisig {
     struct MultisigDefinition: Codable, Equatable {
         enum CodingKeys: String, CodingKey {
             case timepoint = "when"
-            case deposit
             case depositor
             case approvals
         }
 
         let timepoint: MultisigTimepoint
-        let deposit: BigUInt
         @BytesCodable var depositor: AccountId
         var approvals: [BytesCodable]
     }
 
     struct MultisigTimepoint: Codable, Equatable {
-        let height: BlockNumber
+        @StringCodable var height: BlockNumber
         @StringCodable var index: UInt32
     }
 }
