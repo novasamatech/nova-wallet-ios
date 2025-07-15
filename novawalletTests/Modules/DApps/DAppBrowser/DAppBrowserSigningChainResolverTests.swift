2f082b0f2e949c51dbea2863996a3114c5d4f60d
MuizNadeem <muiz.nadeem@gmail.com>
2025-07-15 15:48:24 +0100

    fix tests

--- novawalletTests/Modules/DApps/DAppBrowser/DAppBrowserSigningChainResolverTests.swift
+++ novawalletTests/Modules/DApps/DAppBrowser/DAppBrowserSigningChainResolverTests.swift
@@ -1,6 +1,6 @@
 import XCTest
 @testable import novawallet
-import SubstrateSdk
+import NovaCrypto
 
 class DAppBrowserSigningChainResolverTests: XCTestCase {
     
@@ -30,6 +30,28 @@ class DAppBrowserSigningChainResolverTests: XCTestCase {
             hasCrowdloans: false
         )
         
+        // Add ethereumBased option to make it an Ethereum chain
+        let ethereumChainWithOption = ChainModel(
+            chainId: ethereumChain.chainId,
+            parentId: ethereumChain.parentId,
+            name: ethereumChain.name,
+            assets: ethereumChain.assets,
+            nodes: ethereumChain.nodes,
+            nodeSwitchStrategy: ethereumChain.nodeSwitchStrategy,
+            addressPrefix: ethereumChain.addressPrefix,
+            legacyAddressPrefix: ethereumChain.legacyAddressPrefix,
+            types: ethereumChain.types,
+            icon: ethereumChain.icon,
+            options: [.ethereumBased],
+            externalApis: ethereumChain.externalApis,
+            explorers: ethereumChain.explorers,
+            order: ethereumChain.order,
+            additional: ethereumChain.additional,
+            syncMode: ethereumChain.syncMode,
+            source: ethereumChain.source,
+            connectionMode: ethereumChain.connectionMode
+        )
+        
         let substrateChain = ChainModelGenerator.generateChain(
             generatingAssets: 1,
             addressPrefix: 0, // Polkadot chain
@@ -38,7 +60,7 @@ class DAppBrowserSigningChainResolverTests: XCTestCase {
             hasCrowdloans: false
         )
         
-        let chains = [ethereumChain, substrateChain]
+        let chains = [ethereumChainWithOption, substrateChain]
         
         // When
         let resolvedChain = try resolver.resolveChainForBytesSigning(
@@ -49,35 +71,50 @@ class DAppBrowserSigningChainResolverTests: XCTestCase {
         
         // Then
         XCTAssertTrue(resolvedChain.isEthereumBased, "Should resolve to an Ethereum-based chain")
-        XCTAssertEqual(resolvedChain.chainId, ethereumChain.chainId, "Should resolve to the correct Ethereum chain")
+        XCTAssertEqual(resolvedChain.chainId, ethereumChainWithOption.chainId, "Should resolve to the correct Ethereum chain")
     }
     
     func testSubstrateAddressChainResolution() throws {
         // Given
         let resolver = DAppSignBytesChainResolver()
-        let substrateAddress = "3rBVRJQeQzmvsVH8QvwJafGVdRtHLTFTUdMRoyTmggnx6Qqh"
+        let substrateAddress = "15cfSaBcTxNr8rV59cbhdMNCRagFr3GE6B3zZRsCp4QHHKPu"
         
         let accountId = try substrateAddress.toAccountId()
+        let cryptoType: UInt8 = 0 // sr25519
         
+        // Get the address prefix from the test address
+        let addressPrefix = try SS58AddressFactory().type(fromAddress: substrateAddress).uint16Value
+        
+        let substrateChain = ChainModelGenerator.generateChain(
+            generatingAssets: 1,
+            addressPrefix: ChainModel.AddressPrefix(addressPrefix), // Use the actual address prefix
+            assetPresicion: 12,
+            hasStaking: false,
+            hasCrowdloans: false
+        )
+        
+        // Add a chain account for the test chain
+        let chainAccount = ChainAccountModel(
+            chainId: substrateChain.chainId,
+            accountId: accountId,
+            publicKey: accountId,
+            cryptoType: cryptoType,
+            proxy: nil,
+            multisig: nil
+        )
+        
+        // Test with both chain account and root substrate fields
         let wallet = MetaAccountModel(
             metaId: UUID().uuidString,
             name: "Test Wallet",
             substrateAccountId: accountId,
-            substrateCryptoType: 0,
+            substrateCryptoType: cryptoType,
             substratePublicKey: accountId,
             ethereumAddress: nil,
             ethereumPublicKey: nil,
-            chainAccounts: [],
+            chainAccounts: [chainAccount],
             type: .secrets,
             multisig: nil
-        )
-        
-        let substrateChain = ChainModelGenerator.generateChain(
-            generatingAssets: 1,
-            addressPrefix: 0, // Polkadot chain
-            assetPresicion: 12,
-            hasStaking: false,
-            hasCrowdloans: false
         )
         
         let chains = [substrateChain]
