b97c53e9a110ae6ddc88a666e364f341067ab1e7
svojsu <svojsuu@gmail.com>
2025-09-02 01:18:11 +0300

    updated notifications cleaner tests

---
+++ novawalletTests/Common/Storage/WalletUpdatesCleaning/WalletNotificationsCleaningTests.swift
@@ -0,0 +1,9 @@
+//
+//  WalletNotificationsCleaningTests.swift
+//  novawalletTests
+//
+//  Created by Levan Chikvaidze on 01/09/2025.
+//  Copyright © 2025 Nova Foundation. All rights reserved.
+//
+
+import Foundation
