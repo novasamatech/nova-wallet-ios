22d2f560ac576d784dc5075222aad2e17736c393
svojsu <svojsuu@gmail.com>
2025-09-15 23:38:58 +0300

    privacy state manager added

---
+++ novawallet/Common/View/Securable/PrivacyStateManager.swift
@@ -0,0 +1,67 @@
+import UIKit
+import Foundation
+import Keystore_iOS
+
+enum ViewPrivacyMode {
+    case visible
+    case hidden(style: HidingStyle)
+    
+    enum HidingStyle {
+        case dots(text: String)
+        case blur
+        case placeholder(image: UIImage)
+        case invisible
+    }
+}
+
+protocol PrivacyStateManagerProtocol: AnyObject {
+    var settingsEnabled: Bool { get set }
+    
+    var lastEnabled: Bool { get set }
+
+    func addObserver(
+        with owner: AnyObject,
+        queue: DispatchQueue?,
+        closure: @escaping (Bool, Bool) -> Void
+    )
+
+    func removeObserver(by owner: AnyObject)
+}
+
+final class PrivacyStateManager: Observable<Bool>, PrivacyStateManagerProtocol {    
+    private let settingsManager: SettingsManagerProtocol
+    
+    var settingsEnabled: Bool {
+        get {
+            settingsManager.privacyModeSettings.privacySettingsEnabled
+        }
+        set {
+            updateState(with: settingsManager.privacyModeSettings
+                .with(privacySettingsEnabled: newValue))
+        }
+    }
+    
+    var lastEnabled: Bool {
+        get {
+            settingsManager.privacyModeSettings.lastEnabled
+        }
+        set {
+            updateState(with: settingsManager.privacyModeSettings
+                .with(lastEnabled: newValue))
+        }
+    }
+    
+    init(settingsManager: SettingsManagerProtocol) {
+        self.settingsManager = settingsManager
+        
+        super.init(state: settingsManager.privacyModeSettings.enabled)
+    }
+    
+    private func updateState(with updatedSettings: PrivacyModeSettings) {
+        settingsManager.privacyModeSettings = updatedSettings
+        
+        guard state != updatedSettings.enabled else { return }
+        
+        state = updatedSettings.enabled
+    }
+}
