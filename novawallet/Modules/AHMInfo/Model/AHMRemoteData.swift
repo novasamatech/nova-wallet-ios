49c17af3ba79332cd3bc35213f2083f1893800b2
svojsu <svojsuu@gmail.com>
2025-10-01 21:14:34 +0300

    fix config values decoding

--- novawallet/Modules/AHMInfo/Model/AHMRemoteData.swift
+++ novawallet/Modules/AHMInfo/Model/AHMRemoteData.swift
@@ -1,38 +1,21 @@
 import Foundation
 import BigInt
+import SubstrateSdk
 
 struct AHMRemoteData: Codable, Equatable {
     struct ChainData: Codable, Equatable {
         let chainId: ChainModel.Id
         let assetId: AssetModel.Id
-        let minBalance: BigUInt
-        let averageFee: BigUInt
+        @StringCodable var minBalance: BigUInt
+        @StringCodable var averageFee: BigUInt
     }
 
     let sourceData: ChainData
     let destinationData: ChainData
-    let blockNumber: BlockNumber = 20
+    let blockNumber: BlockNumber
     let timestamp: UInt64
     let newTokenNames: [String]
     let bannerPath: Banners.Domain
     let migrationInProgress: Bool
     let wikiURL: URL
-}
-
-extension AHMRemoteData.ChainData {
-    enum CodingKeys: String, CodingKey {
-        case chainId
-        case assetId
-        case minBalance
-        case averageFee
-    }
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        chainId = try container.decode(ChainModel.Id.self, forKey: .chainId)
-        assetId = try container.decode(AssetModel.Id.self, forKey: .assetId)
-        minBalance = try container.decodeHex(BigUInt.self, forKey: .minBalance)
-        averageFee = try container.decodeHex(BigUInt.self, forKey: .averageFee)
-    }
 }
