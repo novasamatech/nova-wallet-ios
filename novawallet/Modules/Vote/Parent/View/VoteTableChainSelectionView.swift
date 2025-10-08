ff48d7193c89ca7bcdbb98a2c8516a3f8a19da48
svojsu <svojsuu@gmail.com>
2025-10-08 13:13:54 +0300

    gov selector icon change

--- novawallet/Modules/Vote/Parent/View/VoteTableChainSelectionView.swift
+++ novawallet/Modules/Vote/Parent/View/VoteTableChainSelectionView.swift
@@ -4,15 +4,18 @@ import UIKit_iOS
 final class VoteTableChainSelectionControl: ControlView<
     TriangularedView,
     GenericTitleValueView<
-        IconDetailsGenericView<GenericMultiValueView<DotsSecureView<UILabel>>>,
+        GenericPairValueView<
+            AssetIconView,
+            GenericMultiValueView<DotsSecureView<UILabel>>
+        >,
         UIImageView
     >
 > {
-    var iconImageView: UIImageView { controlContentView.titleView.imageView }
+    var assetIconView: AssetIconView { controlContentView.titleView.fView }
 
-    var titleLabel: UILabel { controlContentView.titleView.detailsView.valueTop }
-    var subtitleLabel: UILabel { controlContentView.titleView.detailsView.valueBottom.originalView }
-    var subtitleSecureView: DotsSecureView<UILabel> { controlContentView.titleView.detailsView.valueBottom }
+    var titleLabel: UILabel { controlContentView.titleView.sView.valueTop }
+    var subtitleLabel: UILabel { controlContentView.titleView.sView.valueBottom.originalView }
+    var subtitleSecureView: DotsSecureView<UILabel> { controlContentView.titleView.sView.valueBottom }
 
     var accessoryImageView: UIImageView { controlContentView.valueView }
 
@@ -40,20 +43,29 @@ private extension VoteTableChainSelectionControl {
     // MARK: - Layout
 
     func setupLayout() {
+        setupContentBackgroundLayout()
         setupImageDetailsLayout()
         setupAccessoryImageLayout()
     }
 
+    func setupContentBackgroundLayout() {
+        contentInsets = Constants.contentInsets
+    }
+
     func setupImageDetailsLayout() {
+        controlContentView.titleView.makeHorizontal()
         controlContentView.titleView.spacing = Constants.iconDetailsSpacing
-        controlContentView.titleView.detailsView.spacing = Constants.titleSubtitleSpacing
-        controlContentView.titleView.detailsView.stackView.alignment = .leading
-        controlContentView.titleView.iconWidth = Constants.iconSize.width
+        controlContentView.titleView.sView.spacing = Constants.titleSubtitleSpacing
+        controlContentView.titleView.sView.stackView.alignment = .leading
 
         subtitleSecureView.preferredSecuredHeight = Constants.subtitleSecureViewHeight
 
         titleLabel.snp.makeConstraints { make in
             make.height.equalTo(Constants.titleLabelHeight)
+        }
+
+        assetIconView.snp.makeConstraints { make in
+            make.size.equalTo(Constants.iconSize)
         }
     }
 
@@ -69,6 +81,7 @@ private extension VoteTableChainSelectionControl {
         setupBackground()
         setupLabelStyles()
         setupAccessoryImage()
+        setupAssetIconViewStyle()
     }
 
     func setupBackground() {
@@ -89,6 +102,12 @@ private extension VoteTableChainSelectionControl {
         accessoryImageView.image = R.image.iconMore()?.withRenderingMode(.alwaysTemplate)
         accessoryImageView.tintColor = R.color.colorIconSecondary()!
     }
+
+    func setupAssetIconViewStyle() {
+        assetIconView.backgroundView.cornerRadius = Constants.iconSize.height / 2.0
+        assetIconView.backgroundView.fillColor = R.color.colorTokenContainerBackground()!
+        assetIconView.backgroundView.highlightedFillColor = R.color.colorContainerBackground()!
+    }
 }
 
 // MARK: - Internal
@@ -106,16 +125,7 @@ extension VoteTableChainSelectionControl {
     }
 
     func bind(imageViewModel: ImageViewModelProtocol) {
-        self.imageViewModel?.cancel(on: iconImageView)
-        iconImageView.image = nil
-
-        self.imageViewModel = imageViewModel
-
-        imageViewModel.loadImage(
-            on: iconImageView,
-            targetSize: Constants.iconSize,
-            animated: true
-        )
+        assetIconView.bind(viewModel: imageViewModel, size: Constants.iconSize)
     }
 }
 
@@ -126,10 +136,17 @@ private extension VoteTableChainSelectionControl {
         static let iconDetailsSpacing: CGFloat = 12.0
         static let titleSubtitleSpacing: CGFloat = 0.0
 
-        static let iconSize: CGSize = .init(width: 32, height: 32)
+        static let iconSize: CGSize = .init(width: 44, height: 44)
         static let accessoryImageSize: CGSize = .init(width: 24, height: 24)
 
         static let titleLabelHeight: CGFloat = 20
         static let subtitleSecureViewHeight: CGFloat = 18
+
+        static let contentInsets: UIEdgeInsets = .init(
+            top: .zero,
+            left: 10.0,
+            bottom: .zero,
+            right: 16.0
+        )
     }
 }
