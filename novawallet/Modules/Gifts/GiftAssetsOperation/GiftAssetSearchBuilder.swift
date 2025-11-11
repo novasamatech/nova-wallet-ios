b524b91b044fc14c792e1d5475c167d12be24fc2
svojsu <svojsuu@gmail.com>
2025-11-11 14:21:59 +0300

    fix balances on search

--- novawallet/Modules/Gifts/GiftAssetsOperation/GiftAssetSearchBuilder.swift
+++ novawallet/Modules/Gifts/GiftAssetsOperation/GiftAssetSearchBuilder.swift
@@ -1,4 +1,5 @@
 import Foundation
+import BigInt
 import Operation_iOS
 
 final class GiftAssetSearchBuilder: AssetSearchBuilder {
@@ -22,6 +23,30 @@ final class GiftAssetSearchBuilder: AssetSearchBuilder {
             callbackClosure: callbackClosure,
             operationQueue: operationQueue,
             logger: logger
+        )
+    }
+
+    override func assetListState(from model: AssetListModel) -> AssetListState {
+        let chainAssets = model.allChains.flatMap { _, chain in
+            chain.assets.map { ChainAssetId(chainId: chain.chainId, assetId: $0.assetId) }
+        }
+
+        let balanceResults = chainAssets.reduce(into: [ChainAssetId: Result<BigUInt, Error>]()) {
+            switch model.balances[$1] {
+            case let .success(amount):
+                $0[$1] = .success(amount.transferable)
+            case let .failure(error):
+                $0[$1] = .failure(error)
+            case .none:
+                $0[$1] = .success(0)
+            }
+        }
+
+        return AssetListState(
+            priceResult: model.priceResult,
+            balanceResults: balanceResults,
+            allChains: model.allChains,
+            externalBalances: nil
         )
     }
 
