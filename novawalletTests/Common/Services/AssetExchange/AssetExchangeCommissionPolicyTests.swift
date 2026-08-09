5e90d2a49886c8c4026c448c8541e44227e961e2
svojsu <svojsuu@gmail.com>
2026-08-09 18:03:35 +0300

    test commission policy selection, gate and arithmetic

---
+++ novawalletTests/Common/Services/AssetExchange/AssetExchangeCommissionPolicyTests.swift
@@ -0,0 +1,361 @@
+import XCTest
+@testable import novawallet
+import Operation_iOS
+import BigInt
+import Cuckoo
+
+final class AssetExchangeCommissionPolicyTests: XCTestCase {
+    func testChargingIndexForRouteShapes() {
+        let policy = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1).policy
+
+        XCTAssertEqual(
+            policy.chargingOperationIndex(in: CommissionTestFixtures.createPath([.crossChain, .hydraSwap, .crossChain])),
+            1
+        )
+        XCTAssertEqual(
+            policy.chargingOperationIndex(in: CommissionTestFixtures.createPath([.hydraSwap, .crossChain, .assetHubSwap])),
+            0
+        )
+        XCTAssertEqual(
+            policy.chargingOperationIndex(
+                in: CommissionTestFixtures.createPath([.hydraSwap, .hydraSwap, .crossChain, .hydraSwap])
+            ),
+            2
+        )
+        XCTAssertEqual(
+            policy.chargingOperationIndex(
+                in: CommissionTestFixtures.createPath(
+                    [.hydraSwap, .hydraSwap, .crossChain, .assetHubSwap, .crossChain, .hydraSwap]
+                )
+            ),
+            4
+        )
+        XCTAssertEqual(
+            policy.chargingOperationIndex(in: CommissionTestFixtures.createPath([.hydraSwap, .hydraSwap])),
+            0
+        )
+    }
+
+    func testChargingIndexMatchesAtomicGrouping() throws {
+        try assertChargingIndexMatchesAtomicGrouping(
+            edgeTypes: [.hydraSwap, .hydraSwap, .crossChain, .hydraSwap],
+            expectedOperationCount: 3,
+            expectedChargingIndex: 2,
+            expectedChargedAssetId: 4
+        )
+
+        try assertChargingIndexMatchesAtomicGrouping(
+            edgeTypes: [.hydraSwap, .hydraSwap, .crossChain, .assetHubSwap, .crossChain, .hydraSwap],
+            expectedOperationCount: 5,
+            expectedChargingIndex: 4,
+            expectedChargedAssetId: 6
+        )
+    }
+
+    func testChargingIndexEqualsMetaOperationIndex() throws {
+        try assertChargingIndexEqualsMetaOperationIndex(
+            edgeTypes: [.hydraSwap, .hydraSwap, .crossChain, .hydraSwap],
+            expectedOperationCount: 3
+        )
+
+        try assertChargingIndexEqualsMetaOperationIndex(
+            edgeTypes: [.hydraSwap, .hydraSwap, .crossChain, .assetHubSwap, .crossChain, .hydraSwap],
+            expectedOperationCount: 5
+        )
+    }
+
+    func testAllHydraPoolFamiliesCharge() {
+        let edges = CommissionTestFixtures.makeHydraEdges()
+
+        XCTAssertEqual(edges.count, 4)
+        edges.forEach { XCTAssertEqual($0.type, .hydraSwap) }
+    }
+
+    func testNoChargeWithoutHydraEdge() {
+        let policy = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1).policy
+
+        XCTAssertNil(policy.chargingOperationIndex(in: CommissionTestFixtures.createPath([.crossChain, .crossChain])))
+        XCTAssertNil(policy.chargingOperationIndex(in: CommissionTestFixtures.createPath([.assetHubSwap, .crossChain])))
+        XCTAssertNil(policy.chargingOperationIndex(in: CommissionTestFixtures.createPath([])))
+    }
+
+    func testAmountNeverExceedsRateOfQuote() throws {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1)
+        let route = CommissionTestFixtures.createRoute([.hydraSwap], amount: 1_000_000)
+
+        let withSlippage = try resolveCommission(
+            using: policyUnderTest.policy,
+            route: route,
+            slippage: BigRational(numerator: 1, denominator: 100)
+        )
+        let withSlippageAmount = try XCTUnwrap(withSlippage?.estimatedAmount)
+
+        XCTAssertEqual(withSlippageAmount, 8_415)
+        XCTAssertLessThanOrEqual(withSlippageAmount, AssetExchangeCommissionConstants.rate.mul(value: 1_000_000))
+
+        let noSlippage = try resolveCommission(
+            using: policyUnderTest.policy,
+            route: route,
+            slippage: BigRational(numerator: 0, denominator: 100)
+        )
+
+        XCTAssertEqual(noSlippage?.estimatedAmount, 8_500)
+    }
+
+    func testBaseComesFromLastEdgeOfRun() throws {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1)
+        let route = CommissionTestFixtures.createRoute([.hydraSwap, .hydraSwap], amounts: [1_000_000, 7_000_000])
+
+        let commission = try resolveCommission(
+            using: policyUnderTest.policy,
+            route: route,
+            slippage: BigRational(numerator: 0, denominator: 100)
+        )
+
+        XCTAssertEqual(commission?.asset, CommissionTestFixtures.asset(2))
+        XCTAssertEqual(commission?.estimatedAmount, 59_500)
+    }
+
+    func testCommissionAssetIsChargingSegmentAssetOut() throws {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1)
+        let route = CommissionTestFixtures.createRoute([.crossChain, .hydraSwap, .crossChain], amount: 1_000_000)
+
+        let commission = try resolveCommission(using: policyUnderTest.policy, route: route)
+
+        XCTAssertEqual(commission?.asset, CommissionTestFixtures.asset(2))
+    }
+
+    func testBeneficiaryDecodesToConfiguredAccount() throws {
+        let expectedBeneficiary = try AssetExchangeCommissionConstants.hydrationBeneficiaryAddress.toAccountId()
+        XCTAssertEqual(expectedBeneficiary.count, 32)
+
+        let policy = AssetExchangeCommissionPolicyFactory.createHydrationPolicy(
+            chainRegistry: MockChainRegistryProtocol().applyDefault(for: [CommissionTestFixtures.chain]),
+            operationQueue: OperationQueue(),
+            logger: Logger.shared
+        )
+
+        let concretePolicy = try XCTUnwrap(policy as? AssetExchangeCommissionPolicy)
+        XCTAssertEqual(concretePolicy.beneficiary, expectedBeneficiary)
+    }
+
+    func testSkipsWhenBeneficiaryBelowExistentialDeposit() throws {
+        let belowDeposit = CommissionTestFixtures.createPolicy(beneficiaryFree: 0, minBalance: 1_000_000)
+        let poorRoute = CommissionTestFixtures.createRoute([.hydraSwap], amount: 1_000_000_000)
+
+        XCTAssertNil(try resolveCommission(using: belowDeposit.policy, route: poorRoute))
+
+        let atDeposit = CommissionTestFixtures.createPolicy(beneficiaryFree: 1_000_000, minBalance: 1_000_000)
+        let tinyRoute = CommissionTestFixtures.createRoute([.hydraSwap], amount: 200)
+
+        let commission = try resolveCommission(using: atDeposit.policy, route: tinyRoute)
+
+        XCTAssertEqual(commission?.estimatedAmount, 1)
+    }
+
+    func testGateIsIndependentOfAmount() throws {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(beneficiaryFree: 5, minBalance: 5)
+
+        let largeRoute = CommissionTestFixtures.createRoute([.hydraSwap], amount: 1_000_000_000)
+        XCTAssertNotNil(try resolveCommission(using: policyUnderTest.policy, route: largeRoute))
+
+        let smallRoute = CommissionTestFixtures.createRoute([.hydraSwap], amount: 200)
+        XCTAssertNotNil(try resolveCommission(using: policyUnderTest.policy, route: smallRoute))
+    }
+
+    func testSkipsForZeroAmount() throws {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(beneficiaryFree: 5, minBalance: 5)
+        let route = CommissionTestFixtures.createRoute([.hydraSwap], amount: 117)
+
+        let commission = try resolveCommission(using: policyUnderTest.policy, route: route)
+
+        XCTAssertNil(commission)
+        XCTAssertEqual(policyUnderTest.balanceQueryFactory.callCount, 0)
+        XCTAssertEqual(policyUnderTest.storageInfoFactory.depositCallCount, 0)
+    }
+
+    func testSkipsForEvmChargedAsset() throws {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(
+            beneficiaryFree: 10,
+            minBalance: 1,
+            storageInfoResult: .success(.evmNative)
+        )
+        let route = CommissionTestFixtures.createRoute([.hydraSwap], amount: 1_000_000_000)
+
+        XCTAssertNil(try resolveCommission(using: policyUnderTest.policy, route: route))
+    }
+
+    func testFeeAssetNeverCharged() throws {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1)
+        let route = CommissionTestFixtures.createRoute([.hydraSwap], amount: 1_000_000)
+
+        let commission = try resolveCommission(using: policyUnderTest.policy, route: route)
+
+        XCTAssertEqual(commission?.asset, CommissionTestFixtures.asset(1))
+    }
+
+    func testFailurePropagatesRatherThanNilCommission() {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(
+            beneficiaryFree: 10,
+            minBalance: 1,
+            storageInfoResult: .failure(CommonError.dataCorruption)
+        )
+        let route = CommissionTestFixtures.createRoute([.hydraSwap], amount: 1_000_000_000)
+
+        XCTAssertThrowsError(try resolveCommission(using: policyUnderTest.policy, route: route))
+    }
+
+    func testEachFeeRequestReReadsTheBeneficiaryBalance() throws {
+        let policyUnderTest = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1)
+        let firstRoute = CommissionTestFixtures.createRoute([.hydraSwap], amount: 1_000_000_000)
+
+        _ = try resolveCommission(using: policyUnderTest.policy, route: firstRoute)
+
+        XCTAssertEqual(policyUnderTest.storageInfoFactory.depositCallCount, 1)
+        XCTAssertEqual(policyUnderTest.balanceQueryFactory.callCount, 1)
+
+        let secondRoute = CommissionTestFixtures.createRoute([.hydraSwap], amount: 1_000_000_000)
+
+        _ = try resolveCommission(using: policyUnderTest.policy, route: secondRoute)
+
+        XCTAssertEqual(policyUnderTest.storageInfoFactory.depositCallCount, 2)
+        XCTAssertEqual(policyUnderTest.balanceQueryFactory.callCount, 2)
+
+        policyUnderTest.balanceQueryFactory.requestedAccountIds.forEach {
+            XCTAssertEqual($0, CommissionTestFixtures.beneficiary)
+        }
+    }
+
+    func testGrossUpIsCeilingAndPathScoped() {
+        let policy = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1).policy
+
+        XCTAssertEqual(
+            policy.grossingUpAmountOut(1_000_000_000, for: CommissionTestFixtures.createPath([.hydraSwap])),
+            1_008_572_870
+        )
+        XCTAssertEqual(
+            policy.grossingUpAmountOut(1_000_000_000, for: CommissionTestFixtures.createPath([.crossChain])),
+            1_000_000_000
+        )
+    }
+
+    func testBuyGrossUpBoundedByOnePlank() {
+        let policy = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1).policy
+        let path = CommissionTestFixtures.createPath([.hydraSwap])
+
+        let targets: [Balance] = [1, 2, 117, 118, 999, 1_000_000_000, 123_456_789_012_345]
+
+        for target in targets {
+            let gross = policy.grossingUpAmountOut(target, for: path)
+            let net = policy.netAmount(from: gross, willCharge: true)
+
+            XCTAssertGreaterThanOrEqual(net, target)
+            XCTAssertLessThanOrEqual(net, target + 1)
+        }
+    }
+
+    func testNetAmountAppliesRateNotAbsoluteAmount() {
+        let policy = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1).policy
+
+        XCTAssertEqual(policy.netAmount(from: 1_000_000, willCharge: true), 991_500)
+        XCTAssertEqual(policy.netAmount(from: 1_000_000, willCharge: false), 1_000_000)
+    }
+
+    func testArithmeticAtExtremes() {
+        let policy = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1).policy
+        let path = CommissionTestFixtures.createPath([.hydraSwap])
+
+        XCTAssertEqual(policy.netAmount(from: 0, willCharge: true), 0)
+        XCTAssertEqual(policy.netAmount(from: 1, willCharge: true), 1)
+
+        let huge = Balance(UInt64.max) * Balance(UInt64.max)
+
+        let netHuge = policy.netAmount(from: huge, willCharge: true)
+        let grossHuge = policy.grossingUpAmountOut(huge, for: path)
+
+        XCTAssertLessThan(netHuge, huge)
+        XCTAssertGreaterThan(grossHuge, huge)
+
+        XCTAssertEqual(policy.grossingUpAmountOut(0, for: path), 0)
+    }
+}
+
+private extension AssetExchangeCommissionPolicyTests {
+    func resolveCommission(
+        using policy: AssetExchangeCommissionPolicyProtocol,
+        route: AssetExchangeRoute,
+        slippage: BigRational = BigRational(numerator: 0, denominator: 100)
+    ) throws -> AssetExchangeCommission? {
+        let wrapper = policy.resolveCommissionWrapper(for: route, slippage: slippage)
+
+        OperationQueue().addOperations(wrapper.allOperations, waitUntilFinished: true)
+
+        return try wrapper.targetOperation.extractNoCancellableResultData()
+    }
+
+    func assertChargingIndexMatchesAtomicGrouping(
+        edgeTypes: [AssetExchangeEdgeType],
+        expectedOperationCount: Int,
+        expectedChargingIndex: Int,
+        expectedChargedAssetId: AssetModel.Id
+    ) throws {
+        let route = CommissionTestFixtures.createRoute(edgeTypes, amount: 1_000_000)
+        let path = route.items.map(\.edge)
+
+        let policy = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1).policy
+        let chargingOperationIndex = try XCTUnwrap(policy.chargingOperationIndex(in: path))
+
+        let commission = AssetExchangeCommission(
+            chargingOperationIndex: chargingOperationIndex,
+            asset: CommissionTestFixtures.asset(expectedChargedAssetId),
+            estimatedAmount: 1,
+            beneficiary: CommissionTestFixtures.beneficiary,
+            rate: AssetExchangeCommissionConstants.rate
+        )
+
+        let factory = CommissionTestFixtures.makeFactory()
+
+        let operations = try factory.prepareAtomicOperations(
+            for: route,
+            slippage: BigRational(numerator: 0, denominator: 100),
+            feeAssetId: CommissionTestFixtures.asset(0),
+            commission: commission
+        )
+
+        XCTAssertEqual(operations.count, expectedOperationCount)
+
+        let chargingIndices = operations.enumerated().compactMap { index, operation -> Int? in
+            (operation as? StubAtomicOperation)?.args.commission != nil ? index : nil
+        }
+
+        XCTAssertEqual(chargingIndices, [expectedChargingIndex])
+
+        let chargingOperation = try XCTUnwrap(operations[expectedChargingIndex] as? StubAtomicOperation)
+        XCTAssertEqual(chargingOperation.edges.last?.destination, CommissionTestFixtures.asset(expectedChargedAssetId))
+    }
+
+    func assertChargingIndexEqualsMetaOperationIndex(
+        edgeTypes: [AssetExchangeEdgeType],
+        expectedOperationCount: Int
+    ) throws {
+        let route = CommissionTestFixtures.createRoute(edgeTypes, amount: 1_000_000)
+
+        let policyUnderTest = CommissionTestFixtures.createPolicy(beneficiaryFree: 10, minBalance: 1)
+        let commission = try XCTUnwrap(resolveCommission(using: policyUnderTest.policy, route: route))
+
+        let factory = CommissionTestFixtures.makeFactory()
+
+        let atomicOperations = try factory.prepareAtomicOperations(
+            for: route,
+            slippage: BigRational(numerator: 0, denominator: 100),
+            feeAssetId: CommissionTestFixtures.asset(0),
+            commission: commission
+        )
+
+        let metaOperations = try factory.createMetaOperationsFrom(route: route)
+
+        XCTAssertEqual(atomicOperations.count, expectedOperationCount)
+        XCTAssertEqual(metaOperations.count, atomicOperations.count)
+        XCTAssertEqual(metaOperations[commission.chargingOperationIndex].assetOut.chainAssetId, commission.asset)
+    }
+}
---
+++ novawalletTests/Common/Services/AssetExchange/CommissionTestFixtures.swift
@@ -0,0 +1,318 @@
+import Foundation
+@testable import novawallet
+import Operation_iOS
+import SubstrateSdk
+import BigInt
+import Cuckoo
+
+enum CommissionTestFixtures {
+    // `addressPrefix` is `ChainModel.AddressPrefix`, a typealias for `UInt64`
+    // (`ChainModel.swift:10`), so the integer literal binds without a conversion.
+    static let chain = ChainModelGenerator.generateChain(
+        defaultChainId: KnowChainId.hydra,
+        generatingAssets: 8,
+        addressPrefix: 63
+    )
+
+    static func asset(_ id: AssetModel.Id) -> ChainAssetId {
+        ChainAssetId(chainId: chain.chainId, assetId: id)
+    }
+
+    /// Path whose i-th edge has the given type and connects asset i to asset i+1.
+    static func createPath(_ types: [AssetExchangeEdgeType]) -> AssetExchangeGraphPath {
+        types.enumerated().map { index, type in
+            AnyAssetExchangeEdge(
+                StubAssetExchangeEdge(
+                    origin: asset(AssetModel.Id(index)),
+                    destination: asset(AssetModel.Id(index + 1)),
+                    type: type,
+                    chain: chain
+                )
+            )
+        }
+    }
+
+    /// Route over `createPath(types)`. `amounts` gives each item's quote in path order, so
+    /// `items[i].amountOut(for: .sell)` is `amounts[i]`. Precondition: counts are equal.
+    static func createRoute(
+        _ types: [AssetExchangeEdgeType],
+        amounts: [Balance],
+        direction: AssetConversion.Direction = .sell
+    ) -> AssetExchangeRoute {
+        let items = zip(createPath(types), amounts).map { edge, amount in
+            AssetExchangeRouteItem(edge: edge, amount: amount, quote: amount)
+        }
+
+        return AssetExchangeRoute(items: items, amount: amounts.first ?? 0, direction: direction)
+    }
+
+    /// Every item carries the same amount.
+    static func createRoute(
+        _ types: [AssetExchangeEdgeType],
+        amount: Balance,
+        direction: AssetConversion.Direction = .sell
+    ) -> AssetExchangeRoute {
+        createRoute(types, amounts: Array(repeating: amount, count: types.count), direction: direction)
+    }
+
+    static func ormlInfo(existentialDeposit: Balance) -> AssetStorageInfo {
+        .orml(
+            info: OrmlTokenStorageInfo(
+                currencyId: .stringValue("0"),
+                currencyData: Data(),
+                module: "Tokens",
+                existentialDeposit: existentialDeposit,
+                canTransferAll: true
+            )
+        )
+    }
+
+    static let beneficiary = AccountId(repeating: 1, count: 32)
+
+    struct PolicyUnderTest {
+        let policy: AssetExchangeCommissionPolicy
+        let storageInfoFactory: CountingAssetStorageInfoFactory
+        let balanceQueryFactory: CountingBalanceQueryFactory
+    }
+
+    /// The stubs are returned alongside the policy because three rows assert on their counters,
+    /// and the policy stores them behind protocol types.
+    static func createPolicy(
+        beneficiaryFree: Balance,
+        minBalance: Balance,
+        storageInfoResult: Result<AssetStorageInfo, Error> = .success(ormlInfo(existentialDeposit: 1))
+    ) -> PolicyUnderTest {
+        let storageInfoFactory = CountingAssetStorageInfoFactory(
+            storageInfoResult: storageInfoResult,
+            minBalance: minBalance
+        )
+
+        let balanceQueryFactory = CountingBalanceQueryFactory(free: beneficiaryFree)
+
+        let policy = AssetExchangeCommissionPolicy(
+            rate: AssetExchangeCommissionConstants.rate,
+            beneficiary: beneficiary,
+            assetStorageInfoFactory: storageInfoFactory,
+            balanceQueryFactory: balanceQueryFactory,
+            chainRegistry: MockChainRegistryProtocol().applyDefault(for: [chain]),
+            operationQueue: OperationQueue()
+        )
+
+        return PolicyUnderTest(
+            policy: policy,
+            storageInfoFactory: storageInfoFactory,
+            balanceQueryFactory: balanceQueryFactory
+        )
+    }
+}
+
+final class CountingAssetStorageInfoFactory: AssetStorageInfoOperationFactoryProtocol {
+    let storageInfoResult: Result<AssetStorageInfo, Error>
+    let minBalance: Balance
+
+    private(set) var storageInfoCallCount = 0
+    private(set) var depositCallCount = 0
+
+    init(storageInfoResult: Result<AssetStorageInfo, Error>, minBalance: Balance) {
+        self.storageInfoResult = storageInfoResult
+        self.minBalance = minBalance
+    }
+
+    func createStorageInfoWrapper(
+        from _: AssetModel,
+        runtimeProvider _: RuntimeCodingServiceProtocol
+    ) -> CompoundOperationWrapper<AssetStorageInfo> {
+        storageInfoCallCount += 1
+
+        switch storageInfoResult {
+        case let .success(info):
+            return .createWithResult(info)
+        case let .failure(error):
+            return .createWithError(error)
+        }
+    }
+
+    func createAssetBalanceExistenceOperation(
+        for _: AssetStorageInfo,
+        chainId _: ChainModel.Id,
+        asset _: AssetModel
+    ) -> CompoundOperationWrapper<AssetBalanceExistence> {
+        depositCallCount += 1
+
+        return .createWithResult(AssetBalanceExistence(minBalance: minBalance, isSelfSufficient: true))
+    }
+}
+
+final class CountingBalanceQueryFactory: WalletRemoteQueryWrapperFactoryProtocol {
+    let free: Balance
+
+    private(set) var callCount = 0
+    private(set) var requestedAccountIds: [AccountId] = []
+
+    init(free: Balance) {
+        self.free = free
+    }
+
+    func queryBalance(
+        for accountId: AccountId,
+        chainAsset: ChainAsset
+    ) -> CompoundOperationWrapper<AssetBalance> {
+        callCount += 1
+        requestedAccountIds.append(accountId)
+
+        return .createWithResult(
+            AssetBalance(
+                chainAssetId: chainAsset.chainAssetId,
+                accountId: accountId,
+                freeInPlank: free,
+                reservedInPlank: 0,
+                frozenInPlank: 0,
+                edCountMode: .basedOnFree,
+                transferrableMode: .regular,
+                blocked: false
+            )
+        )
+    }
+}
+
+final class StubExchangePathCostEstimator: AssetsExchangePathCostEstimating {
+    func costEstimationWrapper(
+        for _: AssetExchangeGraphPath
+    ) -> CompoundOperationWrapper<AssetsExchangePathCost> {
+        CompoundOperationWrapper.createWithResult(.zero)
+    }
+}
+
+final class StubExchangeGraph: AssetsExchangeGraphProtocol {
+    let paths: [AssetExchangeGraphPath]
+
+    init(paths: [AssetExchangeGraphPath]) {
+        self.paths = paths
+    }
+
+    func fetchPaths(
+        from _: ChainAssetId,
+        to _: ChainAssetId,
+        maxTopPaths _: Int
+    ) -> [AssetExchangeGraphPath] {
+        paths
+    }
+
+    func fetchReachability() -> AssetsExchageGraphReachabilityProtocol {
+        fatalError("unused")
+    }
+
+    func fetchAssetsIn(given _: ChainAssetId?) -> Set<ChainAssetId> {
+        []
+    }
+
+    func fetchAssetsOut(given _: ChainAssetId?) -> Set<ChainAssetId> {
+        []
+    }
+}
+
+final class HydraExchangeHostStub: HydraExchangeHostProtocol {
+    var chain: ChainModel { CommissionTestFixtures.chain }
+    var operationQueue: OperationQueue { OperationQueue() }
+    var logger: LoggerProtocol { Logger.shared }
+
+    var selectedAccount: ChainAccountResponse { fatalError("unused") }
+    var submissionMonitorFactory: ExtrinsicSubmitMonitorFactoryProtocol { fatalError("unused") }
+    var extrinsicOperationFactory: ExtrinsicOperationFactoryProtocol { fatalError("unused") }
+    var extrinsicParamsFactory: HydraExchangeExtrinsicParamsFactoryProtocol { fatalError("unused") }
+    var signingWrapper: SigningWrapperProtocol { fatalError("unused") }
+    var runtimeService: RuntimeProviderProtocol { fatalError("unused") }
+    var connection: JSONRPCEngine { fatalError("unused") }
+    var executionTimeEstimator: AssetExchangeTimeEstimating { fatalError("unused") }
+}
+
+extension CommissionTestFixtures {
+    static func makeFactory() -> AssetsExchangeOperationFactory {
+        AssetsExchangeOperationFactory(
+            graph: StubExchangeGraph(paths: []),
+            pathCostEstimator: StubExchangePathCostEstimator(),
+            commissionPolicy: createPolicy(beneficiaryFree: 10, minBalance: 1).policy,
+            operationQueue: OperationQueue(),
+            logger: Logger.shared
+        )
+    }
+
+    static func makeHydraEdges() -> [any AssetExchangableGraphEdge] {
+        let account = AccountGenerator.generateSubstrateChainAccountResponse(for: KnowChainId.hydra)
+        let connection = TestJSONRPCEngine()
+        let runtimeProvider = MockRuntimeProviderProtocol().applyDefault(for: chain.chainId)
+        let queue = OperationQueue()
+        let host = HydraExchangeHostStub()
+        let pair = HydraDx.RemoteSwapPair(assetIn: 0, assetOut: 1)
+
+        return [
+            HydraOmnipoolExchangeEdge(
+                origin: asset(0),
+                destination: asset(1),
+                remoteSwapPair: pair,
+                host: host,
+                quoteFactory: HydraOmnipoolQuoteFactory(
+                    flowState: HydraOmnipoolFlowState(
+                        account: account,
+                        chain: chain,
+                        connection: connection,
+                        runtimeProvider: runtimeProvider,
+                        notificationsRegistrar: nil,
+                        operationQueue: queue,
+                        logger: Logger.shared
+                    )
+                )
+            ),
+            HydraStableswapExchangeEdge(
+                origin: asset(0),
+                destination: asset(1),
+                remoteSwapPair: pair,
+                poolAsset: 0,
+                host: host,
+                quoteFactory: HydraStableswapQuoteFactory(
+                    flowState: HydraStableswapFlowState(
+                        account: account,
+                        chain: chain,
+                        connection: connection,
+                        runtimeProvider: runtimeProvider,
+                        notificationsRegistrar: nil,
+                        operationQueue: queue
+                    )
+                )
+            ),
+            AssetsHydraXYKExchangeEdge(
+                origin: asset(0),
+                destination: asset(1),
+                remoteSwapPair: pair,
+                host: host,
+                quoteFactory: HydraXYKSwapQuoteFactory(
+                    flowState: HydraXYKFlowState(
+                        account: account,
+                        chain: chain,
+                        connection: connection,
+                        runtimeProvider: runtimeProvider,
+                        notificationsRegistrar: nil,
+                        operationQueue: queue,
+                        logger: Logger.shared
+                    )
+                )
+            ),
+            HydraAaveExchangeEdge(
+                origin: asset(0),
+                destination: asset(1),
+                remoteSwapPair: pair,
+                host: host,
+                quoteFactory: HydraAaveSwapQuoteFactory(
+                    flowState: HydraAaveFlowState(
+                        account: account,
+                        connection: connection,
+                        runtimeProvider: runtimeProvider,
+                        notificationsRegistrar: nil,
+                        operationQueue: queue,
+                        logger: Logger.shared
+                    )
+                )
+            )
+        ]
+    }
+}
