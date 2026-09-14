61744a78ce1b7be26c3c216e219c5f8686899ca3
svojsu <svojsuu@gmail.com>
2026-09-14 03:34:26 +0300

    cover the whole config extension with the line length exemption

--- novawallet/Common/Configs/ApplicationConfigs.swift
+++ novawallet/Common/Configs/ApplicationConfigs.swift
@@ -66,6 +66,7 @@ final class ApplicationConfig {
 }
 
 extension ApplicationConfig: ApplicationConfigProtocol {
+    // swiftlint:disable line_length
     var termsURL: URL {
         URL(string: "https://novawallet.io/terms")!
     }
@@ -123,7 +124,6 @@ extension ApplicationConfig: ApplicationConfigProtocol {
     // swiftlint:enable force_cast
 
     var logoURL: URL {
-        // swiftlint:disable:next line_length
         let logoString = "https://raw.githubusercontent.com/novasamatech/branding/master/logos/Nova_Wallet_Horizontal_iOS_Ramp.png"
         return URL(string: logoString)!
     }
@@ -265,22 +265,18 @@ extension ApplicationConfig: ApplicationConfigProtocol {
     }
 
     var learnPayoutURL: URL {
-        // swiftlint:disable:next line_length
         URL(string: "https://docs.novawallet.io/nova-wallet-wiki/staking/staking-faq#q-what-is-the-difference-between-restake-rewards-and-transferable-rewards")!
     }
 
     var learnControllerAccountURL: URL {
-        // swiftlint:disable:next line_length
         URL(string: "https://docs.novawallet.io/nova-wallet-wiki/staking/staking-faq#q-what-are-stash-and-controller-accounts")!
     }
 
     var paritySignerTroubleshoutingURL: URL {
-        // swiftlint:disable:next line_length
         URL(string: "https://docs.novawallet.io/nova-wallet-wiki/wallet-management/hardware-wallets/parity-signer/troubleshooting")!
     }
 
     var polkadotVaultTroubleshoutingURL: URL {
-        // swiftlint:disable:next line_length
         URL(string: "https://docs.novawallet.io/nova-wallet-wiki/wallet-management/hardware-wallets/polkadot-vault/troubleshooting")!
     }
 
@@ -289,12 +285,10 @@ extension ApplicationConfig: ApplicationConfigProtocol {
     }
 
     var ledgerMigrationURL: URL {
-        // swiftlint:disable:next line_length
         URL(string: "https://docs.novawallet.io/nova-wallet-wiki/wallet-management/hardware-wallets/ledger-nano-x/ledger-app-migration")!
     }
 
     var learnRecommendedValidatorsURL: URL {
-        // swiftlint:disable:next line_length
         URL(string: "https://docs.novawallet.io/nova-wallet-wiki/staking/staking-faq#q-how-does-nova-wallet-select-validators-collators")!
     }
 
@@ -310,7 +304,6 @@ extension ApplicationConfig: ApplicationConfigProtocol {
         URL(string: "https://docs.novawallet.io/nova-wallet-wiki/misc/developer-documentation/integrate-network")!
     }
 
-    // swiftlint:disable line_length
     var inAppUpdatesEntrypointURL: URL {
         #if F_RELEASE
             URL(string: "https://raw.githubusercontent.com/novasamatech/nova-wallet-ios-releases/master/updates/v1/entrypoint_release.json")!
